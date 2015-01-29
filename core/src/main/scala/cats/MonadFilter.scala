bc9c3cbc8a23d6a0e25692be583263ad5a36a176
Erik Osheim <d_m@plastic-idolatry.com>
2015-01-29 00:09:09 -0500

    Add MonadFilter and MonadCombine.
    MonadFilter has a concept of emptiness.
    MonadCombine (aka MonadPlus) has a MonoidK as well.

---
+++ core/src/main/scala/cats/MonadFilter.scala
@@ -0,0 +1,14 @@
+package cats
+
+import simulacrum._
+
+@typeclass trait MonadFilter[F[_]] extends Monad[F] {
+
+  def empty[A]: F[A]
+
+  def filter[A](fa: F[A])(f: A => Boolean): F[A] =
+    flatMap(fa)(a => if (f(a)) pure(a) else empty[A])
+
+  def filterM[A](fa: F[A])(f: A => F[Boolean]): F[A] =
+    flatMap(fa)(a => flatMap(f(a))(b => if (b) pure(a) else empty[A]))
+}
