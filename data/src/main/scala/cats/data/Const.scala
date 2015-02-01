bac9b1ef962a06472bb965cee464fcc64a1bda80
Julien Truffaut <truffaut.julien@gmail.com>
2015-02-01 19:55:34 +0100

    correct Apply.apply implementation for Const and review implicit usage

--- data/src/main/scala/cats/data/Const.scala
+++ data/src/main/scala/cats/data/Const.scala
@@ -18,8 +18,8 @@ sealed abstract class ConstInstances extends ConstInstances0 {
   implicit def constOrder[A: Order, B]: Order[Const[A, B]] =
     Order.by[Const[A, B], A](_.getConst)
 
-  implicit def constShow[A: Show, B]: Show[Const[A, B]] =
-    Show.show[Const[A, B]](c => s"Const(${Show[A].show(c.getConst)}})")
+  implicit def constShow[A, B](implicit A: Show[A]): Show[Const[A, B]] =
+    Show.show[Const[A, B]](c => s"Const(${A.show(c.getConst)}})")
 
   implicit def constTraverse[C]: Traverse[Const[C, ?]] = new Traverse[Const[C, ?]] {
     def traverse[G[_]: Applicative, A, B](fa: Const[C, A])(f: A => G[B]): G[Const[C, B]] =
@@ -32,28 +32,28 @@ sealed abstract class ConstInstances extends ConstInstances0 {
     def foldRight[A, B](fa: Const[C, A], b: Lazy[B])(f: (A, Lazy[B]) => B): Lazy[B] = b
   }
 
-  implicit def monoidConst[A: Monoid, B]: Monoid[Const[A, B]] = new Monoid[Const[A, B]]{
-    def empty: Const[A, B] = Const[A, B](Monoid[A].empty)
+  implicit def monoidConst[A, B](implicit A: Monoid[A]): Monoid[Const[A, B]] = new Monoid[Const[A, B]]{
+    def empty: Const[A, B] = Const(A.empty)
 
     def combine(x: Const[A, B], y: Const[A, B]): Const[A, B] =
-      Const[A, B](Monoid[A].combine(x.getConst, y.getConst))
+      Const(A.combine(x.getConst, y.getConst))
   }
 }
 
 sealed abstract class ConstInstances0 extends ConstInstances1 {
-  implicit def constApplicative[C: Monoid]: Applicative[Const[C, ?]] = new Applicative[Const[C, ?]] {
+  implicit def constApplicative[C](implicit C: Monoid[C]): Applicative[Const[C, ?]] = new Applicative[Const[C, ?]] {
     def pure[A](x: A): Const[C, A] =
-      Const[C, A](Monoid[C].empty)
+      Const(C.empty)
 
     def apply[A, B](fa: Const[C, A])(f: Const[C, A => B]): Const[C, B] =
-      fa.retag[B]
+      Const(C.combine(fa.getConst, f.getConst))
   }
 }
 
 sealed abstract class ConstInstances1 {
-  implicit def constApply[C: Semigroup]: Apply[Const[C, ?]] = new Apply[Const[C, ?]] {
+  implicit def constApply[C](implicit C: Semigroup[C]): Apply[Const[C, ?]] = new Apply[Const[C, ?]] {
     def apply[A, B](fa: Const[C, A])(f: Const[C, A => B]): Const[C, B] =
-      fa.retag[B]
+      Const(C.combine(fa.getConst, f.getConst))
 
     def map[A, B](fa: Const[C, A])(f: A => B): Const[C, B] =
       fa.retag[B]
