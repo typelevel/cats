658f218c7ea8217c6c35a4a4d75c2c304c2c98cc
Julien Truffaut <truffaut.julien@gmail.com>
2015-02-02 22:48:59 +0100

    Implement typeclass methods on Const

--- data/src/main/scala/cats/data/Const.scala
+++ data/src/main/scala/cats/data/Const.scala
@@ -1,6 +1,6 @@
 package cats.data
 
-import algebra.{Monoid, Order, Semigroup}
+import algebra._
 import cats.{Applicative, Apply, Lazy, Show, Traverse}
 
 /**
@@ -10,6 +10,18 @@ import cats.{Applicative, Apply, Lazy, Show, Traverse}
 final case class Const[A, B](getConst: A) {
   def retag[C]: Const[A, C] =
     this.asInstanceOf[Const[A, C]]
+
+  def combine(that: Const[A, B])(implicit A: Semigroup[A]): Const[A, B] =
+    Const(A.combine(getConst, that.getConst))
+
+  def traverse[F[_], C](f: B => F[C])(implicit F: Applicative[F]): F[Const[A, C]] =
+    F.pure(retag[C])
+
+  def ===(that: Const[A, B])(implicit A: Eq[A]): Boolean =
+    A.eqv(getConst, that.getConst)
+
+  def show(implicit A: Show[A]): String =
+    s"Const(${A.show(getConst)}})"
 }
 
 object Const extends ConstInstances
@@ -18,12 +30,12 @@ sealed abstract class ConstInstances extends ConstInstances0 {
   implicit def constOrder[A: Order, B]: Order[Const[A, B]] =
     Order.by[Const[A, B], A](_.getConst)
 
-  implicit def constShow[A, B](implicit A: Show[A]): Show[Const[A, B]] =
-    Show.show[Const[A, B]](c => s"Const(${A.show(c.getConst)}})")
+  implicit def constShow[A: Show, B]: Show[Const[A, B]] =
+    Show.show[Const[A, B]](_.show)
 
   implicit def constTraverse[C]: Traverse[Const[C, ?]] = new Traverse[Const[C, ?]] {
     def traverse[G[_]: Applicative, A, B](fa: Const[C, A])(f: A => G[B]): G[Const[C, B]] =
-      Applicative[G].pure(fa.retag[B])
+      fa.traverse(f)
 
     def foldLeft[A, B](fa: Const[C, A], b: B)(f: (B, A) => B): B = b
 
@@ -36,24 +48,30 @@ sealed abstract class ConstInstances extends ConstInstances0 {
     def empty: Const[A, B] = Const(A.empty)
 
     def combine(x: Const[A, B], y: Const[A, B]): Const[A, B] =
-      Const(A.combine(x.getConst, y.getConst))
+      x combine y
   }
 }
 
 sealed abstract class ConstInstances0 extends ConstInstances1 {
+  implicit def constPartialOrder[A: PartialOrder, B]: PartialOrder[Const[A, B]] =
+    PartialOrder.by[Const[A, B], A](_.getConst)
+
   implicit def constApplicative[C](implicit C: Monoid[C]): Applicative[Const[C, ?]] = new Applicative[Const[C, ?]] {
     def pure[A](x: A): Const[C, A] =
       Const(C.empty)
 
     def apply[A, B](fa: Const[C, A])(f: Const[C, A => B]): Const[C, B] =
-      Const(C.combine(fa.getConst, f.getConst))
+      fa.retag[B] combine f.retag[B]
   }
 }
 
 sealed abstract class ConstInstances1 {
+  implicit def constEq[A: Eq, B]: Eq[Const[A, B]] =
+    Eq.by[Const[A, B], A](_.getConst)
+
   implicit def constApply[C](implicit C: Semigroup[C]): Apply[Const[C, ?]] = new Apply[Const[C, ?]] {
     def apply[A, B](fa: Const[C, A])(f: Const[C, A => B]): Const[C, B] =
-      Const(C.combine(fa.getConst, f.getConst))
+      fa.retag[B] combine f.retag[B]
 
     def map[A, B](fa: Const[C, A])(f: A => B): Const[C, B] =
       fa.retag[B]
