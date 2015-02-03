fb3f8b3981b90d6c96efa97efe002089a405e1d7
Pascal Voitot <pascal.voitot.dev@gmail.com>
2015-02-04 00:27:19 +0100

    renamed stringInstance to stringShow

--- std/src/main/scala/cats/std/string.scala
+++ std/src/main/scala/cats/std/string.scala
@@ -3,6 +3,6 @@ package std
 
 trait StringInstances extends algebra.std.StringInstances {
 
-  implicit val stringInstance: Show[String] = Show.show(identity)
+  implicit val stringShow: Show[String] = Show.show(identity)
 
 }
