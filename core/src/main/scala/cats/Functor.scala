53617ce38128a9aafd1c2b5d5fc682b1e0e551e4
Mike (stew) O'Connor <stew@vireo.org>
2015-02-04 00:11:01 -0800

    fix the style of my comments, because non hates me

--- core/src/main/scala/cats/Functor.scala
+++ core/src/main/scala/cats/Functor.scala
@@ -19,22 +19,21 @@ import simulacrum._
 
   def imap[A, B](fa: F[A])(f: A => B, fi: B => A): F[B] = map(fa)(f)
 
-  /////////////////////////////////////////////////////////////////////////
   // derived functions
 
   /**
-    * Lift a function f to operate on Functors
-    */
+   * Lift a function f to operate on Functors
+   */
   def lift[A,B](f: A => B): F[A] => F[B] = map(_)(f)
 
   /**
-    * empty the fa of the values, preserving the structure
-    */
+   * empty the fa of the values, preserving the structure
+   */
   def void[A](fa: F[A]): F[Unit] = map(fa)(_ => ())
 
   /**
-    * Tuple the values in fa with the result of applying a function
-    * with the value */
+   * Tuple the values in fa with the result of applying a function
+   * with the value */
   def fproduct[A,B](fa: F[A])(f: A => B): F[(A,B)] = map(fa)(a => a -> f(a))
 
   /** Replaces the `A` value in `F[A]` with the supplied value. */
@@ -42,10 +41,10 @@ import simulacrum._
     map(fa)(_ => b)
 
   /**
-    * Compose this functor F with a functor G to produce a composite
-    * Functor on G[F[_]], with a map method which uses an A => B to
-    * map a G[F[A]] to a G[F[B]].
-    */
+   * Compose this functor F with a functor G to produce a composite
+   * Functor on G[F[_]], with a map method which uses an A => B to
+   * map a G[F[A]] to a G[F[B]].
+   */
   def compose[G[_]](implicit GG: Functor[G]): Functor[λ[α => F[G[α]]]] = new CompositeFunctor[F,G] {
     implicit def F: Functor[F] = self
     implicit def G: Functor[G] = GG
