e998ae5def9e0f264a84c3ed6777a8d71370277d
Pascal Voitot <pascal.voitot.dev@gmail.com>
2015-02-04 21:57:49 +0100

    replaced by real Show instance

--- std/src/main/scala/cats/std/string.scala
+++ std/src/main/scala/cats/std/string.scala
@@ -3,6 +3,8 @@ package std
 
 trait StringInstances extends algebra.std.StringInstances {
 
-  implicit val stringShow: Show[String] = Show.show(identity)
+  implicit val stringShow: Show[String] = new Show[String] {
+    def show(s: String): String = s
+  }
 
 }
