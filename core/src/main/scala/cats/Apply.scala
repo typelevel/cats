4169bc179a3f2e95bcce6a8f839eff5929db353b
Adelbert Chang <adelbertc@gmail.com>
2015-02-05 21:04:08 -0800

    Add more comments to Apply, closes #60

--- core/src/main/scala/cats/Apply.scala
+++ core/src/main/scala/cats/Apply.scala
@@ -9,14 +9,29 @@ import simulacrum._
  *  - apply(apply(fa)(fab))(fbc) = apply(fa)(apply(fab)(map(fbc)(bc => ab => ab andThen bc)))
  */
 @typeclass trait Apply[F[_]] extends Functor[F] { self =>
+  /** Given a value and a function in the Apply context, applies the function to the value. */
   def apply[A, B](fa: F[A])(f: F[A => B]): F[B]
 
+  /** apply2 is a binary version of apply, itself defined in terms of apply */
   def apply2[A, B, Z](fa: F[A], fb: F[B])(f: F[(A, B) => Z]): F[Z] =
     apply(fa)(apply(fb)(map(f)(ff => (b: B) => (a: A) => ff(a, b))))
 
+  /**
+   * Applies the pure (binary) function f to the effectful values fa and fb.
+   *
+   * map2 can be seen as a binary version of [[cats.Functor]]#map.
+   */
   def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] =
     apply(fa)(map(fb)(b => (a: A) => f(a, b)))
 
+  /**
+   * Two sequentially dependent Applys can be composed.
+   *
+   * The composition of Applys `F` and `G`, `F[G[x]]`, is also an Apply.
+   *
+   * Apply[Option].compose[List].map2(Some(List(1, 2)), Some(List(3)))(_ + _)
+   * = Some(List(4, 5))
+   */
   def compose[G[_]](implicit GG: Apply[G]): Apply[({type λ[α] = F[G[α]]})#λ] =
     new CompositeApply[F,G] {
       implicit def F: Apply[F] = self
