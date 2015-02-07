e87dcbe78a63a9f68e5f650e841a73e0d704985a
Erik Osheim <d_m@plastic-idolatry.com>
2015-02-07 16:56:13 -0500

    Add some initial documentation for univeral algebras.

--- core/src/main/scala/cats/MonoidK.scala
+++ core/src/main/scala/cats/MonoidK.scala
@@ -2,9 +2,36 @@ package cats
 
 import simulacrum._
 
+/**
+ * MonoidK is a universal monoid which operates on kinds.
+ * 
+ * This type class is useful when its type parameter F[_] has a
+ * structure that can be combined for any particular type, and which
+ * also has an "empty" representation. Thus, MonoidK is like a Monoid
+ * for kinds (i.e. parameterized types).
+ * 
+ * A MonoidK[F] can produce a Monoid[F[A]] for any type A.
+ * 
+ * Here's how to distinguish Monoid and MonoidK:
+ * 
+ *  - Monoid[A] allows A values to be combined, and also means there
+ *    is an "empty" A value that functions as an identity.
+ * 
+ *  - Monoid[F] allows two F[A] values to be combined, for any A.  It
+ *    also means that for any A, there is an "empty" F[A] value. The
+ *    combination operation and empty value just depend on the
+ *    structure of F, but not on the structure of A.
+ */
 @typeclass trait MonoidK[F[_]] extends SemigroupK[F] { self =>
+
+  /**
+   * Given a type A, create an "empty" F[A] value.
+   */
   def empty[A]: F[A]
 
+  /**
+   * Given a type A, create a concrete Semigroup[F[A]].
+   */
   override def algebra[A]: Monoid[F[A]] =
     new Monoid[F[A]] {
       def empty: F[A] = self.empty
--- core/src/main/scala/cats/SemigroupK.scala
+++ core/src/main/scala/cats/SemigroupK.scala
@@ -2,14 +2,42 @@ package cats
 
 import simulacrum._
 
+/**
+ * SemigroupK is a universal semigroup which operates on kinds.
+ * 
+ * This type class is useful when its type parameter F[_] has a
+ * structure that can be combined for any particular type. Thus,
+ * SemigroupK is like a Semigroup for kinds (i.e. parameterized
+ * types).
+ * 
+ * A Semigroup[F] can produce a Semigroup[F[A]] for any type A.
+ * 
+ * Here's how to distinguish Semigroup and SemigroupK:
+ * 
+ *  - Semigroup[A] allows two A values to be combined.
+ * 
+ *  - Semigroup[F] allows two F[A] values to be combined, for any A.
+ *    The combination operation just depends on the structure of F,
+ *    but not the structure of A.
+ */
 @typeclass trait SemigroupK[F[_]] { self =>
+
+  /**
+   * Combine two F[A] values.
+   */
   def combine[A](x: F[A], y: F[A]): F[A]
 
+  /**
+   * Compose two SemigroupK intsances.
+   */
   def composedWith[G[_]: SemigroupK]: SemigroupK[λ[α => F[G[α]]]] =
     new SemigroupK[λ[α => F[G[α]]]] {
       def combine[A](x: F[G[A]], y: F[G[A]]): F[G[A]] = combine(x, y)
     }
 
+  /**
+   * Given a type A, create a concrete Semigroup[F[A]].
+   */
   def algebra[A]: Semigroup[F[A]] =
     new Semigroup[F[A]] {
       def combine(x: F[A], y: F[A]): F[A] = self.combine(x, y)
