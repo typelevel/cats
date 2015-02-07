f29d0a56fccc2578c63d087809a37cae7dd3c9b2
Erik Osheim <d_m@plastic-idolatry.com>
2015-02-07 17:07:05 -0500

    remove trailing whitespace

--- core/src/main/scala/cats/SemigroupK.scala
+++ core/src/main/scala/cats/SemigroupK.scala
@@ -4,18 +4,18 @@ import simulacrum._
 
 /**
  * SemigroupK is a universal semigroup which operates on kinds.
- * 
+ *
  * This type class is useful when its type parameter F[_] has a
  * structure that can be combined for any particular type. Thus,
  * SemigroupK is like a Semigroup for kinds (i.e. parameterized
  * types).
- * 
+ *
  * A SemigroupK[F] can produce a Semigroup[F[A]] for any type A.
- * 
+ *
  * Here's how to distinguish Semigroup and SemigroupK:
- * 
+ *
  *  - Semigroup[A] allows two A values to be combined.
- * 
+ *
  *  - SemigroupK[F] allows two F[A] values to be combined, for any A.
  *    The combination operation just depends on the structure of F,
  *    but not the structure of A.
