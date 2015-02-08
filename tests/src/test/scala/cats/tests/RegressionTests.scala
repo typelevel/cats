473e9ad3eccf3d370d259f7240dcd6e88e2cea24
Erik Osheim <d_m@plastic-idolatry.com>
2015-02-08 18:54:50 -0500

    Fix order-related error, add List[Option[Int]] case.

--- tests/src/test/scala/cats/tests/RegressionTests.scala
+++ tests/src/test/scala/cats/tests/RegressionTests.scala
@@ -6,25 +6,8 @@ import org.scalatest.FunSuite
 
 class RegressionTests extends FunSuite {
 
-  // test sequence order using a contrived, unsafe state monad.
-  // 
-
-  test("#140: confirm sequence order") {
-    val allocated = List("Alice", "Bob", "Claire").map(alloc)
-    val state = Traverse[List].sequence[State[Int, ?],Person](allocated)
-    val (people, counter) = state.run(0)
-    assert(people == List(Person(0, "Alice"), Person(1, "Bob"), Person(2, "Claire")))
-    assert(counter == 3)
-  }
-
-  case class Person(id: Int, name: String)
-
-  def alloc(name: String): State[Int, Person] =
-    State(id => (Person(id, name), id + 1))
-
   // toy state class
   // not stack safe, very minimal, not for actual use
-
   case class State[S, A](run: S => (A, S)) { self =>
     def map[B](f: A => B) =
       State[S, B]({ s => val (a, s2) = self.run(s); (f(a), s2) })
@@ -37,5 +20,24 @@ class RegressionTests extends FunSuite {
       def pure[A](a: A) = State[S, A](s => (a, s))
       def flatMap[A, B](sa: State[S, A])(f: A => State[S, B]) = sa.flatMap(f)
     }
+  }
+
+  case class Person(id: Int, name: String)
+
+  def alloc(name: String): State[Int, Person] =
+    State(id => (Person(id, name), id + 1))
+
+  test("#140: confirm sequence order") {
+
+    // test result order
+    val ons = List(Option(1), Option(2), Option(3))
+    assert(Traverse[List].sequence(ons) == Some(List(1, 2, 3)))
+
+    // test order of effects using a contrived, unsafe state monad.
+    val allocated = List("Alice", "Bob", "Claire").map(alloc)
+    val state = Traverse[List].sequence[State[Int, ?],Person](allocated)
+    val (people, counter) = state.run(0)
+    assert(people == List(Person(0, "Alice"), Person(1, "Bob"), Person(2, "Claire")))
+    assert(counter == 3)
   }
 }
