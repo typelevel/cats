8ac539309484a671a2cf6444fa9ea1f3d7359f72
Frank S. Thomas <frank@timepit.eu>
2015-02-09 18:50:55 +0100

    Add ContravariantLaws

--- core/src/main/scala/cats/functor/Contravariant.scala
+++ core/src/main/scala/cats/functor/Contravariant.scala
@@ -3,6 +3,9 @@ package functor
 
 import simulacrum._
 
+/**
+ * Must obey the laws defined in [[laws.ContravariantLaws]].
+ */
 @typeclass trait Contravariant[F[_]] extends Invariant[F] {
   def contramap[A, B](fa: F[A])(f: B => A): F[B]
   override def imap[A, B](fa: F[A])(f: A => B)(fi: B => A): F[B] = contramap(fa)(fi)
