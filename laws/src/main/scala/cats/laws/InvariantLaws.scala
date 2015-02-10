535166f17776171e66a0214de75012252b1df660
Colt Frederickson <coltfred@gmail.com>
2015-02-10 09:12:39 -0700

    Update InvariantLaws.scala

--- laws/src/main/scala/cats/laws/InvariantLaws.scala
+++ laws/src/main/scala/cats/laws/InvariantLaws.scala
@@ -12,5 +12,4 @@ class InvariantLaws[F[_]](implicit F: Invariant[F]) {
 
   def invariantComposition[A, B, C](fa: F[A], f1: A => B, f2: B => A, g1: B => C, g2: C => B): (F[C], F[C]) =
     fa.imap(f1)(f2).imap(g1)(g2) -> fa.imap(f1 andThen g1)(g2 andThen f2)
-
 }
