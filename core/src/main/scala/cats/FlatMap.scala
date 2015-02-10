b9931367f8bfad6eca57a61adf4659320b8fd622
wedens <wedens@eldis.ru>
2015-02-10 22:15:38 +0600

    remove memoization in ifM

--- core/src/main/scala/cats/FlatMap.scala
+++ core/src/main/scala/cats/FlatMap.scala
@@ -36,9 +36,7 @@ trait FlatMap[F[_]] extends Apply[F] {
    * `if` lifted into monad.
    */
   def ifM[B](fa: F[Boolean])(ifTrue: => F[B], ifFalse: => F[B]): F[B] = {
-    lazy val t = ifTrue
-    lazy val f = ifFalse
-    flatMap(fa)(if (_) t else f)
+    flatMap(fa)(if (_) ifTrue else ifFalse)
   }
 }
 
