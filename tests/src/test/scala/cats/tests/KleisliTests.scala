5fa23b237ecaeea1a8378ce7cb96ee5750687c4b
Julien Truffaut <truffaut.julien@gmail.com>
2015-02-10 21:46:52 +0100

    add approximation of Kleisli Eq and add test for Kleisli Functor law

--- tests/src/test/scala/cats/tests/KleisliTests.scala
+++ tests/src/test/scala/cats/tests/KleisliTests.scala
@@ -1,21 +1,28 @@
 package cats.tests
 
-import cats.Functor
+import algebra.Eq
 import cats.data.Kleisli
+import cats.laws.FunctorLaws
+import org.scalacheck.Arbitrary
 import org.scalatest.FunSuite
+import org.typelevel.discipline.scalatest.Discipline
+import cats.Traverse
+import cats.std.list._
+import cats.std.option._
+import cats.std.int._
 
 
-class KleisliTests extends FunSuite {
+class KleisliTests extends FunSuite with Discipline {
 
-  /** MyId id is a dummy case class used to test implicit resolution */
-  case class MyId[A](runId: A)
+  // create an approximation of Eq[Kleisli[F, A, B]] by generating 100 values for A
+  // and compare the result of applying these A to two kleisli
+  implicit def kleisliEq[F[_], A, B](implicit A: Arbitrary[A], FB: Eq[F[B]]): Eq[Kleisli[F, A, B]] = new Eq[Kleisli[F, A, B]] {
+    def eqv(x: Kleisli[F, A, B], y: Kleisli[F, A, B]): Boolean =
+      Traverse[List].traverse(List.fill(100)(0))(_ => A.arbitrary.sample).map( samples =>
+        samples.forall(s => FB.eqv(x.runKleisli(s), y.runKleisli(s)) )
+      ).getOrElse(sys.error("Could not generate 100 values"))
 
-  test("Kleisli is a Functor if F is a Functor") {
-    implicit val myIdFunctor: Functor[MyId] = new Functor[MyId]{
-      def map[A, B](fa: MyId[A])(f: A => B): MyId[B] = MyId(f(fa.runId))
-    }
-
-    val plus1Times2: Kleisli[MyId, Int, Int] = Functor[Kleisli[MyId, Int, ?]].map(Kleisli((i: Int) => MyId(1 + i)))(_ * 2)
-    assert(plus1Times2.runKleisli(1) == MyId(4))
   }
+
+  checkAll("Kleisli[Option,Int, Int]", FunctorLaws[Kleisli[Option, Int, ?], Int].applicative[Int, Int])
 }
