d8901bb058310bda84776ebddb15b79afb08e810
Erik Osheim <d_m@plastic-idolatry.com>
2015-02-10 21:55:46 -0500

    Documentation updates for Foldable.
    There are also a few small changes to make the code a bit
    more readable, but nothing major. At this point I think
    the documentation for Foldable is about as good as I can
    make it.

--- core/src/main/scala/cats/Foldable.scala
+++ core/src/main/scala/cats/Foldable.scala
@@ -5,6 +5,20 @@ import simulacrum._
 /**
  * Data structures that can be folded to a summary value.
  *
+ * In the case of a collection (such as `List` or `Set`), these
+ * methods will fold together (combine) the values contained in the
+ * collection to produce a single result. Most collection types have
+ * `foldLeft` methods, which will usually be used by the associationed
+ * `Fold[_]` instance.
+ *
+ * Foldable[F] is implemented in terms of two basic methods:
+ *
+ *  - `foldLeft(fa, b)(f)` eagerly folds `fa` from left-to-right.
+ *  - `foldLazy(fa, lb)(f)` lazily folds `fa` from right-to-left.
+ *
+ * Beyond these it provides many other useful methods related to
+ * folding over F[A] values.
+ *
  * See: [[https://www.cs.nott.ac.uk/~gmh/fold.pdf A tutorial on the universality and expressiveness of fold]]
  */
 @typeclass trait Foldable[F[_]] { self =>
@@ -28,6 +42,10 @@ import simulacrum._
 
   /**
    * Right associative fold on 'F' using the function 'f'.
+   *
+   * The default implementation is written in terms of
+   * `foldLazy`. Most instances will want to override this method for
+   * performance reasons.
    */
   def foldRight[A, B](fa: F[A], b: B)(f: (A, B) => B): B =
     foldLazy(fa, Lazy.eager(b)) { a =>
@@ -35,15 +53,7 @@ import simulacrum._
     }.force
 
   /**
-   * Apply f to each element of F and combine them using the Monoid[B].
-   */
-  def foldMap[A, B](fa: F[A])(f: A => B)(implicit B: Monoid[B]): B =
-    foldLeft(fa, B.empty) { (b, a) =>
-      B.combine(b, f(a))
-    }
-
-  /**
-   * Fold up F using the Monoid[A]
+   * Fold implemented using the given Monoid[A] instance.
    */
   def fold[A](fa: F[A])(implicit A: Monoid[A]): A =
     foldLeft(fa, A.empty) { (acc, a) =>
@@ -51,32 +61,79 @@ import simulacrum._
     }
 
   /**
-   * Traverse F in the Applicative G and ignore the return values of 'f'.
+   * Fold implemented by mapping `A` values into `B` and then
+   * combining them using the given `Monoid[B]` instance.
    */
-  def traverse_[G[_]: Applicative, A, B](fa: F[A])(f: A => G[B]): G[Unit] =
-    foldLeft(fa, Applicative[G].pure(())) { (acc, a) =>
-      Applicative[G].map2(acc, f(a)) { (_, _) => () }
+  def foldMap[A, B](fa: F[A])(f: A => B)(implicit B: Monoid[B]): B =
+    foldLeft(fa, B.empty) { (b, a) =>
+      B.combine(b, f(a))
     }
 
   /**
-   * Traverse F in the Applicative G ignoring all values in fga.
+   * Traverse `F[A]` using `Applicative[G]`.
+   *
+   * `A` values will be mapped into `G[B]` and combined using
+   * `Applicative#map2`.
+   *
+   * For example:
+   * {{{
+   *     def parseInt(s: String): Option[Int] = ...
+   *     val F = Foldable[List]
+   *     F.traverse_(List("333", "444"))(parseInt) // Some(())
+   *     F.traverse_(List("333", "zzz"))(parseInt) // None
+   * }}}
+   *
+   * This method is primarily useful when `G[_]` represents an action
+   * or effect, and the specific `A` aspect of `G[A]` is not otherwise
+   * needed.
+   */
+  def traverse_[G[_], A, B](fa: F[A])(f: A => G[B])(implicit G: Applicative[G]): G[Unit] =
+    foldLeft(fa, G.pure(())) { (acc, a) =>
+      G.map2(acc, f(a)) { (_, _) => () }
+    }
+
+  /**
+   * Sequence `F[G[A]]` using `Applicative[G]`.
+   *
+   * This is similar to `traverse_` except it operates on `F[G[A]]`
+   * values, so no additional functions are needed.
+   *
+   * For example:
+   *
+   * {{{
+   *     val F = Foldable[List]
+   *     F.sequence_(List(Option(1), Option(2), Option(3))) // Some(())
+   *     F.sequence_(List(Option(1), None, Option(3)))      // None
+   * }}}
    */
   def sequence_[G[_]: Applicative, A, B](fga: F[G[A]]): G[Unit] =
     traverse_(fga)(identity)
 
   /**
-   * Fold up F using the MonoidK instance for G. Like fold, but the value is of kind * -> *.
+   * Fold implemented using the given `MonoidK[G]` instance.
+   *
+   * This method is identical to fold, except that we use the universal monoid (`MonoidK[G]`)
+   * to get a `Monoid[G[A]]` instance.
+   *
+   * For example:
+   *
+   * {{{
+   *     val F = Foldable[List]
+   *     F.foldK(List(1 :: 2 :: Nil, 3 :: 4 :: 5 :: Nil))
+   *     // List(1, 2, 3, 4, 5)
+   * }}}
    */
   def foldK[G[_], A](fga: F[G[A]])(implicit G: MonoidK[G]): G[A] =
     fold(fga)(G.algebra)
 
   /**
-   * Compose this foldable instance with one for G creating Foldable[F[G]]
+   * Compose this `Foldable[F]` with a `Foldable[G]` to create
+   * a `Foldable[F[G]]` instance.
    */
-  def compose[G[_]](implicit GG: Foldable[G]): Foldable[λ[α => F[G[α]]]] =
+  def compose[G[_]](implicit G0: Foldable[G]): Foldable[λ[α => F[G[α]]]] =
     new CompositeFoldable[F, G] {
       implicit def F: Foldable[F] = self
-      implicit def G: Foldable[G] = GG
+      implicit def G: Foldable[G] = G0
     }
 }
 
@@ -104,6 +161,8 @@ trait CompositeFoldable[F[_], G[_]] extends Foldable[λ[α => F[G[α]]]] {
    */
   def foldLazy[A, B](fa: F[G[A]], b: Lazy[B])(f: A => Fold[B]): Lazy[B] =
     F.foldLazy(fa, b) { ga =>
-      Fold.Continue(b => G.foldLazy(ga, Lazy.eager(b))(a => f(a)).force)
+      Fold.Continue { b =>
+        G.foldLazy(ga, Lazy.eager(b))(f).force
+      }
     }
 }
