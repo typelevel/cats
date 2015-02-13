effe82bea71f03b267d0c0efe0933e521568a3ea
Pascal Voitot <pascal.voitot.dev@gmail.com>
2015-02-13 08:35:15 +0100

    cleaned freeapplicative code

--- core/src/main/scala/cats/free/FreeApplicative.scala
+++ core/src/main/scala/cats/free/FreeApplicative.scala
@@ -2,82 +2,70 @@ package cats
 package free
 
 /** Applicative Functor for Free */
-sealed abstract class FreeApplicative[F[_], A] {
-  self =>
+sealed abstract class FreeApplicative[F[_], A] { self =>
 
-  import FreeApplicative._
+  import FreeApplicative.{FA, Pure, Ap}
 
-  final def ap[B](b: FreeApplicative[F, A => B]): FreeApplicative[F, B] = b match {
-
-    case Pure(f) => this map f
-
-    case x@Ap() => new Ap[F, B] {
-      type Pivot = x.Pivot
-      val pivot = x.pivot
-      val fn: FreeApplicative[F, x.Pivot => B] = self.ap(x.fn map { fx: (x.Pivot => A => B) => (a:A) => (p: x.Pivot) => fx(p)(a) })
+  final def ap[B](b: FA[F, A => B]): FA[F, B] =
+    b match {
+      case Pure(f) =>
+        this.map(f)
+      case x: Ap[F, A => B] =>
+        Ap(x.pivot)(self.ap(x.fn.map(fx => a => p => fx(p)(a))))
     }
-  }
 
-  final def map[B](f: A => B): FreeApplicative[F, B] = this match {
-
-    case Pure(a) => Pure(f(a))
-
-    case x@Ap() => new Ap[F, B] {
-      type Pivot = x.Pivot
-      val pivot = x.pivot
-      val fn = x.fn map (f compose _)
+  final def map[B](f: A => B): FA[F, B] =
+    this match {
+      case Pure(a) => Pure(f(a))
+      case x: Ap[F, A] => Ap(x.pivot)(x.fn.map(f compose _))
     }
-  }
 
   /** Natural Transformation of FreeApplicative based on given Natural Transformation */
-  final def hoist[G[_]](f: F ~> G): FreeApplicative[G, A] = this match {
-
-    case Pure(a) => Pure[G, A](a)
-
-    case x@Ap() => new Ap[G, A] {
-      type Pivot = x.Pivot
-      val pivot = f(x.pivot)
-      val fn = x.fn.hoist(f)
-    }
+  final def hoist[G[_]](f: F ~> G): FA[G, A] =
+    this match {
+      case Pure(a) => Pure[G, A](a)
+      case x: Ap[F, A] => Ap(f(x.pivot))(x.fn.hoist(f))
   }
 
   /** Interpretes/Runs the sequence of operations using the semantics of Applicative G
     * Tail recursive only if G provides tail recursive interpretation (ie G is FreeMonad)
     */
-  final def run[G[_]](f: F ~> G)(implicit G: Applicative[G]): G[A] = this match {
-
-    case Pure(a) => G.pure(a)
-
-    case x@Ap() => G.apply(f(x.pivot))(x.fn.run(f))
-  }
-
-
+  final def run[G[_]](f: F ~> G)(implicit G: Applicative[G]): G[A] =
+    this match {
+      case Pure(a) => G.pure(a)
+      case x: Ap[F, A] => G.apply(f(x.pivot))(x.fn.run(f))
+    }
 }
 
 object FreeApplicative {
 
-  def pure[F[_], A](a: A) = Pure[F, A](a)
+  type FA[F[_], A] = FreeApplicative[F, A]
 
-  def lift[F[_], A](fa: F[A]) = new Ap[F, A] {
-    type Pivot = A
-    val pivot = fa
-    val fn = pure[F, A => A]((a:A) => a)
-  }
+  final case class Pure[F[_], A](a: A) extends FA[F, A]
 
-  case class Pure[F[_], A](a: A) extends FreeApplicative[F, A]
-
-  abstract case class Ap[F[_], A]() extends FreeApplicative[F, A] {
+  abstract class Ap[F[_], A] extends FA[F, A] {
     type Pivot
-
     val pivot: F[Pivot]
-    val fn: FreeApplicative[F, Pivot => A]
+    val fn: FA[F, Pivot => A]
   }
 
-  implicit def theFreeApplicative[S[_]] = new Applicative[FreeApplicative[S, ?]] {
+  final def pure[F[_], A](a: A): FA[F, A] =
+    Pure(a)
 
-    def apply[A, B](fa: FreeApplicative[S, A])(f: FreeApplicative[S, A => B]): FreeApplicative[S, B] = fa.ap(f)
+  final def Ap[F[_], P, A](fp: F[P])(f: FA[F, P => A]): FA[F, A] =
+    new Ap[F, A] {
+      type Pivot = P
+      val pivot: F[Pivot] = fp
+      val fn: FA[F, Pivot => A] = f
+    }
 
-    def pure[A](a: A): FreeApplicative[S, A] = FreeApplicative.pure(a)
+  final def lift[F[_], A](fa: F[A]): FA[F, A] =
+    Ap(fa)(Pure(a => a))
 
+  implicit final def freeApplicative[S[_]]: Applicative[FA[S, ?]] = {
+    new Applicative[FA[S, ?]] {
+      def apply[A, B](fa: FA[S, A])(f: FA[S, A => B]): FA[S, B] = fa.ap(f)
+      def pure[A](a: A): FA[S, A] = Pure(a)
+    }
   }
 }
---
+++ examples/src/main/scala/cats/free/FreeApplicativeSamples.scala
@@ -0,0 +1,5 @@
+package cats
+package free
+package samples
+
+
