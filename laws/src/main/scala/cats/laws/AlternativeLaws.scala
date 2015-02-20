35e08ffba5e189e0a54e234b1637083ea44846a5
Stephen Judkins <stephen.judkins@gmail.com>
2015-02-20 14:24:59 -0800

    Syntax tweaks

--- laws/src/main/scala/cats/laws/AlternativeLaws.scala
+++ laws/src/main/scala/cats/laws/AlternativeLaws.scala
@@ -6,16 +6,16 @@ import cats.syntax.all._
 
 trait AlternativeLaws[F[_]] extends ApplicativeLaws[F] with MonoidKLaws[F] {
   implicit override def F: Alternative[F]
+  implicit def algebra[A] = F.algebra[A]
 
   def rightAbsorption[A, B](ff: F[A => B]): IsEq[F[B]] =
-    F.empty[A].apply(ff) <-> F.empty[B]
+    (F.empty[A] apply ff) <-> F.empty[B]
 
   def leftDistributivity[A, B](fa: F[A], fa2: F[A], f: A => B): IsEq[F[B]] =
-    F.combine(fa, fa2).map(f) <-> F.combine(fa map f, fa2 map f)
+    ((fa |+| fa2) map f) <-> ((fa map f) |+| (fa2 map f))
 
   def rightDistributivity[A, B](fa: F[A], ff: F[A => B], fg: F[A => B]): IsEq[F[B]] =
-    fa.apply(F.combine(ff,fg)) <-> F.combine(fa.apply(ff), fa.apply(fg))
-
+    (fa apply (ff |+| fg)) <-> ((fa apply ff) |+| (fa apply fg))
 
 }
 
