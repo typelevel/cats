5838b57bd6515cf7543486ef7f6dc5e9963f248a
Mike (stew) O'Connor <stew@vireo.org>
2015-02-19 23:03:44 -0800

    add leftMap / rightMap and some scaladoc to bifunctor

--- core/src/main/scala/cats/functor/Bifunctor.scala
+++ core/src/main/scala/cats/functor/Bifunctor.scala
@@ -1,8 +1,28 @@
 package cats
 package functor
 
+/**
+ * A typeclass of types which give rise to two independent, covariant
+ * functors.
+ */
 trait Bifunctor[F[_, _]] { self =>
+
+  /**
+   * The quientessential method of the Bifunctor trait, it applies a
+   * function to each "side" of the bifunctor.
+   */
   def bimap[A, B, C, D](fab: F[A, B])(f: A => C, g: B => D): F[C, D]
+
+  // derived methods
+  /**
+   * apply a function to the "left" functor
+   */
+  def leftMap[A,B,C](fab: F[A, B])(f: A => C): F[C,B] = bimap(fab)(f, identity)
+
+  /**
+   * apply a function ro the "right" functor
+   */
+  def rightMap[A,B,C](fab: F[A, B])(f: B => C): F[A,C] = bimap(fab)(identity, f)
 }
 
 object Bifunctor {
