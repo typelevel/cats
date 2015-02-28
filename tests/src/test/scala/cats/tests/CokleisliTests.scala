0d7575a9ce71c00d865502ffcd64cc6f2d7044fb
Frank S. Thomas <frank@timepit.eu>
2015-02-28 22:27:19 +0100

    Check Profunctor laws for Kleisli and Cokleisli

--- tests/src/test/scala/cats/tests/CokleisliTests.scala
+++ tests/src/test/scala/cats/tests/CokleisliTests.scala
@@ -2,7 +2,8 @@ package cats.tests
 
 import algebra.Eq
 import cats.data.Cokleisli
-import cats.laws.discipline.ApplicativeTests
+import cats.laws.discipline._
+import cats.laws.discipline.arbitrary._
 import cats.laws.discipline.eq._
 import org.scalacheck.Arbitrary
 
@@ -11,5 +12,6 @@ class CokleisliTests extends CatsSuite {
   implicit def cokleisliEq[F[_], A, B](implicit A: Arbitrary[F[A]], FB: Eq[B]): Eq[Cokleisli[F, A, B]] =
     Eq.by[Cokleisli[F, A, B], F[A] => B](_.run)
 
-  checkAll("Cokleisli[Option,Int, Int]", ApplicativeTests[Cokleisli[Option, Int, ?]].applicative[Int, Int, Int])
+  checkAll("Cokleisli[Option, Int, Int]", ApplicativeTests[Cokleisli[Option, Int, ?]].applicative[Int, Int, Int])
+  checkAll("Cokleisli[Option, Int, Int]", ProfunctorTests[Cokleisli[Option, ?, ?]].profunctor[Int, Int, Int, Int, Int, Int])
 }
