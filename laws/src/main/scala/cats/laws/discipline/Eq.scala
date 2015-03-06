bc82454b62cc91ea132f6eded7d49da349073d04
Cody Allen <ceedubs@gmail.com>
2015-03-06 07:52:29 -0500

    Add Invariant[Semigroup] and Invariant[Monoid]

--- laws/src/main/scala/cats/laws/discipline/Eq.scala
+++ laws/src/main/scala/cats/laws/discipline/Eq.scala
@@ -1,12 +1,15 @@
-package cats.laws.discipline
+package cats
+package laws.discipline
 
 import algebra.Eq
 import org.scalacheck.Arbitrary
 
 object eq {
 
-  // create an approximation of Eq[A => B] by generating 100 values for A
-  // and compare the application of the two functions
+  /**
+   * Create an approximation of Eq[A => B] by generating 100 values for A
+   * and comparing the application of the two functions.
+   */
   implicit def function1Eq[A, B](implicit A: Arbitrary[A], B: Eq[B]): Eq[A => B] = new Eq[A => B] {
     def eqv(f: A => B, g: A => B): Boolean = {
       val samples = List.fill(100)(A.arbitrary.sample).collect{
@@ -22,5 +25,20 @@ object eq {
       def eqv(x: (A, B), y: (A, B)): Boolean =
         A.eqv(x._1, y._1) && B.eqv(x._2, y._2)
     }
+
+  /**
+   * Create an approximation of Eq[Semigroup[A]] by generating values for A
+   * and comparing the application of the two combine functions.
+   */
+  implicit def semigroupEq[A](implicit arbAA: Arbitrary[(A, A)], eqA: Eq[A]): Eq[Semigroup[A]] =
+    function1Eq[(A, A), A].on(f =>
+      Function.tupled((x, y) => f.combine(x, y))
+    )
+
+  implicit def monoidEq[A](implicit eqSA: Eq[Semigroup[A]], eqA: Eq[A]): Eq[Monoid[A]] = new Eq[Monoid[A]] {
+    def eqv(f: Monoid[A], g: Monoid[A]): Boolean = {
+      eqSA.eqv(f, g) && eqA.eqv(f.empty, g.empty)
+    }
+  }
 
 }
