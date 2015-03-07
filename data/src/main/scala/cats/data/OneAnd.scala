3b5bd94c15948fe383c1abb75fff52a38aa9c140
Mike (stew) O'Connor <stew@vireo.org>
2015-03-07 15:13:44 -0800

    add Foldable instance for OneAnd

--- data/src/main/scala/cats/data/OneAnd.scala
+++ data/src/main/scala/cats/data/OneAnd.scala
@@ -24,6 +24,21 @@ final case class OneAnd[A, F[_]](head: A, tail: F[A]) {
 
   def combine(other: OneAnd[A,F])(implicit monad: MonadCombine[F]): OneAnd[A,F] =
     OneAnd(head, monad.combine(tail, monad.combine(monad.pure(other.head), other.tail)))
+
+  def foldLeft[B](b: B)(f: (B, A) => B)(implicit foldable: Foldable[F]): B =
+    foldable.foldLeft(tail, f(b, head))(f)
+
+  def foldRight[B](b: B)(f: (A,B) => B)(implicit foldable: Foldable[F]): B =
+    f(head, foldable.foldRight(tail, b)(f))
+
+  def foldLazy[B](b: Lazy[B])(f: A => Fold[B])(implicit foldable: Foldable[F]): Lazy[B] = {
+    import Fold._
+    f(head) match {
+      case Return(b) => Lazy.eager(b)
+      case Continue(c) => foldable.foldLazy(tail, b)(f).map(c)
+      case _ => foldable.foldLazy(tail, b)(f)
+    }
+  }
 }
 
 trait OneAndInstances {
@@ -38,6 +53,15 @@ trait OneAndInstances {
 
   implicit def oneAndSemigroupK[F[_]](implicit monad: MonadCombine[F]): SemigroupK[OneAnd[?,F]] = new SemigroupK[OneAnd[?,F]] {
     def combine[A](a: OneAnd[A, F], b: OneAnd[A, F]) = a combine b
+  }
+
+  implicit def onAndFoldable[F[_]](implicit foldable: Foldable[F]): Foldable[OneAnd[?,F]] = new Foldable[OneAnd[?,F]] {
+    override def foldLeft[A,B](fa: OneAnd[A,F], b: B)(f: (B,A) => B): B =
+      fa.foldLeft(b)(f)
+    override def foldRight[A,B](fa: OneAnd[A,F], b: B)(f: (A,B) => B): B =
+      fa.foldRight(b)(f)
+    override def foldLazy[A,B](fa: OneAnd[A,F], b: Lazy[B])(f: A => Fold[B]): Lazy[B] =
+      fa.foldLazy(b)(f)
   }
 
   implicit def oneAndMonad[F[_]](implicit monad: MonadCombine[F]): Comonad[OneAnd[?, F]] with Monad[OneAnd[?, F]] = new Comonad[OneAnd[?, F]] with Monad[OneAnd[?, F]] {
