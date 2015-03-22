d5c57ff2f04ac5daf95b0a2c75a90fd6397c89db
Mike (stew) O'Connor <stew@vireo.org>
2015-03-22 00:27:12 -0700

    add some documentation for Unapply

--- core/src/main/scala/cats/Unapply.scala
+++ core/src/main/scala/cats/Unapply.scala
@@ -1,32 +1,46 @@
 package cats
 
+/**
+ * A typeclass that is used to help guide scala's type inference to
+ * find typeclass instances for types which have shapes which differ
+ * from what their typeclasses are looking for.
+ *
+ * For example, [[Functor]] is defined for types in the shape
+ * F[_]. Scala has no problem finding instance of Functor which match
+ * this shape, such as Functor[Option], Functor[List], etc. There is
+ * also a functor defined for some types which have the Shape F[_,_]
+ * when one of the two 'holes' is fixed. For example. there is a
+ * Functor for Map[A,?] for any A, and for Either[A,?] for any A,
+ * however the scala compiler will not find them without some coercing.
+ */
 trait Unapply[TC[_[_]], MA] {
+  // a type constructor which is properly kinded for the typeclass
   type M[_]
+  // the type applied to the type constructor to make an MA
   type A
 
+  // the actual typeclass instance found
   def TC: TC[M]
+
+  // a function which will coerce the MA value into one of type M[A]
+  // this will end up being the identity function, but we can't supply
+  // it until we have proven that MA and M[A] are the same type
   def subst: MA => M[A]
 }
 
 object Unapply {
+  // a convenience method for summoning Unapply instances
   def apply[TC[_[_]], MA](implicit ev: Unapply[TC,MA]): Unapply[TC, MA] = implicitly
 
+  // the type we will instantiate when we find a typeclass instance
+  // which is already the expected shape: F[_]
   type Aux1[TC[_[_]], MA, F[_], AA] = Unapply[TC, MA] {
     type M[X] = F[X]
     type A = AA
   }
 
-  type Aux2Left[TC[_[_]], FA, F[_,_], AA, B] = Unapply[TC, FA] {
-    type M[X] = F[X,B]
-    type A = AA
-  }
-
-  type Aux2Right[TC[_[_]], MA, F[_,_], AA, B] = Unapply[TC, MA] {
-    type M[X] = F[AA,X]
-    type A = B
-  }
-
-  implicit def unapply1[TC[_[_]], F[_], AA](implicit tc: TC[F]): Aux1[TC,F[AA],F,AA] =
+  implicit def unapply1[TC[_[_]], F[_], AA](implicit tc: TC[F])
+      : Aux1[TC,F[AA],F,AA] =
     new Unapply[TC,F[AA]] {
       type M[X] = F[X]
       type A = AA
@@ -34,7 +48,22 @@ object Unapply {
       override def subst = identity
   }
 
-   implicit def unapply2left[TC[_[_]], F[_,_], AA, B](implicit tc: TC[F[?,B]]): Aux2Left[TC,F[AA,B], F, AA, B] = new Unapply[TC, F[AA,B]] {
+  // the type we will instantiate when we find a typeclass instance
+  // for a type in the thape F[_,_] when we fix the left type
+  type Aux2Left[TC[_[_]], FA, F[_,_], AA, B] = Unapply[TC, FA] {
+    type M[X] = F[X,B]
+    type A = AA
+  }
+
+  // the type we will instantiate when we find a typeclass instance
+  // for a type in the thape F[_,_] when we fix the right type
+  type Aux2Right[TC[_[_]], MA, F[_,_], AA, B] = Unapply[TC, MA] {
+    type M[X] = F[AA,X]
+    type A = B
+  }
+
+
+  implicit def unapply2left[TC[_[_]], F[_,_], AA, B](implicit tc: TC[F[?,B]]): Aux2Left[TC,F[AA,B], F, AA, B] = new Unapply[TC, F[AA,B]] {
      type M[X] = F[X, B]
      type A = AA
      def TC: TC[F[?, B]] = tc
