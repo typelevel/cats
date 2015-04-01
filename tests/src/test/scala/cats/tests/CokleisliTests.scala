2589c4328cbc1e5e2c64c5b8fc6f8c415f702681
Frank S. Thomas <frank@timepit.eu>
2015-04-01 18:16:51 +0200

    Check StrongLaws for Cokleisli[NonEmptyList, ?, ?]

--- tests/src/test/scala/cats/tests/CokleisliTests.scala
+++ tests/src/test/scala/cats/tests/CokleisliTests.scala
@@ -1,7 +1,7 @@
 package cats.tests
 
 import algebra.Eq
-import cats.data.Cokleisli
+import cats.data.{Cokleisli, NonEmptyList}
 import cats.laws.discipline._
 import cats.laws.discipline.arbitrary._
 import cats.laws.discipline.eq._
@@ -13,5 +13,16 @@ class CokleisliTests extends CatsSuite {
     Eq.by[Cokleisli[F, A, B], F[A] => B](_.run)
 
   checkAll("Cokleisli[Option, Int, Int]", ApplicativeTests[Cokleisli[Option, Int, ?]].applicative[Int, Int, Int])
-  checkAll("Cokleisli[Option, Int, Int]", ProfunctorTests[Cokleisli[Option, ?, ?]].profunctor[Int, Int, Int, Int, Int, Int])
+
+  {
+    type CokleisliNEL[A, B] = Cokleisli[NonEmptyList, A, B]
+
+    implicit def ev0[A: Arbitrary, B: Arbitrary]: Arbitrary[CokleisliNEL[A, B]] =
+      cokleisliArbitrary[NonEmptyList, A, B]
+
+    implicit def ev1[A: Arbitrary, B: Eq]: Eq[CokleisliNEL[A, B]] =
+      cokleisliEq[NonEmptyList, A, B](oneAndArbitrary, Eq[B])
+
+    checkAll("Cokleisli[NonEmptyList, Int, Int]", StrongTests[CokleisliNEL].strong[Int, Int, Int, Int, Int, Int])
+  }
 }
