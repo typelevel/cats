f9153f9d6c44c84d91ae95513cb30b06190984c3
Frank S. Thomas <frank@timepit.eu>
2015-04-06 16:12:00 +0200

    MonadCombineTests include AlternativeTests

--- tests/src/test/scala/cats/tests/OptionTests.scala
+++ tests/src/test/scala/cats/tests/OptionTests.scala
@@ -1,9 +1,8 @@
 package cats.tests
 
-import cats.laws.discipline.{CoflatMapTests, MonadCombineTests, AlternativeTests}
+import cats.laws.discipline.{CoflatMapTests, MonadCombineTests}
 
 class OptionTests extends CatsSuite {
   checkAll("Option[Int]", CoflatMapTests[Option].coflatMap[Int, Int, Int])
   checkAll("Option[Int]", MonadCombineTests[Option].monadCombine[Int, Int, Int])
-  checkAll("Option[Int]", AlternativeTests[Option].alternative[Int, String, Int])
 }
