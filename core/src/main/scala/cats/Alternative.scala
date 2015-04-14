a7cf8b7941371dce16af11b45945ac676f5de4c3
Mike (stew) O'Connor <stew@vireo.org>
2015-04-13 23:30:20 -0700

    oops, didn't mean to add alternative methods that haven't been checked here

--- core/src/main/scala/cats/Alternative.scala
+++ core/src/main/scala/cats/Alternative.scala
@@ -2,11 +2,6 @@ package cats
 
 trait Alternative[F[_]] extends Applicative[F] with MonoidK[F] {
 
-  def some[G[_], A](fa: F[A])(implicit G: Alternative[G]): F[G[A]] =
-    map2(fa, many(fa))((a,b) => G.combine(G.pure(a), b))
-
-  def many[G[_], A](fa: F[A])(implicit G: Alternative[G]): F[G[A]] =
-    combine(some(fa), pure(G.empty))
 }
 
 object Alternative {
