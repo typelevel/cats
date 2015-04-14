9ae590300c9dc375aa4fceaef184df8174e5d245
Marc Siegel <marc@usainnov.com>
2015-04-13 20:48:45 -0400

    Fix typo "will cached" -> "will cache"

--- core/src/main/scala/cats/Lazy.scala
+++ core/src/main/scala/cats/Lazy.scala
@@ -33,7 +33,7 @@ sealed abstract class Lazy[A] extends Product with Serializable { self =>
   def value: A
 
   /**
-   * Given a lazy value, create a new one which will cached
+   * Given a lazy value, create a new one which will cache
    * (i.e. memoize) its value.
    *
    * The practical effect of this method is to convert by-name
