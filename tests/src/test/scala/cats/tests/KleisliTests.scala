f221f843b25bc8bf04ef81a8f8bb5cb9bace531a
Owen Parry <oparry@twitter.com>
2015-06-15 13:26:37 -0700

    better test coverage for kleisli

--- tests/src/test/scala/cats/tests/KleisliTests.scala
+++ tests/src/test/scala/cats/tests/KleisliTests.scala
@@ -1,24 +1,76 @@
 package cats
 package tests
 
-import cats.arrow.Arrow
+import cats.arrow.{Split, Arrow}
 import cats.data.Kleisli
 import cats.functor.Strong
 import cats.laws.discipline._
 import cats.laws.discipline.arbitrary._
 import cats.laws.discipline.eq._
 import org.scalacheck.Arbitrary
+import org.scalacheck.Prop._
 
 class KleisliTests extends CatsSuite {
   implicit def kleisliEq[F[_], A, B](implicit A: Arbitrary[A], FB: Eq[F[B]]): Eq[Kleisli[F, A, B]] =
     Eq.by[Kleisli[F, A, B], A => F[B]](_.run)
 
-  checkAll("Kleisli[Option, Int, Int]", ApplicativeTests[Kleisli[Option, Int, ?]].applicative[Int, Int, Int])
-  checkAll("Applicative[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Applicative[Kleisli[Option, Int, ?]]))
+  {
+    implicit val kleisliArrow = Kleisli.kleisliArrow[Option]
+    checkAll("Kleisli[Option, Int, Int]", ArrowTests[Kleisli[Option, ?, ?]].arrow[Int, Int, Int, Int, Int, Int])
+    checkAll("Arrow[Kleisli[Option, ?, ?]]", SerializableTests.serializable(Arrow[Kleisli[Option, ?, ?]]))
+  }
 
-  checkAll("Kleisli[Option, Int, Int]", StrongTests[Kleisli[Option, ?, ?]].strong[Int, Int, Int, Int, Int, Int])
-  checkAll("Strong[Kleisli[Option, ?, ?]]", SerializableTests.serializable(Strong[Kleisli[Option, ?, ?]]))
+  {
+    implicit val kleisliMonad = Kleisli.kleisliMonad[Option, Int]
+    checkAll("Kleisli[Option, Int, Int]", MonadTests[Kleisli[Option, Int, ?]].monad[Int, Int, Int])
+    checkAll("Monad[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Monad[Kleisli[Option, Int, ?]]))
+  }
 
-  checkAll("Kleisli[Option, Int, Int]", ArrowTests[Kleisli[Option, ?, ?]].arrow[Int, Int, Int, Int, Int, Int])
-  checkAll("Arrow[Kleisli[Option, ?, ?]]", SerializableTests.serializable(Arrow[Kleisli[Option, ?, ?]]))
+  {
+    implicit val kleisliSplit = Kleisli.kleisliSplit[Option]
+    checkAll("Kleisli[Option, Int, Int]", SplitTests[Kleisli[Option, ?, ?]].split[Int, Int, Int, Int, Int, Int])
+    checkAll("Split[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Split[Kleisli[Option, ?, ?]]))
+  }
+
+  {
+    implicit val kleisliStrong = Kleisli.kleisliStrong[Option]
+    checkAll("Kleisli[Option, Int, Int]", StrongTests[Kleisli[Option, ?, ?]].strong[Int, Int, Int, Int, Int, Int])
+    checkAll("Strong[Kleisli[Option, ?, ?]]", SerializableTests.serializable(Strong[Kleisli[Option, ?, ?]]))
+  }
+
+  {
+    implicit val kleisliFlatMap = Kleisli.kleisliFlatMap[Option, Int]
+    checkAll("Kleisli[Option, Int, Int]", FlatMapTests[Kleisli[Option, Int, ?]].flatMap[Int, Int, Int])
+    checkAll("FlatMap[Kleisli[Option, Int, ?]]", SerializableTests.serializable(FlatMap[Kleisli[Option, Int, ?]]))
+  }
+
+  {
+    implicit val kleisliApplicative = Kleisli.kleisliApplicative[Option, Int]
+    checkAll("Kleisli[Option, Int, Int]", ApplicativeTests[Kleisli[Option, Int, ?]].applicative[Int, Int, Int])
+    checkAll("Applicative[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Applicative[Kleisli[Option, Int, ?]]))
+  }
+
+  {
+    implicit val kleisliApply = Kleisli.kleisliApply[Option, Int]
+    checkAll("Kleisli[Option, Int, Int]", ApplyTests[Kleisli[Option, Int, ?]].apply[Int, Int, Int])
+    checkAll("Apply[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Apply[Kleisli[Option, Int, ?]]))
+  }
+
+  {
+    implicit val kleisliFunctor = Kleisli.kleisliFunctor[Option, Int]
+    checkAll("Kleisli[Option, Int, Int]", FunctorTests[Kleisli[Option, Int, ?]].functor[Int, Int, Int])
+    checkAll("Functor[Kleisli[Option, Int, ?]]", SerializableTests.serializable(Functor[Kleisli[Option, Int, ?]]))
+  }
+
+  check {
+    forAll { (f: Int => Option[String], g: Int => Int, i: Int) =>
+      f(g(i)) == Kleisli.local[Option, String, Int](g)(Kleisli.kleisli(f)).run(i)
+    }
+  }
+
+  check {
+    forAll { (i: Int) =>
+      Kleisli.pure[Option, Int, Int](i).run(i) == Kleisli.ask[Option, Int].run(i)
+    }
+  }
 }
