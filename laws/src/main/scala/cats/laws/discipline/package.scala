898e43d65a06e93f840062c5a807bc96038083f4
Adelbert Chang <adelbertc@gmail.com>
2015-07-10 14:04:53 -0700

    Add isEqProp - useful for laws that don't need input

--- laws/src/main/scala/cats/laws/discipline/package.scala
+++ laws/src/main/scala/cats/laws/discipline/package.scala
@@ -7,4 +7,7 @@ import org.scalacheck.Prop
 package object discipline {
   implicit def isEqToProp[A: Eq](isEq: IsEq[A]): Prop =
     isEq.lhs ?== isEq.rhs
+
+  def isEqProp[A: Eq](isEq: IsEq[A]): Prop =
+    Prop(Eq[A].eqv(isEq.lhs, isEq.rhs))
 }
