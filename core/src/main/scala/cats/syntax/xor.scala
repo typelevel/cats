9690587868065487b883ec8007367bf4d7dc6ff6
Dave Rostron <yastero@gmail.com>
2015-08-25 14:49:03 -0700

    make XorOps a value class

--- core/src/main/scala/cats/syntax/xor.scala
+++ core/src/main/scala/cats/syntax/xor.scala
@@ -7,7 +7,7 @@ trait XorSyntax {
   implicit def xorSyntax[A](a: A): XorOps[A] = new XorOps(a)
 }
 
-class XorOps[A](a: A) {
+class XorOps[A](val a: A) extends AnyVal {
   def left[B]: A Xor B = Xor.Left(a)
   def right[B]: B Xor A = Xor.Right(a)
 }
