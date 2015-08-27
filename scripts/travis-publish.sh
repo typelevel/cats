cc3189a2329bfac0c74ccd63a0ded60e96005951
Erik Osheim <d_m@plastic-idolatry.com>
2015-08-27 09:51:49 -0400

    Temporarily disable site generation to get Travis building.
    Fixing these issues will hopefully be easy, but I want to
    get everything into a working state first.

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -6,10 +6,11 @@
 export publish_cmd="publishLocal"
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $(cat version.sbt) =~ "-SNAPSHOT" ]]; then
-  export publish_cmd="publish gitSnapshots publish"   
-  if [[ $TRAVIS_SCALA_VERSION = "2.11.7" ]]; then
-    export publish_cmd="$publish_cmd ghpagesPushSite"
-  fi
+  export publish_cmd="publish gitSnapshots publish"
+  # temporarily disable to stabilize travis
+  #if [[ $TRAVIS_SCALA_VERSION = "2.11.7" ]]; then
+  #  export publish_cmd="$publish_cmd ghpagesPushSite"
+  #fi
 fi
 
 sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
