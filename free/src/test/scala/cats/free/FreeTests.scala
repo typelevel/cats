c027ea4e6f006aaa9a8e7670d00ae3e61267fc5f
Erik Osheim <d_m@plastic-idolatry.com>
2015-09-02 18:23:04 -0400

    Fix free and state projects.

--- free/src/test/scala/cats/free/FreeTests.scala
+++ free/src/test/scala/cats/free/FreeTests.scala
@@ -2,7 +2,7 @@ package cats
 package free
 
 import cats.tests.CatsSuite
-import cats.laws.discipline.{ArbitraryK, MonadTests, SerializableTests}
+import cats.laws.discipline.{ArbitraryK, EqK, MonadTests, SerializableTests}
 import org.scalacheck.{Arbitrary, Gen}
 
 class FreeTests extends CatsSuite {
@@ -19,10 +19,18 @@ class FreeTests extends CatsSuite {
         freeArbitrary[F, A]
     }
 
-  implicit def freeEq[S[_]:Monad, A](implicit SA: Eq[S[A]]): Eq[Free[S, A]] =
+  implicit def freeEq[S[_]: Monad, A](implicit SA: Eq[S[A]]): Eq[Free[S, A]] =
     new Eq[Free[S, A]] {
       def eqv(a: Free[S, A], b: Free[S, A]): Boolean =
         SA.eqv(a.runM(identity),  b.runM(identity))
+    }
+
+  implicit def freeEqK[S[_]: EqK: Monad]: EqK[Free[S, ?]] =
+    new EqK[Free[S, ?]] {
+      def synthesize[A: Eq]: Eq[Free[S, A]] = {
+        implicit val sa: Eq[S[A]] = EqK[S].synthesize[A]
+        freeEq[S, A]
+      }
     }
 
   checkAll("Free[Option, ?]", MonadTests[Free[Option, ?]].monad[Int, Int, Int])
