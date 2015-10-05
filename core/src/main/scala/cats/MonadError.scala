078947da086f2350140c3f8670663b95ced1bcc2
Cody Allen <ceedubs@gmail.com>
2015-10-05 17:50:24 -0400

    Add some helper methods to MonadError
    You should make me write unit tests and ScalaDocs for these before you
    merge. I wanted to open a PR before I forgot about this though.

--- core/src/main/scala/cats/MonadError.scala
+++ core/src/main/scala/cats/MonadError.scala
@@ -1,5 +1,7 @@
 package cats
 
+import cats.data.{Xor, XorT}
+
 /** A monad that also allows you to raise and or handle an error value.
   *
   * This type class allows one to abstract over error-handling monads.
@@ -7,7 +9,21 @@ package cats
 trait MonadError[F[_], E] extends Monad[F] {
   def raiseError[A](e: E): F[A]
 
-  def handleError[A](fea: F[A])(f: E => F[A]): F[A]
+  def handleError[A](fa: F[A])(f: E => F[A]): F[A]
+
+  def attempt[A](fa: F[A]): F[E Xor A] = handleError(
+    map(fa)(Xor.right[E, A])
+  )(e => pure(Xor.left(e)))
+
+  def attemptT[A](fa: F[A]): XorT[F, E, A] = XorT(attempt(fa))
+
+  def recover[A](fa: F[A])(pf: PartialFunction[E, A]): F[A] =
+    handleError(fa)(e =>
+      (pf andThen pure) applyOrElse(e, raiseError))
+
+  def recoverWith[A](fa: F[A])(pf: PartialFunction[E, F[A]]): F[A] =
+    handleError(fa)(e =>
+      pf applyOrElse(e, raiseError))
 }
 
 object MonadError {
