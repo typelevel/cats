55de7f5e20f01860976bd4acec22c8a84097e5a2
Colt Frederickson <coltfred@gmail.com>
2015-10-26 21:15:53 -0600

    Add syntax for validated.

--- core/src/main/scala/cats/syntax/option.scala
+++ core/src/main/scala/cats/syntax/option.scala
@@ -1,7 +1,7 @@
 package cats
 package syntax
 
-import cats.data.Xor
+import cats.data.{ Xor, Validated, ValidatedNel }
 
 trait OptionSyntax {
   def none[A] = Option.empty[A]
@@ -16,5 +16,9 @@ class OptionIdOps[A](val a: A) extends AnyVal {
 class OptionOps[A](val oa: Option[A]) extends AnyVal {
   def toLeftXor[B](b: => B): A Xor B = oa.fold[A Xor B](Xor.Right(b))(Xor.Left(_))
   def toRightXor[B](b: => B): B Xor A = oa.fold[B Xor A](Xor.Left(b))(Xor.Right(_))
+  def toInvalid[B](b: => B): Validated[A, B] = oa.fold[Validated[A, B]](Validated.Valid(b))(Validated.Invalid(_))
+  def toInvalidNel[B](b: => B): ValidatedNel[A, B] = oa.fold[ValidatedNel[A, B]](Validated.Valid(b))(Validated.invalidNel(_))
+  def toValid[B](b: => B): Validated[B, A] = oa.fold[Validated[B, A]](Validated.Invalid(b))(Validated.Valid(_))
+  def toValidNel[B](b: => B): ValidatedNel[B, A] = oa.fold[ValidatedNel[B, A]](Validated.invalidNel(b))(Validated.Valid(_))
   def orEmpty(implicit A: Monoid[A]): A = oa.getOrElse(A.empty)
 }
