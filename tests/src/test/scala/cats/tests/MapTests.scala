351cca16cfa6d03979f01a0737faccb3bbd1329a
Mike Curry <mikcurry@gmail.com>
2015-11-15 12:13:23 +0000

    Add a test for Show[Map]
    Adds a test for Show[Map]

--- tests/src/test/scala/cats/tests/MapTests.scala
+++ tests/src/test/scala/cats/tests/MapTests.scala
@@ -9,4 +9,14 @@ class MapTests extends CatsSuite {
 
   checkAll("Map[Int, Int] with Option", TraverseTests[Map[Int, ?]].traverse[Int, Int, Int, Int, Option, Option])
   checkAll("Traverse[Map[Int, ?]]", SerializableTests.serializable(Traverse[Map[Int, ?]]))
+
+  test("show isn't empty and is formatted as expected") {
+    val mapShow = implicitly[Show[Map[Int, String]]]
+
+    forAll { (map: Map[Int, String]) =>
+      val show = mapShow.show(map)
+      show.nonEmpty should === (true)
+      show.startsWith("Map(") should === (true)
+    }
+  }
 }
