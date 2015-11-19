8625b2e87c9c74db56af853b5706a12450708975
Cody Allen <ceedubs@gmail.com>
2015-11-19 08:44:26 -0500

    Check laws on Function1 Monoid

--- tests/src/test/scala/cats/tests/FunctionTests.scala
+++ tests/src/test/scala/cats/tests/FunctionTests.scala
@@ -8,6 +8,7 @@ import cats.functor.Contravariant
 import cats.laws.discipline._
 import cats.laws.discipline.eq._
 import cats.laws.discipline.arbitrary._
+import algebra.laws.GroupLaws
 
 class FunctionTests extends CatsSuite {
   implicit def ev0[A: Arbitrary]: Arbitrary[() => A] = Arbitrary(Arbitrary.arbitrary[A].map { a => () => a })
@@ -25,4 +26,6 @@ class FunctionTests extends CatsSuite {
 
   checkAll("Function1[Int, Int]", ContravariantTests[? => Int].contravariant[Int, Int, Int])
   checkAll("Contravariant[? => Int]", SerializableTests.serializable(Contravariant[? => Int]))
+
+  checkAll("Function1[String, Int]", GroupLaws[Function1[String, Int]].monoid)
 }
