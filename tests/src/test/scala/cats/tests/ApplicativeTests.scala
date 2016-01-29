5d9d2d6f53cd22710a2c70a49469e7439d1f38d1
Luke Wyman <lukewyman@gmail.com>
2016-01-29 15:42:51 -0700

    added replicateA to Applicative

---
+++ tests/src/test/scala/cats/tests/ApplicativeTests.scala
@@ -0,0 +1,12 @@
+package cats
+package tests
+
+import cats.Applicative
+
+
+class ApplicativeTests extends CatsSuite {
+
+  test("replicateA creates a List of 'n' copies of given Applicative 'fa'") {
+
+  }
+}
