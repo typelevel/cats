f84f35b7dc4b7c2b6aa46d5ded1f26507c400047
Luke Wyman <lukewyman@gmail.com>
2016-02-15 15:22:14 -0700

    modified scaladoc comment on replicateA

--- core/src/main/scala/cats/Applicative.scala
+++ core/src/main/scala/cats/Applicative.scala
@@ -30,7 +30,7 @@ import cats.std.list._
   def pureEval[A](x: Eval[A]): F[A] = pure(x.value)
 
   /**
-    * `replicateA` creates a list of the provided Applicative Functor `fa` repeated `n` times
+    * Given fa and n, apply fa n times to construct an F[List[A]] value.
     */
   def replicateA[A](n: Int, fa: F[A]): F[List[A]] =
     sequence(List.fill(n)(fa))
