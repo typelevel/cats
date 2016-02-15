3a8519e3332202182cd01f266bf432f609d79d19
Luke Wyman <lukewyman@gmail.com>
2016-02-15 14:25:37 -0700

    added test for replicateA

--- tests/src/test/scala/cats/tests/ApplicativeTests.scala
+++ tests/src/test/scala/cats/tests/ApplicativeTests.scala
@@ -4,9 +4,13 @@ package tests
 import cats.Applicative
 
 
-class ApplicativeTests extends CatsSuite {
+class ApplicativeCheck extends CatsSuite {
 
   test("replicateA creates a List of 'n' copies of given Applicative 'fa'") {
+
+    val A = Applicative[Option]
+    val fa = A.pure(1)
+    A.replicateA(5, fa) should === (Some(List(1,1,1,1,1)))
 
   }
 }
