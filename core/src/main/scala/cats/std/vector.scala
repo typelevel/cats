461102435fbd2068bf32cf4f067a74a35923dfd0
Tomas Mikula <tomas.mikula@gmail.com>
2016-02-16 14:15:47 -0500

    Avoid calling Vector.size repeatedly in Eq[Vector].

--- core/src/main/scala/cats/std/vector.scala
+++ core/src/main/scala/cats/std/vector.scala
@@ -68,11 +68,11 @@ trait VectorInstances {
   implicit def eqVector[A](implicit ev: Eq[A]): Eq[Vector[A]] =
     new Eq[Vector[A]] {
       def eqv(x: Vector[A], y: Vector[A]): Boolean = {
-        @tailrec def loop(from: Int): Boolean =
-          if(from == x.size) true
-          else ev.eqv(x(from), y(from)) && loop(from + 1)
+        @tailrec def loop(to: Int): Boolean =
+          if(to == -1) true
+          else ev.eqv(x(to), y(to)) && loop(to - 1)
 
-        (x.size == y.size) && loop(0)
+        (x.size == y.size) && loop(x.size - 1)
       }
     }
 }
