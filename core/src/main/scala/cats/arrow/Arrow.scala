de5474175ec7f09caf05a49300278f23e670f221
lucd <luc.duponcheel@gmail.com>
2016-02-23 09:47:49 +0100

    added a few comments to Arrow.scala

--- core/src/main/scala/cats/arrow/Arrow.scala
+++ core/src/main/scala/cats/arrow/Arrow.scala
@@ -3,30 +3,35 @@ package arrow
 
 import cats.functor.Strong
 
-trait Arrow[F[_, _]] extends Split[F] with Strong[F] with Category[F] { self =>
+trait Arrow[F[_, _]] extends Split[F] with Strong[F] with Category[F] {
+  self =>
+
+  //
+  // standard arrow methods
+  // see [[ https://www.haskell.org/arrows ]]
+  //
+  // defined methods are defined using `compose`,
+  // they might as well be defined using `andThen`
+  //
 
   def lift[A, B](f: A => B): F[A, B]
 
-  def dimap[A, B, C, D](fab: F[A, B])(f: C => A)(g: B => D): F[C, D] =
-    compose(lift(g), andThen(lift(f), fab))
+  def dimap[A, B, C, D](fab: F[A, B])(fca: C => A)(fbd: B => D): F[C, D] =
+    compose(lift(fbd), compose(fab, lift(fca)))
 
-  def second[A, B, C](fa: F[A, B]): F[(C, A), (C, B)] = {
-    def swap[X, Y]: F[(X, Y), (Y, X)] = lift[(X, Y), (Y, X)] { case (x, y) => (y, x) }
-    compose(swap, compose(first[A, B, C](fa), swap))
-  }
+  def second[A, B, C](fab: F[A, B]): F[(C, A), (C, B)] =
+    compose(swapArrow, compose(first[A, B, C](fab), swapArrow))
 
-  def split[A, B, C, D](f: F[A, B], g: F[C, D]): F[(A, C), (B, D)] =
-    andThen(first(f), second(g))
+  def split[A, B, C, D](fab: F[A, B], fcd: F[C, D]): F[(A, C), (B, D)] =
+    compose(second(fcd), first(fab))
 
-  // helper arrow: duplicates value
-  def dupArrow[A]: F[A, (A, A)] = lift[A, (A, A)] { a => (a, a) }
-
-  // helper arrow: applies function value to value
-  def applyArrow[A, B]: F[(A, A => B), B] =
-    lift {
-      case (a, a2b) =>
-        a2b(a)
-    }
+  //
+  // arrow calculus related methods
+  // see [[ http://homepages.inf.ed.ac.uk/wadler/papers/arrows/arrows.pdf ]]
+  //
+  // defined methods are defined using `andThen`,
+  // they might as well be defined using `compose`
+  //
 
   // uncurried version of flatDef
   def flatDefine[A, B, C](afb: F[A, B]): F[(A, B), C] => F[A, C] =
@@ -53,6 +58,21 @@ trait Arrow[F[_, _]] extends Split[F] with Strong[F] with Category[F] { self =>
   // curried version of value
   def `val`[A, B, C](b: B): F[B, A => C] => F[A, C] =
     `def` { _ => b }
+
+  //
+  // useful pure arrows
+  // (obtained by lifting corresponding useful functions)
+  //
+
+  // swaps values
+  def swapArrow[A, B]: F[(A, B), (B, A)] = lift[(A, B), (B, A)] { case (a, b) => (b, a) }
+
+  // duplicates value
+  def dupArrow[A]: F[A, (A, A)] = lift[A, (A, A)] { a => (a, a) }
+
+  // applies function value to value
+  def applyArrow[A, B]: F[(A, A => B), B] =
+    lift { case (a, a2b) => a2b(a) }
 
 }
 
