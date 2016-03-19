87063b9439ddcca9e1b672cdc87996d43ca5d743
Adelbert Chang <adelbertc@gmail.com>
2016-03-18 18:20:46 -0700

    Add Bifunctor compose test

---
+++ tests/src/test/scala/cats/tests/BifunctorTests.scala
@@ -0,0 +1,15 @@
+package cats
+package tests
+
+import cats.functor.Bifunctor
+import cats.laws.discipline.{SerializableTests, BifunctorTests}
+import cats.laws.discipline.eq.tuple2Eq
+
+class BifunctorTest extends CatsSuite {
+  type Tuple2Either[A, B] = (Either[A, B], Either[A, B])
+  val tuple2ComposeEither: Bifunctor[Tuple2Either] =
+    Bifunctor[Tuple2].compose[Either]
+
+  checkAll("Tuple2 compose Either", BifunctorTests(tuple2ComposeEither).bifunctor[Int, Int, Int, String, String, String])
+  checkAll("Bifunctor[Tuple2 compose Either]", SerializableTests.serializable(tuple2ComposeEither))
+}
