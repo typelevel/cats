4df21ac394e09140fce05b96b244fe5e66049c64
Jacob Barber <JacobA.Barber@yahoo.com>
2016-04-11 20:48:07 -0500

    Created derivation of Bitraverse from copair

--- core/src/main/scala/cats/syntax/copair.scala
+++ core/src/main/scala/cats/syntax/copair.scala
@@ -1,6 +1,7 @@
 package cats
 package syntax
 
+
 /**
   * Created by jbarber on 4/10/16.
   */
@@ -10,26 +11,47 @@ trait CopairSyntax {
 
   implicit def copairIdSyntax[A](a: A): CopairIdOps[A] = new CopairIdOps[A](a)
 
+  implicit def bitraverseFromCopair[F[_,_]](implicit F: Copair[F]): Bitraverse[F] =
+    new Bitraverse[F] {
+      def bitraverse[G[_], A, B, C, D](fab: F[A, B])(f: A => G[C], g: B => G[D])(implicit G: Applicative[G]): G[F[C, D]] =
+        F.fold(fab)(
+          l => G.map(f(l))(F.left),
+          r => G.map(g(r))(F.right)
+        )
+
+      def bifoldLeft[A, B, C](fab: F[A, B], c: C)(f: (C, A) => C, g: (C, B) => C): C =
+        F.fold(fab)(
+          l => f(c,l),
+          r => g(c,r)
+        )
+
+      def bifoldRight[A, B, C](fab: F[A, B], c: Eval[C])(f: (A, Eval[C]) => Eval[C], g: (B, Eval[C]) => Eval[C]): Eval[C] =
+        F.fold(fab)(
+          l => f(l,c),
+          r => g(r,c)
+        )
+    }
+
+
 }
 
-final class CopairOps[F[_,_]: Copair, A, B](pair: F[A,B]) {
-  val ev = implicitly[Copair[F]]
+final class CopairOps[F[_,_], A, B](pair: F[A,B])(implicit F: Copair[F]) {
 
-  def fold[C](fa: A => C, fb: B => C): C = ev.fold(pair)(fa, fb)
-  def swap: F[B,A] = ev.swap(pair)
+  def fold[C](fa: A => C, fb: B => C): C = F.fold(pair)(fa, fb)
+  def swap: F[B,A] = F.swap(pair)
 
-  def isRight: Boolean = ev.isRight(pair)
-  def isLeft:  Boolean = ev.isLeft(pair)
+  def isRight: Boolean = F.isRight(pair)
+  def isLeft:  Boolean = F.isLeft(pair)
 
-  def foreach(fn: B => Unit): Unit = ev.foreach(pair)(fn)
+  def foreach(fn: B => Unit): Unit = F.foreach(pair)(fn)
 
-  def forall(fn: B => Boolean): Boolean = ev.forall(pair)(fn)
+  def forall(fn: B => Boolean): Boolean = F.forall(pair)(fn)
 
-  def exists(fn: B => Boolean): Boolean = ev.forall(pair)(_ => false, fn)
+  def exists(fn: B => Boolean): Boolean = F.exists(pair)(fn)
 
 }
 
 final class CopairIdOps[A](a: A) {
-  def left[F[_,_]: Copair, B]: F[A,B] = implicitly[Copair[F]].left(a)
-  def right[F[_,_]: Copair, B]: F[B,A] = implicitly[Copair[F]].right(a)
+  def left[F[_,_], B](implicit F: Copair[F]): F[A,B] = F.left(a)
+  def right[F[_,_], B](implicit F: Copair[F]): F[B,A] = F.right(a)
 }
