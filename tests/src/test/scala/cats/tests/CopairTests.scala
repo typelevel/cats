6c9a88600a90a7997258c2f6eec5aa729f9b10b0
Jacob Barber <JacobA.Barber@yahoo.com>
2016-04-13 21:22:52 -0500

    improved test implementations, added explanation as to why some of the tests are written how they are, increased test coverage

--- tests/src/test/scala/cats/tests/CopairTests.scala
+++ tests/src/test/scala/cats/tests/CopairTests.scala
@@ -1,62 +1,72 @@
 package cats.tests
 
 import cats.Copair
-import cats.data.Xor
+import cats.data.{Validated, Xor}
 import cats.laws.discipline.{CopairTests => CopairLaws}
 import cats.laws.discipline.arbitrary._
+import org.scalacheck.Arbitrary
 
 class CopairTests extends CatsSuite {
 
-  checkAll(" ", CopairLaws[Xor].copair[Option, Int, Int, Int, String, String, String])
+  checkAll("Xor", CopairLaws[Xor].copair[Option, Int, Int, Int, String, String, String])
 
-  test("Copair for-each performs side-effect") {
+  testCopairs[Xor]("Xor")
+  testCopairs[Either]("Either")
+  testCopairs[Validated]("Validated")
 
-    forAll { copair: Xor[Int, String] =>
+  def testCopairs[F[_,_]: Copair](ofType: String)(implicit arb: Arbitrary[F[String, Int]]): Unit = {
 
-      def copairForeach[F[_,_]: Copair, A, B](f: F[A,B])(fb: B => Unit): Unit = f.foreach(fb)
+    test(s"$ofType Copair for-each performs side-effect") {
 
-      var sideEffectOccurred = false
-      copairForeach(copair)(_ => sideEffectOccurred = true)
+      forAll { copair: F[String, Int] =>
 
-      sideEffectOccurred should === (copair.isRight)
+        def copairForeach[A, B](f: F[A,B])(fb: B => Unit): Unit = f.foreach(fb)
+
+        var sideEffectOccurred = false
+        copairForeach(copair)(_ => sideEffectOccurred = true)
+
+        sideEffectOccurred should === (copair.isRight)
+      }
     }
-  }
 
-  /**
-    * The tests defined below have a def that tests the [[Copair]] implementation,
-    * followed by a comparison to the [[Xor]] implementation.
-    *
-    * They should follow this template:
-    *   def copairFoo[F[_,_]: Copair, A, B](f: F[A,B])...
-    *   copairFoo(copair) should === (copair.foo)
-    */
+    /**
+      * The tests defined below have a def that tests the [[Copair]] implementation,
+      * followed by a comparison to the [[Xor]] implementation.
+      *
+      * They should follow this template:
+      *   def copairFoo[A, B](f: F[A,B])...
+      *   copairFoo(copair) should === (copair.foo)
+      */
 
-  test("Copair for-all") {
-    forAll { copair: Xor[String, Int] =>
+    test(s"$ofType Copair for-all") {
+      forAll { copair: F[String, Int] =>
 
-      def copairForAll[F[_,_]: Copair, A, B](f: F[A,B])(fb: B => Boolean): Boolean = f.forall(fb)
+        def copairForAll[A, B](f: F[A, B])(fb: B => Boolean): Boolean = f.forall(fb)
 
-      copairForAll(copair)(_ % 2 == 0) should === (copair.forall(_ % 2 == 0))
+        copairForAll(copair)(_ % 2 == 0) should ===(copair.forall(_ % 2 == 0))
+      }
     }
-  }
 
-  test("Copair exists") {
-    forAll { copair: Xor[String, Int] =>
+    test(s"$ofType Copair exists") {
+      forAll { copair: F[String, Int] =>
 
-      def copairExists[F[_,_]: Copair, A, B](f: F[A,B])(fb: B => Boolean): Boolean = f.exists(fb)
+        def copairExists[A, B](f: F[A, B])(fb: B => Boolean): Boolean = f.exists(fb)
 
-      copairExists(copair)(_ % 2 == 0) should === (copair.exists(_ % 2 == 0))
+        copairExists(copair)(_ % 2 == 0) should ===(copair.exists(_ % 2 == 0))
+      }
     }
-  }
 
-  test("Copair left/right") {
-    forAll { copair: Xor[String, Int] =>
+    test(s"$ofType Copair left/right") {
+      forAll { copair: F[String, Int] =>
 
-      def copairIsLeft[F[_,_]: Copair](f: F[_,_]): Boolean = f.isLeft
-      def copairIsRight[F[_,_]: Copair](f: F[_,_]): Boolean = f.isRight
+        def copairIsLeft(f: F[_, _]): Boolean = f.isLeft
+        def copairIsRight(f: F[_, _]): Boolean = f.isRight
 
-      copairIsLeft(copair) should === (copair.isLeft)
-      copairIsRight(copair) should === (copair.isRight)
+        copairIsLeft(copair) should ===(copair.isLeft)
+        copairIsRight(copair) should ===(copair.isRight)
+      }
     }
+
+
   }
 }
