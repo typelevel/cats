7a67f713c539cf770a0ac10afe538b3134a19b13
Jacob Barber <JacobA.Barber@yahoo.com>
2016-04-13 19:56:35 -0500

    updated copair laws to use syntax.

--- laws/src/main/scala/cats/laws/CopairLaws.scala
+++ laws/src/main/scala/cats/laws/CopairLaws.scala
@@ -1,23 +1,22 @@
 package cats.laws
 
 import cats.Copair
+import cats.syntax.copair._
 
 trait CopairLaws[F[_,_]] extends BitraverseLaws[F] with BifoldableLaws[F] with BifunctorLaws[F] {
   implicit override def F: Copair[F]
 
   def copairFoldIdentity[A, B](fab: F[A, B]): IsEq[F[A, B]] =
-    fab <-> F.fold[A,B,F[A,B]](fab)(F.left, F.right)
+    fab <-> fab.fold(_.leftC[F, B], _.rightC[F, A])
 
   def copairSwapIdentity[A,B](fab: F[A,B]): IsEq[F[A,B]] =
-    fab <-> F.swap(F.swap(fab))
+    fab <-> fab.swap.swap
 
   def copairLeftAssociativeIdentity[A, B, C](a: A, fa: A => C, fb: B => C): IsEq[C] =
-    F.fold(F.left(a))(fa, fb) <-> fa(a)
+    a.leftC[F, B].fold(fa, fb) <-> fa(a)
 
   def copairRightAssociativeIdentity[A, B, C](b: B, fa: A => C, fb: B => C): IsEq[C] =
-    F.fold(F.right(b))(fa, fb) <-> fb(b)
-
-
+    b.rightC[F, A].fold(fa, fb) <-> fb(b)
 }
 
 object CopairLaws {
