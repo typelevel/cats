c21a0651253cc2ad733f985ffc613253c3f0241a
peterneyens <peter.neyens@gmail.com>
2016-04-15 13:55:51 +0200

    Add syntax tests for MonadCombine and rename/move the syntax conversion method and class for `separate`.

--- core/src/main/scala/cats/syntax/monadCombine.scala
+++ core/src/main/scala/cats/syntax/monadCombine.scala
@@ -1,15 +1,13 @@
 package cats
 package syntax
 
-trait MonadCombineSyntax extends MonadCombineSyntax1 {
+trait MonadCombineSyntax {
   // TODO: use simulacrum instances eventually
   implicit def monadCombineSyntax[F[_]: MonadCombine, G[_], A](fga: F[G[A]]): MonadCombineOps[F, G, A] =
     new MonadCombineOps[F, G, A](fga)
-}
 
-private[syntax] trait MonadCombineSyntax1 {
-  implicit def nestedMonadCombineSyntax[F[_]: MonadCombine, G[_, _], A, B](fgab: F[G[A, B]]): NestedMonadCombineOps[F, G, A, B] =
-    new NestedMonadCombineOps[F, G, A, B](fgab)
+  implicit def separateSyntax[F[_]: MonadCombine, G[_, _], A, B](fgab: F[G[A, B]]): SeparateOps[F, G, A, B] =
+    new SeparateOps[F, G, A, B](fgab)
 }
 
 final class MonadCombineOps[F[_], G[_], A](fga: F[G[A]])(implicit F: MonadCombine[F]) {
@@ -30,7 +28,7 @@ final class MonadCombineOps[F[_], G[_], A](fga: F[G[A]])(implicit F: MonadCombin
   def unite(implicit G: Foldable[G]): F[A] = F.unite(fga)
 }
 
-final class NestedMonadCombineOps[F[_], G[_, _], A, B](fgab: F[G[A, B]])(implicit F: MonadCombine[F]) {
+final class SeparateOps[F[_], G[_, _], A, B](fgab: F[G[A, B]])(implicit F: MonadCombine[F]) {
 
   /**
    * @see [[MonadCombine.separate]]
