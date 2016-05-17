1752755e48d00476b9dc899bd33e2361026e1ae0
Julien Richard-Foy <julien@richard-foy.fr>
2016-05-17 15:37:56 +0200

    Use MonadError[Option, Unit] in tests

--- tests/src/test/scala/cats/tests/MonadErrorSuite.scala
+++ tests/src/test/scala/cats/tests/MonadErrorSuite.scala
@@ -1,26 +1,22 @@
 package cats
 package tests
 
-import cats.data.Xor
-
 class MonadErrorSuite extends CatsSuite {
 
-  type ErrorOr[A] = String Xor A
-
-  val successful: ErrorOr[Int] = 42.right
-  val failed: ErrorOr[Int] = "Oops".left
+  val successful: Option[Int] = 42.some
+  val failed: Option[Int] = None
 
   test("ensure raises an error if the predicate fails") {
-    monadErrorSyntax(successful).ensure("Error")(i => false) should === ("Error".left)
+    successful.ensure(())(i => false) should === (None)
   }
 
   test("ensure returns the successful value if the predicate succeeds") {
-    monadErrorSyntax(successful).ensure("Error")(i => true) should === (successful)
+    successful.ensure(())(i => true) should === (successful)
   }
 
   test("ensure returns the failure, when applied to a failure") {
-    monadErrorSyntax(failed).ensure("Error")(i => false) should === (failed)
-    monadErrorSyntax(failed).ensure("Error")(i => true) should === (failed)
+    failed.ensure(())(i => false) should === (failed)
+    failed.ensure(())(i => true) should === (failed)
   }
 
 }
