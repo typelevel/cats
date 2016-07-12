374d0bbc162f8446f8d5ebef9b9f61a00830a4b4
Joseph Abrahamson <me@jspha.com>
2016-07-11 22:38:08 -0400

    better documentation and inlinability

--- core/src/main/scala/cats/eq/Is.scala
+++ core/src/main/scala/cats/eq/Is.scala
@@ -3,61 +3,64 @@ package cats.eq
 import cats.Id
 
 /**
- * A value of `A Is B` witnesses that the types `A` and `B` are the same.
- * You can use this value to coerce `A` into `B` and therefore `A Is B` is
- * similar in feel to `A =:= B`; however, `A Is B` has more computational
- * content and offers greater functionality.
+ * A value of `A Is B` is proof that the types `A` and `B` are the same. More
+ * powerfully, it asserts that they have the same meaning in all type
+ * contexts. This can be a more powerful assertion than `A =:= B` and is more
+ * easily used in manipulation of types while avoiding (potentially
+ * erroneous) coercions.
  *
- * `A Is B` is also known as Leibniz equality and represents computationally
- * the idea that two things are equal if they take equal values on all
- * predicates. In other words, `A` equals `B` when for all predicates `P[_]`
- * we have that `P[A]` implies `P[B]` and visa versa. `A Is B` represents
- * this idea computationally providing exploitable structure to our equalities.
- *
- * The heart of `A Is B` is the `subst` method which converts values `F[A]`
- * into values `F[B]` for any `F[_]`. Creative choices of `F` enable type
- * equalities to be substituted into many more complex type expressions.
+ * `A Is B` is also known as Leibniz equality.
  */
 abstract class Is[A, B] {
   def subst[F[_]](fa: F[A]): F[B]
 
-  final def andThen[C](next: B Is C): A Is C =
+  @inline final def andThen[C](next: B Is C): A Is C =
     next.subst[A Is ?](this)
 
-  final def compose[C](prev: C Is A): C Is B =
+  @inline final def compose[C](prev: C Is A): C Is B =
     prev andThen this
 
-  final def from: B Is A =
+  @inline final def from: B Is A =
     this.subst[? Is A](Is.refl)
 
-  final def lift[F[_]]: F[A] Is F[B] =
+  @inline final def lift[F[_]]: F[A] Is F[B] =
     subst[λ[α => F[A] Is F[α]]](Is.refl)
 
   /**
    * Substitution on identity brings about a direct coercion function of the
    * same form that `=:=` provides.
    */
-  def coerce: A => B =
-    subst[Id]
+  @inline final def coerce(a: A): B =
+    subst[Id](a)
 }
 
 object Is {
+
+  /**
+   * In truth, "all values of `A Is B` are `refl`". `reflAny` is that
+   * single value.
+   */
+  private[this] val reflAny = new Is[Any, Any] {
+    def subst[F[_]](fa: F[Any]) = fa
+  }
 
   /**
    * In normal circumstances the only `Is` value which is available is the
    * computational identity `A Is A` at all types `A`. These "self loops"
    * generate all of the behavior of equality and also ensure that at its
    * heart `A Is B` is always just an identity relation.
+   *
+   * Implementation note: all values of `refl` return the same (private)
+   * instance at whatever type is appropriate to save on allocations.
    */
-  implicit def refl[A]: A Is A = new Is[A, A] {
-    def subst[F[_]](fa: F[A]): F[A] = fa
-  }
+  implicit def refl[A]: A Is A =
+    reflAny.asInstanceOf
 
   /**
    * A value `A Is B` is always sufficient to produce a similar `Predef.=:=`
    * value.
    */
-  implicit def scalaEq[A, B](t: A Is B): A =:= B =
+  implicit def predefEq[A, B](t: A Is B): A =:= B =
     t.subst[A =:= ?](implicitly[A =:= A])
 
 }
