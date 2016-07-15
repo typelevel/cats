f0536445e5c4392fc6d342f0893159a6dce214e1
Cody Allen <ceedubs@gmail.com>
2016-07-15 07:11:29 -0400

    Add bisequence and bimap examples
    This is an attempt to not have net negative code coverage on #1194.

--- core/src/main/scala/cats/Bitraverse.scala
+++ core/src/main/scala/cats/Bitraverse.scala
@@ -9,7 +9,31 @@ trait Bitraverse[F[_, _]] extends Bifoldable[F] with Bifunctor[F] {
   /** Traverse each side of the structure with the given functions */
   def bitraverse[G[_]: Applicative, A, B, C, D](fab: F[A, B])(f: A => G[C], g: B => G[D]): G[F[C, D]]
 
-  /** Sequence each side of the structure with the given functions */
+  /**
+   * Sequence each side of the structure with the given functions.
+   *
+   * Example:
+   * {{{
+   * scala> import cats.data.Xor
+   * scala> import cats.implicits._
+   *
+   * scala> val rightSome: Option[String] Xor Option[Int] = Xor.right(Some(3))
+   * scala> rightSome.bisequence
+   * res0: Option[String Xor Int] = Some(Right(3))
+   *
+   * scala> val rightNone: Option[String] Xor Option[Int] = Xor.right(None)
+   * scala> rightNone.bisequence
+   * res0: Option[String Xor Int] = None
+   *
+   * scala> val leftSome: Option[String] Xor Option[Int] = Xor.left(Some("foo"))
+   * scala> leftSome.bisequence
+   * res0: Option[String Xor Int] = Some(Left(foo))
+   *
+   * scala> val leftNone: Option[String] Xor Option[Int] = Xor.left(None)
+   * scala> leftNone.bisequence
+   * res0: Option[String Xor Int] = None
+   * }}}
+   */
   def bisequence[G[_]: Applicative, A, B](fab: F[G[A], G[B]]): G[F[A, B]]
 
   /** If F and G are both [[cats.Bitraverse]] then so is their composition F[G[_, _], G[_, _]] */
