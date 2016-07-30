d2af644a0f566e871124d96de9e1faf441c182ca
Cody Allen <ceedubs@gmail.com>
2016-07-30 12:11:38 -0400

    Use Simulacrum in arrow package

--- core/src/main/scala/cats/syntax/split.scala
+++ core/src/main/scala/cats/syntax/split.scala
@@ -3,12 +3,4 @@ package syntax
 
 import cats.arrow.Split
 
-trait SplitSyntax {
-  // TODO: use simulacrum instances eventually
-  implicit def catsSyntaxSplit[F[_, _]: Split, A, B](fab: F[A, B]): SplitOps[F, A, B] =
-    new SplitOps[F, A, B](fab)
-}
-
-final class SplitOps[F[_, _], A, B](fab: F[A, B])(implicit F: Split[F]) {
-  def split[C, D](fcd: F[C, D]): F[(A, C), (B, D)] = F.split(fab, fcd)
-}
+trait SplitSyntax extends Split.ToSplitOps
--- core/src/main/scala/cats/syntax/strong.scala
+++ core/src/main/scala/cats/syntax/strong.scala
@@ -3,13 +3,4 @@ package syntax
 
 import cats.functor.Strong
 
-trait StrongSyntax {
-  // TODO: use simulacrum instances eventually
-  implicit def catsSyntaxStrong[F[_, _]: Strong, A, B](fab: F[A, B]): StrongOps[F, A, B] =
-    new StrongOps[F, A, B](fab)
-}
-
-final class StrongOps[F[_, _], A, B](fab: F[A, B])(implicit F: Strong[F]) {
-  def first[C]: F[(A, C), (B, C)] = F.first(fab)
-  def second[C]: F[(C, A), (C, B)] = F.second(fab)
-}
+trait StrongSyntax extends Strong.ToStrongOps
