2261797ec737916863f3c8d011ef02c032a21a37
zainab-ali <zainab.ali.london@gmail.com>
2016-08-03 19:32:44 +0100

    Removing rightMap from Bifunctor

--- core/src/main/scala/cats/functor/Bifunctor.scala
+++ core/src/main/scala/cats/functor/Bifunctor.scala
@@ -19,11 +19,6 @@ trait Bifunctor[F[_, _]] extends Any with Serializable { self =>
    */
   def leftMap[A, B, C](fab: F[A, B])(f: A => C): F[C, B] = bimap(fab)(f, identity)
 
-  /**
-   * apply a function ro the "right" functor
-   */
-  def rightMap[A, B, C](fab: F[A, B])(f: B => C): F[A, C] = bimap(fab)(identity, f)
-
   /** The composition of two Bifunctors is itself a Bifunctor */
   def compose[G[_, _]](implicit G0: Bifunctor[G]): Bifunctor[λ[(α, β) => F[G[α, β], G[α, β]]]] =
     new ComposedBifunctor[F, G] {
