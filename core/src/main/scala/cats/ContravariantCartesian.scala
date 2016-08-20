0851b76c3c04fbfe3069230387461015cba16562
Oscar Boykin <oscar@stripe.com>
2016-08-20 06:58:36 -1000

    remove unused and untested code

--- core/src/main/scala/cats/ContravariantCartesian.scala
+++ core/src/main/scala/cats/ContravariantCartesian.scala
@@ -17,17 +17,10 @@ import simulacrum.typeclass
 
 object ContravariantCartesian extends KernelContravariantCartesianInstances
 
-private[cats] sealed trait KernelContravariantCartesianInstances extends ContravariantCartesian1 {
+private[cats] sealed trait KernelContravariantCartesianInstances {
   implicit val catsContravariantCartesianEq: ContravariantCartesian[Eq] = new ContravariantCartesian[Eq] {
     def contramap[A, B](fa: Eq[A])(fn: B => A): Eq[B] = fa.on(fn)
     def product[A, B](fa: Eq[A], fb: Eq[B]): Eq[(A, B)] =
       Eq.instance { (left, right) => fa.eqv(left._1, right._1) && fb.eqv(left._2, right._2) }
-  }
-}
-
-private[cats] sealed trait ContravariantCartesian1 {
-  implicit def catsContravariantCartesianFromEach[F[_]](implicit contra: Contravariant[F], cart: Cartesian[F]): ContravariantCartesian[F] = new ContravariantCartesian[F] {
-    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = cart.product(fa, fb)
-    def contramap[A, B](fa: F[A])(fn: B => A): F[B] = contra.contramap(fa)(fn)
   }
 }
