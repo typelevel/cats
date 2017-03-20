8fbf5462afe8ae9ede5cfbbed4d26f14ffe1e7b5
Greg Pfeil <greg@technomadic.org>
2017-03-20 07:38:23 -0600

    Improvements to Inject. (#1557)
    * Improvements to Inject.
    
    - move it back to cats-core (near `Coproduct`)
    - make `inj`/`prj` natural transformations
    - add `apply`/`unapply` (lowered `inj`/`prj`)
    - define `Free.roll`
    - move `injectRoll` (née `Inject.inject`) and `match_` to `Free`
    - reintroduce "null identity" test (written by @edmundnoble)
    
    * Fix tut breakage, and remove commented import.
    
    * Remove an unused import.
    
    * Change to not require syntax.either.
    
    Otherwise we need it for 2.10 & 2.11 and need to _not_ have it for 2.12.
    
    * Add tests for Inject#apply and #unapply.

--- free/src/main/scala/cats/free/package.scala
+++ free/src/main/scala/cats/free/package.scala
@@ -4,11 +4,4 @@ package object free {
   /** Alias for the free monad over the `Function0` functor. */
   type Trampoline[A] = Free[Function0, A]
   object Trampoline extends TrampolineFunctions
-
-  /** [[cats.free.Inject]][F, G] */
-  type :<:[F[_], G[_]] = Inject[F, G]
-
-  /** [[cats.free.Inject]][F, G] */
-  type :≺:[F[_], G[_]] = Inject[F, G]
-
 }
