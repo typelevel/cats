22fd6b0d78971cec20d4f582eacd65b584772c42
Julien Truffaut <truffaut.julien@gmail.com>
2017-03-27 14:22:12 +0100

    add last, sortBy and sorted to NonEmptyList

--- core/src/main/scala/cats/data/NonEmptyList.scala
+++ core/src/main/scala/cats/data/NonEmptyList.scala
@@ -20,6 +20,8 @@ final case class NonEmptyList[+A](head: A, tail: List[A]) {
    */
   def toList: List[A] = head :: tail
 
+  def last: A = tail.lastOption.getOrElse(head)
+
   /**
    *  Applies f to all the elements of the structure
    */
@@ -217,6 +219,38 @@ final case class NonEmptyList[+A](head: A, tail: List[A]) {
     }
     NonEmptyList((head, 0), bldr.result)
   }
+
+  /**
+    * Sorts this `NonEmptyList` according to an `Order` on transformed `B` from `A`
+    *
+    * {{{
+    * scala> import cats.data.NonEmptyList
+    * scala> import cats.instances.int._
+    * scala> val nel = NonEmptyList.of(('a', 4), ('z', 1), ('e', 22))
+    * scala> nel.sortBy(_._2)
+    * res0: cats.data.NonEmptyList[(Char, Int)] = NonEmptyList((z,1), (a,4), (e,22))
+    * }}}
+    */
+  def sortBy[B: Order](f: A => B): NonEmptyList[A] = {
+    toList.sortBy(f)(Order[B].toOrdering) match {
+      case x :: xs => NonEmptyList(x, xs)
+      case Nil     => sys.error("absurd")
+    }
+  }
+
+  /**
+    * Sorts this `NonEmptyList` according to an `Order`
+    *
+    * {{{
+    * scala> import cats.data.NonEmptyList
+    * scala> import cats.instances.int._
+    * scala> val nel = NonEmptyList.of(12, 4, 3, 9)
+    * scala> nel.sorted
+    * res0: cats.data.NonEmptyList[Int] = NonEmptyList(3, 4, 9, 12)
+    * }}}
+    */
+  def sorted[AA >: A](implicit AA: Order[AA]): NonEmptyList[AA] =
+    sortBy(a => a: AA)
 
   /**
     * Groups elements inside of this `NonEmptyList` using a mapping function
