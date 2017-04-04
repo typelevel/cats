e77bb99721127cb975817d9e2627a514af87047b
Leandro Bolivar <leandrob13@users.noreply.github.com>
2017-04-04 04:24:55 -0500

    Ior syntax (#1540)
    * Added Ior syntax
    
    * Removed examples on ior syntax comments
    
    * Removed toValidatedNel functions
    
    * Added syntax to list for IorNel and ValidatedNel
    
    * Added test for toValidated function
    
    * Added newline to end of ior.scala file
    
    * Added toIor functions to option syntax
    
    * Added fromIor to ValidatedFunctions and toIor function to Validated datatype
    
    * Corrected error on duplicated implicit defs on syntax.ior.scala
    
    * Added tests for new functions in Validated and Ior
    
    * Added examples to list syntax functions
    
    * Corrected errors on list syntax examples
    
    * Added corrected examples to ior syntax
    
    * Corrected names for IorNel syntax functions
    
    * Redifined the IorNel type alias
    
    * Fixed test descriptions on IorTests
    
    * Removed some functions from ior syntax object, deleted functions from list syntax object

--- core/src/main/scala/cats/syntax/list.scala
+++ core/src/main/scala/cats/syntax/list.scala
@@ -8,6 +8,24 @@ trait ListSyntax {
 }
 
 final class ListOps[A](val la: List[A]) extends AnyVal {
+
+  /**
+    * Returns an Option of NonEmptyList from a List
+    *
+    * Example:
+    * {{{
+    * scala> import cats.data.NonEmptyList
+    * scala> import cats.implicits._
+    *
+    * scala> val result1: List[Int] = List(1, 2)
+    * scala> result1.toNel
+    * res0: Option[NonEmptyList[Int]] = Some(NonEmptyList(1, 2))
+    *
+    * scala> val result2: List[Int] = List.empty[Int]
+    * scala> result2.toNel
+    * res1: Option[NonEmptyList[Int]] = None
+    * }}}
+    */
   def toNel: Option[NonEmptyList[A]] = NonEmptyList.fromList(la)
   def groupByNel[B](f: A => B): Map[B, NonEmptyList[A]] =
     toNel.fold(Map.empty[B, NonEmptyList[A]])(_.groupBy(f))
--- core/src/main/scala/cats/syntax/package.scala
+++ core/src/main/scala/cats/syntax/package.scala
@@ -22,6 +22,7 @@ package object syntax {
   object functorFilter extends FunctorFilterSyntax
   object group extends GroupSyntax
   object invariant extends InvariantSyntax
+  object ior extends IorSyntax
   object list extends ListSyntax
   object monad extends MonadSyntax
   object monadCombine extends MonadCombineSyntax
