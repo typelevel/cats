0ea5608d72f33e5c8d40b19866d6b9537fda5a01
Kailuo Wang <kailuo.wang@gmail.com>
2017-04-07 08:02:22 -0400

    removed unused imports

--- core/src/main/scala/cats/instances/bitSet.scala
+++ core/src/main/scala/cats/instances/bitSet.scala
@@ -1,8 +1,6 @@
 package cats.instances
 
 import scala.collection.immutable.BitSet
-import int.catsStdShowForInt
-import cats.syntax.show._
 import cats.Show
 
 trait BitSetInstances extends cats.kernel.instances.BitSetInstances {
