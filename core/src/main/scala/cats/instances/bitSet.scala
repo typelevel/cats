6e3ed2e26647a76109251f8dfb7595da0a1dc4a8
Kailuo Wang <kailuo.wang@gmail.com>
2017-04-06 22:29:08 -0400

    unnecessary show implementation

--- core/src/main/scala/cats/instances/bitSet.scala
+++ core/src/main/scala/cats/instances/bitSet.scala
@@ -6,8 +6,5 @@ import cats.syntax.show._
 import cats.Show
 
 trait BitSetInstances extends cats.kernel.instances.BitSetInstances {
-  implicit def catsStdShowForBitSet: Show[BitSet] = new Show[BitSet] {
-    def show(fa: BitSet): String =
-      fa.toIterator.map(_.show).mkString("BitSet(", ", ", ")")
-  }
+  implicit def catsStdShowForBitSet: Show[BitSet] = Show.fromToString[BitSet]
 }
