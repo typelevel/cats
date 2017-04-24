35dc00c560f4e4a7b0d731fc73e58bc0deb6de59
wedens <kirill.wedens@gmail.com>
2017-04-24 21:17:11 +0700

    fix doc style

--- core/src/main/scala/cats/MonadTrans.scala
+++ core/src/main/scala/cats/MonadTrans.scala
@@ -1,13 +1,14 @@
 package cats
 
 /**
-  * A type class which abstracts over the ability to lift an M[A] into a
-  * MonadTransformer
-  */
+ * A type class which abstracts over the ability to lift an M[A] into a
+ * MonadTransformer
+ */
 trait MonadTrans[MT[_[_], _]] {
+
   /**
-    * Lift a value of type M[A] into a monad transformer MT[M, A]
-    */
+   * Lift a value of type M[A] into a monad transformer MT[M, A]
+   */
   def liftT[M[_]: Monad, A](ma: M[A]): MT[M, A]
 }
 
