20f5e27615d577145dd59de04f207e2089aa54b4
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-05-05 17:23:13 +0200

    Add ensure and ensureOr laws to MonadError

--- laws/src/main/scala/cats/laws/MonadErrorLaws.scala
+++ laws/src/main/scala/cats/laws/MonadErrorLaws.scala
@@ -7,6 +7,12 @@ trait MonadErrorLaws[F[_], E] extends ApplicativeErrorLaws[F, E] with MonadLaws[
 
   def monadErrorLeftZero[A, B](e: E, f: A => F[B]): IsEq[F[B]] =
     F.flatMap(F.raiseError[A](e))(f) <-> F.raiseError[B](e)
+
+  def monadErrorEnsureConsistency[A](fa: F[A], e: E, p: A => Boolean): IsEq[F[A]] =
+    F.ensure(fa)(e)(p) <-> F.flatMap(fa)(a => if (p(a)) F.pure(a) else F.raiseError(e))
+
+  def monadErrorEnsureOrConsistency[A](fa: F[A], e: A => E, p: A => Boolean): IsEq[F[A]] =
+    F.ensureOr(fa)(e)(p) <-> F.flatMap(fa)(a => if (p(a)) F.pure(a) else F.raiseError(e(a)))
 }
 
 object MonadErrorLaws {
