aa5d6aacbb16863d8d99f0a43fd9d0696ecd61bd
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-05-28 17:19:45 +0200

    Add Law tests for traverse1 instances of NonEmptyList and Vector

--- tests/src/test/scala/cats/tests/NonEmptyVectorTests.scala
+++ tests/src/test/scala/cats/tests/NonEmptyVectorTests.scala
@@ -6,7 +6,7 @@ import catalysts.Platform
 import cats.kernel.laws.{GroupLaws, OrderLaws}
 
 import cats.data.NonEmptyVector
-import cats.laws.discipline.{ComonadTests, SemigroupKTests, FoldableTests, SerializableTests, TraverseTests, ReducibleTests, MonadTests}
+import cats.laws.discipline.{ComonadTests, SemigroupKTests, FoldableTests, SerializableTests, TraverseTests, Traverse1Tests, ReducibleTests, MonadTests}
 import cats.laws.discipline.arbitrary._
 
 import scala.util.Properties
@@ -19,6 +19,7 @@ class NonEmptyVectorTests extends CatsSuite {
   checkAll("NonEmptyVector[Int]", OrderLaws[NonEmptyVector[Int]].eqv)
 
   checkAll("NonEmptyVector[Int] with Option", TraverseTests[NonEmptyVector].traverse[Int, Int, Int, Int, Option, Option])
+  checkAll("NonEmptyVector[Int] with Option", Traverse1Tests[NonEmptyVector].traverse1[Option, Int, Int, Int, Int, Option, Option])
   checkAll("Traverse[NonEmptyVector[A]]", SerializableTests.serializable(Traverse[NonEmptyVector]))
 
   checkAll("NonEmptyVector[Int]", ReducibleTests[NonEmptyVector].reducible[Option, Int, Int])
