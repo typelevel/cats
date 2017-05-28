a2bbee4a95c9aa7f082cee80079226b5479c5acc
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-05-28 21:30:28 +0200

    Fix Indentation

--- laws/src/main/scala/cats/laws/discipline/Traverse1Tests.scala
+++ laws/src/main/scala/cats/laws/discipline/Traverse1Tests.scala
@@ -11,30 +11,30 @@ trait Traverse1Tests[F[_]] extends TraverseTests[F] with ReducibleTests[F] {
   def laws: Traverse1Laws[F]
 
   def traverse1[G[_]: Applicative, A: Arbitrary, B: Arbitrary, C: Arbitrary, M: Arbitrary, X[_]: Applicative, Y[_]: Applicative](implicit
-                                                                                                             ArbFA: Arbitrary[F[A]],
-                                                                                                             ArbXB: Arbitrary[X[B]],
-                                                                                                             ArbYB: Arbitrary[Y[B]],
-                                                                                                             ArbYC: Arbitrary[Y[C]],
-                                                                                                             ArbFB: Arbitrary[F[B]],
-                                                                                                             ArbFGA: Arbitrary[F[G[A]]],
-                                                                                                             ArbGB: Arbitrary[G[B]],
-                                                                                                             CogenA: Cogen[A],
-                                                                                                             CogenB: Cogen[B],
-                                                                                                             CogenC: Cogen[C],
-                                                                                                             CogenM: Cogen[M],
-                                                                                                             M: Monoid[M],
-                                                                                                             MB: Monoid[B],
-                                                                                                             EqFA: Eq[F[A]],
-                                                                                                             EqFC: Eq[F[C]],
-                                                                                                             EqG: Eq[G[Unit]],
-                                                                                                             EqM: Eq[M],
-                                                                                                             EqA: Eq[A],
-                                                                                                             EqB: Eq[B],
-                                                                                                             EqXYFC: Eq[X[Y[F[C]]]],
-                                                                                                             EqXFB: Eq[X[F[B]]],
-                                                                                                             EqYFB: Eq[Y[F[B]]],
-                                                                                                             EqOptionA: Eq[Option[A]]
-                                                                                                            ): RuleSet = {
+    ArbFA: Arbitrary[F[A]],
+    ArbXB: Arbitrary[X[B]],
+    ArbYB: Arbitrary[Y[B]],
+    ArbYC: Arbitrary[Y[C]],
+    ArbFB: Arbitrary[F[B]],
+    ArbFGA: Arbitrary[F[G[A]]],
+    ArbGB: Arbitrary[G[B]],
+    CogenA: Cogen[A],
+    CogenB: Cogen[B],
+    CogenC: Cogen[C],
+    CogenM: Cogen[M],
+    M: Monoid[M],
+    MB: Monoid[B],
+    EqFA: Eq[F[A]],
+    EqFC: Eq[F[C]],
+    EqG: Eq[G[Unit]],
+    EqM: Eq[M],
+    EqA: Eq[A],
+    EqB: Eq[B],
+    EqXYFC: Eq[X[Y[F[C]]]],
+    EqXFB: Eq[X[F[B]]],
+    EqYFB: Eq[Y[F[B]]],
+    EqOptionA: Eq[Option[A]]
+  ): RuleSet = {
     implicit def EqXFBYFB : Eq[(X[F[B]], Y[F[B]])] = new Eq[(X[F[B]], Y[F[B]])] {
       override def eqv(x: (X[F[B]], Y[F[B]]), y: (X[F[B]], Y[F[B]])): Boolean =
         EqXFB.eqv(x._1, y._1) && EqYFB.eqv(x._2, y._2)
