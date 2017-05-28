f437a9825221fa8c014dd90e6614b957c81c2753
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-05-28 15:05:23 +0200

    Remove Unapply syntax

--- core/src/main/scala/cats/syntax/reducible.scala
+++ core/src/main/scala/cats/syntax/reducible.scala
@@ -1,12 +1,13 @@
 package cats
 package syntax
 
-private[syntax] trait ReducibleSyntax1 {
-  implicit def catsSyntaxUReducible[FA](fa: FA)(implicit U: Unapply[Reducible, FA]): Reducible.Ops[U.M, U.A] =
-    new Reducible.Ops[U.M, U.A] {
-      val self = U.subst(fa)
-      val typeClassInstance = U.TC
-    }
+
+trait ReducibleSyntax extends Reducible.ToReducibleOps {
+  implicit final def catsSyntaxNestedReducible[F[_]: Reducible, G[_], A](fga: F[G[A]]): NestedReducibleOps[F, G, A] =
+    new NestedReducibleOps[F, G, A](fga)
 }
 
-trait ReducibleSyntax extends Reducible.ToReducibleOps with ReducibleSyntax1
+final class NestedReducibleOps[F[_], G[_], A](val fga: F[G[A]]) extends AnyVal {
+  def reduceK(implicit F: Reducible[F], G: SemigroupK[G]): G[A] = F.reduceK(fga)
+}
+
--- core/src/main/scala/cats/syntax/traverse1.scala
+++ core/src/main/scala/cats/syntax/traverse1.scala
@@ -1,11 +1,4 @@
 package cats
 package syntax
 
-trait Traverse1Syntax extends Traverse1.ToTraverse1Ops {
-  implicit def catsSyntaxNestedTraverse1[F[_]: Traverse1, G[_], A](fga: F[G[A]]): NestedTraverse1Ops[F, G, A] =
-    new NestedTraverse1Ops[F, G, A](fga)
-}
-
-final class NestedTraverse1Ops[F[_], G[_], A](fga: F[G[A]])(implicit F: Traverse1[F]) {
-  def reduceK(implicit G: SemigroupK[G]): G[A] = F.reduceK(fga)
-}
+trait Traverse1Syntax extends Traverse1.ToTraverse1Ops
