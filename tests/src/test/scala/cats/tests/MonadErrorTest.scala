340157b8db36c66ac11d6e87d6c881b9b0f8c43c
Daniel Spiewak <djspiewak@gmail.com>
2017-06-04 14:43:40 -0600

    Added tests for MonadError#guarantee

--- tests/src/test/scala/cats/tests/MonadErrorTest.scala
+++ tests/src/test/scala/cats/tests/MonadErrorTest.scala
@@ -1,7 +1,11 @@
 package cats
 package tests
 
-class MonadErrorSuite extends CatsSuite {
+import data.StateT
+
+import scala.language.postfixOps
+
+class MonadErrorTest extends CatsSuite {
 
   val successful: Option[Int] = 42.some
   val failed: Option[Int] = None
@@ -32,5 +36,31 @@ class MonadErrorSuite extends CatsSuite {
     failed.ensureOr(_ => ())(_ => true) should === (failed)
   }
 
+  {
+    import StateTTests._
 
+    type Test[A] = StateT[Either[Boolean, ?], Int, A]
+
+    val successful: Test[String] = StateT.modify[Either[Boolean, ?], Int](1 +).map(_ => "foo")
+    val failed1: Test[String] = StateT.lift(Left(true))
+    val failed2: Test[String] = StateT.lift(Left(false))
+    val finalizer: Test[Unit] = StateT.modify[Either[Boolean, ?], Int](10 *)
+
+    test("guarantee returns successful") {
+      successful.guarantee(finalizer) should === (successful.flatMap(a => finalizer.map(_ => a)))
+    }
+
+    test("guarantee runs finalizer on fail") {
+      val expected: Test[String] = for {
+        _ <- StateT.modify[Either[Boolean, ?], Int](10 *)
+        _ <- failed1
+      } yield "foo"
+
+      failed1.guarantee(finalizer) should === (expected)
+    }
+
+    test("guarantee returns inner errors on double failure") {
+      failed1.guarantee(failed2 >> ().pure[Test]) should === (failed2)
+    }
+  }
 }
