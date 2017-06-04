a8806ee6e21707a446b2b5bbba5307cb7038a19c
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2017-06-04 19:00:16 +0100

    Cats-1567 Add Commutative Arrows
    We add a type-class of commutative arrows, which are those in which
    the `split` operation is commutative (can pair in any order).

---
+++ laws/src/main/scala/cats/laws/CommutativeArrowLaws.scala
@@ -0,0 +1,22 @@
+package cats
+package laws
+
+import cats.arrow.CommutativeArrow
+import cats.syntax.compose._
+import cats.syntax.strong._
+
+/** Reference: "Causal Commutative Arrows", Journal of Functional Programming
+  *  Figure 4.
+  */
+trait CommutativeArrowLaws[F[_, _]] extends ArrowLaws[F] {
+  implicit override def F: CommutativeArrow[F]
+
+  def arrowCommutative[A, B, C, D](f: F[A, B], g: F[C, D]): IsEq[F[(A, C), (B, D)]] =
+    (f.first[C] >>> g.second[B]) <-> (g.second[A] >>> f.first[D])
+
+}
+
+object CommutativeArrowLaws {
+  def apply[F[_, _]](implicit ev: CommutativeArrow[F]): CommutativeArrowLaws[F] =
+    new CommutativeArrowLaws[F] { def F: CommutativeArrow[F] = ev }
+}
