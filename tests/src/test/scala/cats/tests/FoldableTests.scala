e2f53aed10e97c2fe09d799491b21e45c386e7f8
Kailuo Wang <kailuo.wang@gmail.com>
2017-06-23 16:04:24 -0400

    use default foldM implemenation to test short-circuit

--- tests/src/test/scala/cats/tests/FoldableTests.scala
+++ tests/src/test/scala/cats/tests/FoldableTests.scala
@@ -197,22 +197,39 @@ class FoldableTestsAdditional extends CatsSuite {
     }
     assert(result.value)
 
+    // test trampolining
+    val large = Stream((1 to 10000): _*)
+    assert(contains(large, 10000).value)
+
+    // test laziness of foldM
+    dangerous.foldM(0)((acc, a) => if (a < 2) Some(acc + a) else None) should === (None)
 
   }
 
+  def foldableStreamWithDefaultImpl = new Foldable[Stream] {
+    def foldLeft[A, B](fa: Stream[A], b: B)(f: (B, A) => B): B =
+      instances.stream.catsStdInstancesForStream.foldLeft(fa, b)(f)
+
+    def foldRight[A, B](fa: Stream[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+      instances.stream.catsStdInstancesForStream.foldRight(fa, lb)(f)
+  }
+
   test(".foldLeftM short-circuiting") {
+    implicit val F = foldableStreamWithDefaultImpl
     val ns = Stream.continually(1)
-    val res = Foldable[Stream].foldLeftM[Either[Int, ?], Int, Int](ns, 0) { (sum, n) =>
+    val res = F.foldLeftM[Either[Int, ?], Int, Int](ns, 0) { (sum, n) =>
       if (sum >= 100000) Left(sum) else Right(sum + n)
     }
     assert(res == Left(100000))
   }
 
   test(".foldLeftM short-circuiting optimality") {
+    implicit val F = foldableStreamWithDefaultImpl
+
     // test that no more elements are evaluated than absolutely necessary
 
     def concatUntil(ss: Stream[String], stop: String): Either[String, String] =
-      Foldable[Stream].foldLeftM[Either[String, ?], String, String](ss, "") { (acc, s) =>
+      F.foldLeftM[Either[String, ?], String, String](ss, "") { (acc, s) =>
         if (s == stop) Left(acc) else Right(acc + s)
       }
 
