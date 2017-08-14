14e16950c9b2fdfac18ca191ddeedfa4624d1673
David R. Bild <david@davidbild.org>
2017-08-14 10:46:36 -0500

    Limit runtime of monad loop tests

--- tests/src/test/scala/cats/tests/MonadTest.scala
+++ tests/src/test/scala/cats/tests/MonadTest.scala
@@ -7,18 +7,20 @@ import org.scalacheck.Gen
 class MonadTest extends CatsSuite {
   implicit val testInstance: Monad[StateT[Id, Int, ?]] = StateT.catsDataMonadForStateT[Id, Int]
 
+  val smallPosInt = Gen.choose(1, 5000)
+
   val increment: StateT[Id, Int, Unit] = StateT.modify(_ + 1)
   val incrementAndGet: StateT[Id, Int, Int] = increment >> StateT.get
 
   test("whileM_") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, _) = increment.whileM_(StateT.inspect(i => !(i >= max))).run(0)
       result should ===(Math.max(0, max))
     }
   }
 
   test("whileM") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, aggregation) = incrementAndGet.whileM[Vector](StateT.inspect(i => !(i >= max))).run(0)
       result should ===(Math.max(0, max))
       aggregation should === ( if(max > 0) (1 to max).toVector else Vector.empty )
@@ -26,14 +28,14 @@ class MonadTest extends CatsSuite {
   }
 
   test("untilM_") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, _) = increment.untilM_(StateT.inspect(_ >= max)).run(-1)
       result should ===(max)
     }
   }
 
   test("untilM") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, aggregation) = incrementAndGet.untilM[Vector](StateT.inspect(_ >= max)).run(-1)
       result should ===(max)
       aggregation should === ((0 to max).toVector)
@@ -51,7 +53,7 @@ class MonadTest extends CatsSuite {
   }
 
   test("iterateWhile") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, _) = incrementAndGet.iterateWhile(_ < max).run(-1)
       result should ===(Math.max(0, max))
     }
@@ -63,7 +65,7 @@ class MonadTest extends CatsSuite {
   }
 
   test("iterateUntil") {
-    forAll(Gen.posNum[Int]) { (max: Int) =>
+    forAll(smallPosInt) { (max: Int) =>
       val (result, _) = incrementAndGet.iterateUntil(_ == max).run(-1)
       result should ===(Math.max(0, max))
     }
