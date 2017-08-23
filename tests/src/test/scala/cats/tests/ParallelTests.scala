425955484c0201850e118e7ab96beebda7ce3b33
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-08-23 10:24:30 +0200

    Add more tests

--- tests/src/test/scala/cats/tests/ParallelTests.scala
+++ tests/src/test/scala/cats/tests/ParallelTests.scala
@@ -18,6 +18,25 @@ class ParallelTests extends CatsSuite {
     }
   }
 
+  test("ParTraverse identity should be equivalent to parSequence") {
+    forAll { es: List[Either[String, Int]] =>
+      (es.parTraverse(identity)) should === (es.parSequence)
+    }
+  }
+
+  test("ParTraverse_ identity should be equivalent to parSequence_") {
+    forAll { es: Set[Either[String, Int]] =>
+      (Parallel.parTraverse_(es)(identity)) should === (Parallel.parSequence_(es))
+    }
+  }
+
+  test("parAp2 accumulates errors in order") {
+    val plus = (_: Int) + (_: Int)
+    val rightPlus: Either[String, (Int, Int) => Int] = Right(plus)
+    Parallel.parAp2(rightPlus)("Hello".asLeft, "World".asLeft) should === (Left("HelloWorld"))
+  }
+
+
   checkAll("Parallel[Either[String, ?], Validated[String, ?]]", ParallelTypeclassTests[Either[String, ?], Validated[String, ?], Int].parallel)
 
 }
