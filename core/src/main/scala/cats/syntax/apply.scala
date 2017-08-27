16117c4e947d90a4c29c54ffb477be54ea9a43ee
Alistair Johnson <alistair.johnson@johnsonusm.com>
2017-08-27 18:58:07 +0200

    Set simulacrum version to 0.11.0

--- core/src/main/scala/cats/syntax/apply.scala
+++ core/src/main/scala/cats/syntax/apply.scala
@@ -4,6 +4,8 @@ package syntax
 trait ApplySyntax extends TupleCartesianSyntax {
   implicit final def catsSyntaxApply[F[_], A](fa: F[A])(implicit F: Apply[F]): Apply.Ops[F, A] =
     new Apply.Ops[F, A] {
+      type TypeClassType = Apply[F]
+
       val self = fa
       val typeClassInstance = F
     }
