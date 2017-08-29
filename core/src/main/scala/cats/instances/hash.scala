4cedb1ffddbd2163b036bfcdcc9eb5e375f6d246
ctongfei <ctongfei@gmail.com>
2017-08-28 23:48:33 -0400

    Optimize hash computation on case classes without allocating a new object

--- core/src/main/scala/cats/instances/hash.scala
+++ core/src/main/scala/cats/instances/hash.scala
@@ -9,10 +9,9 @@ import cats.functor._
 trait HashInstances {
   implicit val catsFunctorContravariantForHash: Contravariant[Hash] =
     new Contravariant[Hash] {
-      /** Derive an `Hash` for `B` given an `Hash[A]` and a function `B => A`.
-       *
-       * Note: resulting instances are law-abiding only when the functions used are injective (represent a one-to-one mapping)
+      /**
+       * Derive an `Hash` for `B` given an `Hash[A]` and a function `B => A`.
        */
-      def contramap[A, B](fa: Hash[A])(f: B => A): Hash[B] = Hash.by(f)(fa)
+      def contramap[A, B](ha: Hash[A])(f: B => A): Hash[B] = Hash.by(f)(ha)
     }
 }
