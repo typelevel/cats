b9b2264cdcc1f46239cf81469cb3c555a7cc89b8
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-09-21 10:45:35 +0200

    Remove unused machinery

--- kernel-laws/src/main/scala/cats/kernel/laws/discipline/package.scala
+++ kernel-laws/src/main/scala/cats/kernel/laws/discipline/package.scala
@@ -4,6 +4,6 @@ import cats.kernel.Eq
 import org.scalacheck.Prop
 
 package object discipline {
-  implicit def catsLawsIsEqToProp[A: Eq](isEq: IsEq[A]): Prop =
-    isEq.lhs ?== isEq.rhs
+  implicit def catsLawsIsEqToProp[A: Eq](isEq: IsEq[A])(implicit ev: Eq[A]): Prop =
+    ev.eqv(isEq.lhs, isEq.rhs)
 }
