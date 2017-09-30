965f9c26edb58702173b997301f472e8c0343da7
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-09-30 18:46:23 +0200

    Add unordered traversal for Sets

--- core/src/main/scala/cats/CommutativeApplicative.scala
+++ core/src/main/scala/cats/CommutativeApplicative.scala
@@ -12,3 +12,18 @@ import simulacrum.typeclass
   * Must obey the laws defined in cats.laws.CommutativeApplicativeLaws.
   */
 @typeclass trait CommutativeApplicative[F[_]] extends Applicative[F] with CommutativeApply[F]
+
+
+object CommutativeApplicative {
+
+  def traverseUnordered[F[_]: CommutativeApplicative, A, B](sa: Set[A])(f: A => F[B]): F[Set[B]] =
+    sa.foldLeft(Applicative[F].pure(Set.empty[B])) { (buf, a) =>
+      Applicative[F].map2(buf, f(a))(_ + _)
+    }
+
+  def sequenceUnordered[F[_]: CommutativeApplicative, A](sa: Set[F[A]]): F[Set[A]] =
+    sa.foldLeft(Applicative[F].pure(Set.empty[A])) { (acc, a) =>
+      Applicative[F].map2(acc, a)(_ + _)
+    }
+
+}
