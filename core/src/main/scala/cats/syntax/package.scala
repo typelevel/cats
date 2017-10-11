e7a641356ea32b24a3c4d50f8db0a65676f8e2be
Kai(luo) Wang <kailuo.wang@gmail.com>
2017-10-11 09:29:42 -0400

    added a deprecated cartesian syntax back

--- core/src/main/scala/cats/syntax/package.scala
+++ core/src/main/scala/cats/syntax/package.scala
@@ -10,6 +10,8 @@ package object syntax {
   object bifunctor extends BifunctorSyntax
   object bifoldable extends BifoldableSyntax
   object bitraverse extends BitraverseSyntax
+  @deprecated("use cats.syntax.semigroupal instead", "1.0.0-RC1") 
+  object cartesian extends SemigroupalSyntax
   object coflatMap extends CoflatMapSyntax
   object eitherK extends EitherKSyntax
   object comonad extends ComonadSyntax
