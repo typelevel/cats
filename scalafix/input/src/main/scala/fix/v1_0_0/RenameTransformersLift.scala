be784fa8d8fd2a36979b3d2507d57e58ab86d7b6
fabio labella <fabio.labella@ncr.com>
2017-11-21 16:59:21 +0000

    Add scalafix to rename `lift` to `liftF`

---
+++ scalafix/input/src/main/scala/fix/v1_0_0/RenameTransformersLift.scala
@@ -0,0 +1,17 @@
+/*
+rule = "scala:fix.v1_0_0.RenameTransformersLift"
+ */
+package fix
+package to1_0_0
+
+object RenameTransformersLiftTests {
+  import cats.instances.option._
+  import cats.instances.string._
+  import cats.data.{Kleisli, StateT, RWST, WriterT}
+
+  val fa: Option[Int] = Some(42)
+  val k: Kleisli[Option, Nothing, Int] = Kleisli.lift(fa)
+  val w: WriterT[Option, String, Int] = WriterT.lift(fa)
+  val s: StateT[Option, Nothing, Int] = StateT.lift(fa)
+  val rws: RWST[Option, Nothing, String, Nothing, Int] = RWST.lift(fa)
+}
---
+++ scalafix/output/src/main/scala/fix/v1_0_0/RenameTransformersLift.scala
@@ -0,0 +1,17 @@
+/*
+rule = "scala:fix.v1_0_0.RenameTransformersLift"
+ */
+package fix
+package to1_0_0
+
+object RenameTransformersLiftTests {
+  import cats.instances.option._
+  import cats.instances.string._
+  import cats.data.{Kleisli, StateT, RWST, WriterT}
+
+  val fa: Option[Int] = Some(42)
+  val k: Kleisli[Option, Nothing, Int] = Kleisli.liftF(fa)
+  val w: WriterT[Option, String, Int] = WriterT.liftF(fa)
+  val s: StateT[Option, Nothing, Int] = StateT.liftF(fa)
+  val rws: RWST[Option, Nothing, String, Nothing, Int] = RWST.liftF(fa)
+}
