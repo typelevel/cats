76915e6e66b7cc4eeebca8c525bdc3f8e3763368
Colt Frederickson <coltfred@gmail.com>
2017-11-30 17:34:09 -0700

    Move Functor requirement off the class

--- core/src/main/scala/cats/syntax/DistributiveSyntax.scala
+++ core/src/main/scala/cats/syntax/DistributiveSyntax.scala
@@ -8,7 +8,7 @@ trait DistributiveSyntax extends Distributive.ToDistributiveOps {
 }
 
 // Add syntax to functor as part of importing distributive syntax.
-final class DistributiveOps[F[_]: Functor, A](val fa: F[A]) {
-  def distribute[G[_], B](f: A => G[B])(implicit G: Distributive[G]): G[F[B]] = G.distribute(fa)(f)
-  def cosequence[G[_], B](implicit G: Distributive[G], ev: A === G[B]): G[F[B]] = G.cosequence(ev.substitute(fa))
+final class DistributiveOps[F[_], A](val fa: F[A]) extends AnyVal {
+  def distribute[G[_], B](f: A => G[B])(implicit G: Distributive[G], F: Functor[F]): G[F[B]] = G.distribute(fa)(f)
+  def cosequence[G[_], B](implicit G: Distributive[G], F: Functor[F], ev: A === G[B]): G[F[B]] = G.cosequence(ev.substitute(fa))
 }
