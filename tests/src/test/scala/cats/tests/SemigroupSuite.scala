f126e298cc70b9e77605932f574c8524efc8ce2c
Eugene Platonov <jozic@live.com>
2017-12-12 23:42:19 -0500

    Add Semigroup.instance method

--- tests/src/test/scala/cats/tests/SemigroupSuite.scala
+++ tests/src/test/scala/cats/tests/SemigroupSuite.scala
@@ -2,10 +2,10 @@ package cats
 package tests
 
 import org.scalatest._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 
-
-class SemigroupSuite extends FunSuite {
+class SemigroupSuite extends FunSuite with Matchers with GeneratorDrivenPropertyChecks {
   {
     import cats.implicits._
     Invariant[Semigroup]
@@ -18,5 +18,15 @@ class SemigroupSuite extends FunSuite {
     Invariant[Semigroup]
     Semigroupal[Semigroup]
     InvariantMonoidal[Semigroup]
+  }
+
+  test("Semigroup.instance creates a Semigroup from the given function") {
+    val mult: (Int, Int) => Int = (a, b) => a * b
+    val add: (Int, Int) => Int = (a, b) => a + b
+
+    forAll { (a: Int, b: Int) =>
+      Semigroup.instance(mult).combine(a, b) should === (a * b)
+      Semigroup.instance(add).combine(a, b) should === (a + b)
+    }
   }
 }
