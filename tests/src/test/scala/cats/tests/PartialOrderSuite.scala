a2252233c195c331d57102362ac9e6170ff26388
Cody Allen <ceedubs@gmail.com>
2017-12-13 08:56:35 -0800

    Change PartialOrder syntax tests to handle NaN

--- tests/src/test/scala/cats/tests/PartialOrderSuite.scala
+++ tests/src/test/scala/cats/tests/PartialOrderSuite.scala
@@ -2,8 +2,18 @@ package cats
 package tests
 
 import Helpers.POrd
+import org.scalatest.Assertion
 
 class PartialOrderSuite extends CatsSuite {
+
+  /**
+   * Check that two partial compare results are "the same".
+   * This works around the fact that `NaN` is not equal to itself.
+   */
+  def checkPartialCompare(res1: Double, res2: Double): Assertion = {
+    (res1 == res2 || (res1.isNaN && res2.isNaN)) should ===(true)
+  }
+
   {
     Invariant[PartialOrder]
     Contravariant[PartialOrder]
@@ -11,7 +21,7 @@ class PartialOrderSuite extends CatsSuite {
 
   test("companion object syntax") {
     forAll { (i: Int, j: Int) =>
-      PartialOrder.partialCompare(i, j) should ===(catsKernelStdOrderForInt.partialCompare(i, j))
+      checkPartialCompare(PartialOrder.partialCompare(i, j), catsKernelStdOrderForInt.partialCompare(i, j))
       PartialOrder.tryCompare(i, j) should ===(catsKernelStdOrderForInt.tryCompare(i, j))
       PartialOrder.pmin(i, j) should ===(catsKernelStdOrderForInt.pmin(i, j))
       PartialOrder.pmax(i, j) should ===(catsKernelStdOrderForInt.pmax(i, j))
@@ -29,7 +39,7 @@ class PartialOrderSuite extends CatsSuite {
       (i < j) should ===(PartialOrder.lt(i, j))
       (i <= j) should ===(PartialOrder.lteqv(i, j))
 
-      (i partialCompare j) should ===(PartialOrder.partialCompare(i, j))
+      checkPartialCompare(i partialCompare j, PartialOrder.partialCompare(i, j))
       (i tryCompare j) should ===(PartialOrder.tryCompare(i, j))
       (i pmin j) should ===(PartialOrder.pmin(i, j))
       (i pmax j) should ===(PartialOrder.pmax(i, j))
