b7097730ebe422ab73b6884e49ced05b1186f61f
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2018-03-14 10:01:23 +0100

    Make newtype private

--- core/src/main/scala/cats/data/Newtype.scala
+++ core/src/main/scala/cats/data/Newtype.scala
@@ -5,7 +5,7 @@ package data
   * Helper trait for `newtype`s. These allow you to create a zero-allocation wrapper around a specific type.
   * Similar to `AnyVal` value classes, but never have any runtime overhead.
   */
-trait Newtype { self =>
+private[data] trait Newtype { self =>
   private[data] type Base
   private[data] trait Tag extends Any
   type Type[A] <: Base with Tag
