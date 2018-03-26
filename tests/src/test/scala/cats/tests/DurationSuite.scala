0e2489440fe5a06833cec93771157988c58447ec
Cody Allen <ceedubs@gmail.com>
2018-03-26 15:05:09 -0700

    Test Show instance for Duration (#2215)

---
+++ tests/src/test/scala/cats/tests/DurationSuite.scala
@@ -0,0 +1,21 @@
+package cats
+package tests
+
+import cats.instances.duration._
+import cats.laws.discipline.SerializableTests
+
+import scala.concurrent.duration.{Duration, DurationInt}
+
+class DurationSuite extends CatsSuite {
+  checkAll("Show[Duration]", SerializableTests.serializable(Show[Duration]))
+
+  test("show works for FiniteDuration"){
+    Show[Duration].show(23.minutes) should ===("23 minutes")
+  }
+
+  test("show works for non-finite durations"){
+    Show[Duration].show(Duration.Inf) should ===("Duration.Inf")
+    Show[Duration].show(Duration.MinusInf) should ===("Duration.MinusInf")
+    Show[Duration].show(Duration.Undefined) should ===("Duration.Undefined")
+  }
+}
