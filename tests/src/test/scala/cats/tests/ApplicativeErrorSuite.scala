d62b3b2a7dc36de0f018615b702c7774bf6c8602
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-05-04 21:31:47 +0100

    Added wrapper to test syntax orElse

--- tests/src/test/scala/cats/tests/ApplicativeErrorSuite.scala
+++ tests/src/test/scala/cats/tests/ApplicativeErrorSuite.scala
@@ -35,11 +35,31 @@ class ApplicativeErrorSuite extends CatsSuite {
     failed.recoverWith { case _ => Some(7) } should === (Some(7))
   }
 
-  test("orElse leaves a success unchanged") {
-    17.some orElse None should === (Some(17))
-  }
+  {
+    final case class OptionWrapper[A](option: Option[A])
 
-  test("orElse transforms an error to the alternative") {
-    failed orElse Some(17) should === (Some(17))
+    implicit def mayBeApplicativeError[E](implicit ev: ApplicativeError[Option, E]): ApplicativeError[OptionWrapper, E] =
+      new ApplicativeError[OptionWrapper, E] {
+
+        def raiseError[A](e: E): OptionWrapper[A] =
+          OptionWrapper(ev.raiseError(e))
+
+        def handleErrorWith[A](fa: OptionWrapper[A])(f: E => OptionWrapper[A]): OptionWrapper[A] =
+          OptionWrapper(ev.handleErrorWith(fa.option)(f(_).option))
+
+        def pure[A](x: A): OptionWrapper[A] =
+          OptionWrapper(x.some)
+
+        def ap[A, B](ff: OptionWrapper[A => B])(fa: OptionWrapper[A]): OptionWrapper[B] =
+          OptionWrapper(ev.ap(ff.option)(fa.option))
+      }
+
+    test("orElse leaves a success unchanged") {
+      (OptionWrapper(17.some) orElse OptionWrapper(None)).option should === (OptionWrapper(17.some).option)
+    }
+
+    test("orElse transforms an error to the alternative") {
+      (().raiseError[OptionWrapper, Int] orElse OptionWrapper(17.some)).option should === (OptionWrapper(17.some).option)
+    }
   }
 }
