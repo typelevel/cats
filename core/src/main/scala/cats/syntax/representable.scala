c31445e3cc83e58c25491aece2e2b8e3c62e7a6d
Eli Jordan <elias.k.jordan@gmail.com>
2018-07-08 12:58:51 +1000

    updates to RepresentableSyntax

--- core/src/main/scala/cats/syntax/representable.scala
+++ core/src/main/scala/cats/syntax/representable.scala
@@ -5,12 +5,12 @@ trait RepresentableSyntax {
   implicit final def catsSyntaxTabulate[A, R](f: R => A): TabulateOps[A, R] =
     new TabulateOps[A, R](f)
 
-  implicit final def catsSyntaxIndex[F[_], A, R](fa: F[A])(implicit R: Representable.Aux[F, R]): IndexOps[F, A, R] =
-    new IndexOps[F, A, R](fa)
+  implicit final def catsSyntaxIndex[F[_], A](fa: F[A]): IndexOps[F, A] =
+    new IndexOps[F, A](fa)
 }
 
-final class IndexOps[F[_], A, R](val fa: F[A]) extends AnyVal {
-  def index(implicit R: Representable.Aux[F, R]): R => A = R.index(fa)
+final class IndexOps[F[_], A](val fa: F[A]) extends AnyVal {
+  def index[R](implicit R: Representable.Aux[F, R]): R => A = R.index(fa)
 }
 
 final class TabulateOps[A, R](val f: R => A) extends AnyVal {
