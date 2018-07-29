e6ae9855a2d249bf8caf4dc938deb661188a4c04
Kailuo Wang <kailuo.wang@gmail.com>
2018-07-28 20:27:16 -0400

    fixing build

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -47,7 +47,7 @@ fi
 
 
 if [[ $TRAVIS_SCALA_VERSION == *"2.12"* ]]; then
-scalafix="sbt ';coreJVM/publishLocal;freeJVM/publishLocal' && cd scalafix && sbt tests/test && cd .. &&"
+scalafix="$sbt_cmd coreJVM/publishLocal freeJVM/publishLocal && cd scalafix && $sbt_cmd tests/test && cd .. &&"
 else
 scalafix=""
 fi
