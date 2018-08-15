9ffd73901281ca8db9a8be5911a131cb7fda5faa
Denis Rosca <denisrosca@users.noreply.github.com>
2018-08-15 11:35:29 +0300

    Serialization test for Compose instance for Map

--- tests/src/test/scala/cats/tests/MapSuite.scala
+++ tests/src/test/scala/cats/tests/MapSuite.scala
@@ -1,6 +1,7 @@
 package cats
 package tests
 
+import cats.arrow.Compose
 import cats.laws.discipline.{FlatMapTests, SemigroupalTests, SerializableTests, UnorderedTraverseTests, ComposeTests}
 
 class MapSuite extends CatsSuite {
@@ -15,7 +16,8 @@ class MapSuite extends CatsSuite {
   checkAll("Map[Int, Int] with Option", UnorderedTraverseTests[Map[Int, ?]].unorderedTraverse[Int, Int, Int, Option, Option])
   checkAll("UnorderedTraverse[Map[Int, ?]]", SerializableTests.serializable(UnorderedTraverse[Map[Int, ?]]))
 
-  checkAll("Compose[Map]", ComposeTests[Map].compose[Int, Long, String, Double])
+  checkAll("Map[Int, Long]", ComposeTests[Map].compose[Int, Long, String, Double])
+  checkAll("Compose[Map]", SerializableTests.serializable(Compose[Map]))
 
 
   test("show isn't empty and is formatted as expected") {
