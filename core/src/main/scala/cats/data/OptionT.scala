f04805918bdec3486fe021344bbe12ceba13a6fb
Kai(luo) Wang <kailuo.wang@gmail.com>
2018-09-05 10:33:07 -0400

    add back the implicit method as explicit to maintain BC

--- core/src/main/scala/cats/data/OptionT.scala
+++ core/src/main/scala/cats/data/OptionT.scala
@@ -217,6 +217,10 @@ object OptionT extends OptionTInstances {
 }
 
 private[data] sealed abstract class OptionTInstances extends OptionTInstances0 {
+  //to maintain binary compatibility
+  def catsDataMonadForOptionT[F[_]](implicit F0: Monad[F]): Monad[OptionT[F, ?]] =	
+    new OptionTMonad[F] { implicit val F = F0 }
+  
   implicit def catsDataTraverseForOptionT[F[_]](implicit F0: Traverse[F]): Traverse[OptionT[F, ?]] =
     new OptionTTraverse[F] with OptionTFunctor[F] { implicit val F = F0 }
 
