62cfdaa3254519270cdd6616c9190d4d92b7c3ce
Leandro Galvan <leusgalvan@gmail.com>
2018-09-09 14:40:54 -0300

    Add ScalaDoc for maxBy/minBy methods in Reducible, Foldable and UnorderedFoldable

--- core/src/main/scala/cats/syntax/reducible.scala
+++ core/src/main/scala/cats/syntax/reducible.scala
@@ -16,9 +16,41 @@ trait ReducibleSyntaxBinCompat0 {
 }
 
 final class ReducibleOpsBinCompat0[F[_], A](val fa: F[A]) extends AnyVal {
+  /**
+    * Find the maximum item in this structure according to the given function.
+    *
+    * For example:
+    * {{{
+    * scala> import cats.implicits._
+    *
+    * scala> val list1 = List[String]("Two", "Three", "Four")
+    * scala> list1.maxBy(_.length)
+    * res0: String = Three
+    *
+    * scala> val list2 = List[Int](41, 32, 23)
+    * scala> list2.maxBy(_ % 10)
+    * res1: Int = 23
+    * }}}
+    */
   def maxBy[B](f: A => B)(implicit F: Reducible[F], B: Order[B]): A =
     F.reduceLeft[A](fa)((a1, a2) => if (B.gteqv(f(a1), f(a2))) a1 else a2)
 
+  /**
+    * Find the minimum item in this structure according to the given function.
+    *
+    * For example:
+    * {{{
+    * scala> import cats.implicits._
+    *
+    * scala> val list1 = List[String]("Two", "Three", "Four")
+    * scala> list1.minBy(_.length)
+    * res0: String = Two
+    *
+    * scala> val list2 = List[Int](41, 32, 23)
+    * scala> list2.minBy(_ % 10)
+    * res1: Int = 41
+    * }}}
+    */
   def minBy[B](f: A => B)(implicit F: Reducible[F], B: Order[B]): A =
     F.reduceLeft[A](fa)((a1, a2) => if (B.lteqv(f(a1), f(a2))) a1 else a2)
 }
--- core/src/main/scala/cats/syntax/unorderedFoldable.scala
+++ core/src/main/scala/cats/syntax/unorderedFoldable.scala
@@ -29,9 +29,51 @@ final class UnorderedFoldableOps[F[_], A](val fa: F[A]) extends AnyVal {
       }
     }
 
+  /**
+    * Find the maximum item in this structure according to the given function.
+    *
+    * If there are no elements, the result is `None`.
+    *
+    * For example:
+    * {{{
+    * scala> import cats.implicits._
+    * scala> val set1 = Set[String]()
+    * scala> set1.maxByOption(_.length)
+    * res0: Option[String] = None
+    *
+    * scala> val set2 = Set("Two", "Three", "Four")
+    * scala> set2.maxByOption(_.length)
+    * res1: Option[String] = Some(Three)
+    *
+    * scala> val set3 = Set(41, 32, 23)
+    * scala> set3.maxByOption(_ % 10)
+    * res2: Option[Int] = Some(23)
+    * }}}
+    */
   def maxByOption[B](f: A => B)(implicit F: UnorderedFoldable[F], B: Order[B]): Option[A] =
     F.unorderedFoldMap(fa)(Option.apply)(orderMaxCommutativeMonoidBy(f))
 
+  /**
+    * Find the minimum item in this structure according to the given function.
+    *
+    * If there are no elements, the result is `None`.
+    *
+    * For example:
+    * {{{
+    * scala> import cats.implicits._
+    * scala> val set1 = Set[String]()
+    * scala> set1.minByOption(_.length)
+    * res0: Option[String] = None
+    *
+    * scala> val set2 = Set[String]("Two", "Three", "Four")
+    * scala> set2.minByOption(_.length)
+    * res1: Option[String] = Some(Two)
+    *
+    * scala> val set3 = Set[Int](41, 32, 23)
+    * scala> set3.minByOption(_ % 10)
+    * res2: Option[Int] = Some(41)
+    * }}}
+    */
   def minByOption[B](f: A => B)(implicit F: UnorderedFoldable[F], B: Order[B]): Option[A] =
     F.unorderedFoldMap(fa)(Option.apply)(orderMinCommutativeMonoidBy(f))
 }
