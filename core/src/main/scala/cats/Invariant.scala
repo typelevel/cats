c50f1ef7b79667c057e0ba76d9abf65a0a7e7024
Chris Davenport <chris@christopherdavenport.tech>
2018-09-13 10:52:52 -0400

    Invariant For Applicative Error Base

--- core/src/main/scala/cats/Invariant.scala
+++ core/src/main/scala/cats/Invariant.scala
@@ -121,4 +121,22 @@ object Invariant {
     }
 
   }
+
+  implicit def catsInvariantApplicativeError[F[_]]: Invariant[ApplicativeError[F, ?]] = new Invariant[ApplicativeError[F, ?]]{
+    def imap[A, B](fa: ApplicativeError[F, A])(f: A => B)(g: B => A): ApplicativeError[F, B] = new ApplicativeError[F, B] {
+      private val outerfa = fa
+      private val outerF = f
+      override def raiseError[C](e: B): F[C] = 
+        outerfa.raiseError(g(e))
+
+      def pure[A](x: A): F[A] = outerfa.pure(x)
+  
+      def handleErrorWith[C](fa: F[C])(f: B => F[C]): F[C] = 
+        outerfa.handleErrorWith(fa)(outerF andThen f)
+  
+
+      def ap[C, D](ff: F[C => D])(fa: F[C]): F[D] = outerfa.ap(ff)(fa)
+    }
+  }
+  
 }
