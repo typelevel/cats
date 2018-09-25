196ea1f7ec88b04be96dd2e79234b610808c0673
Daniel Slapman <danslapman@gmail.com>
2018-09-25 22:15:46 +0300

    Add tests for Eq.compose

--- tests/src/test/scala/cats/tests/EqSuite.scala
+++ tests/src/test/scala/cats/tests/EqSuite.scala
@@ -17,4 +17,27 @@ class EqSuite extends CatsSuite {
     checkAll("Eq[Int]", ContravariantMonoidalTests[Eq].contravariantMonoidal[Int, Int, Int])
     checkAll("ContravariantMonoidal[Eq]", SerializableTests.serializable(ContravariantMonoidal[Eq]))
 
+    case class Something(a: Int, b: Int, c: Int)
+
+    test("Eq returned by Eq.compose yields true on all underlying Eq's true") {
+        val somethingEq = Eq.compose[Something](
+            Eq.by(_.a), Eq.by(_.b), Eq.by(_.c)
+        )
+
+        val sut = Something(1, 2, 3)
+
+        somethingEq.eqv(sut, sut) shouldBe true
+    }
+
+    test("Eq returned by Eq.compose yields false if some of underlying Eq's false") {
+        val somethingEq = Eq.compose[Something](
+            Eq.by(_.a), Eq.by(_.b), Eq.by(_.c)
+        )
+
+        val sut = Something(1, 2, 3)
+
+        somethingEq.eqv(sut, sut.copy(a = 4)) shouldBe false
+        somethingEq.eqv(sut, sut.copy(b = 4)) shouldBe false
+        somethingEq.eqv(sut, sut.copy(c = 4)) shouldBe false
+    }
 }
