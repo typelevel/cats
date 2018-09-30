cc8a9e73461254f683c8c15f90be5fa3c647ecb2
Calvin Brown <calvin.brown@banno.com>
2018-09-30 09:29:15 -0500

    not duplicating, just re-using an instance

--- core/src/main/scala/cats/instances/finiteDuration.scala
+++ core/src/main/scala/cats/instances/finiteDuration.scala
@@ -1,35 +1,9 @@
-package cats.kernel
+package cats
 package instances
 
-import scala.concurrent.duration.{ Duration, FiniteDuration }
+import scala.concurrent.duration.FiniteDuration
 
-object finiteDuration extends FiniteDurationInstances
-
-trait FiniteDurationInstances {
-  implicit val catsKernelStdOrderForDuration: Order[FiniteDuration] with Hash[FiniteDuration] = new FiniteDurationOrder
-  implicit val catsKernelStdGroupForDuration: CommutativeGroup[FiniteDuration] = new FiniteDurationGroup
-}
-
-
-class FiniteDurationOrder extends Order[FiniteDuration] with Hash[FiniteDuration] {
-  def hash(x: FiniteDuration): Int = x.hashCode()
-
-  def compare(x: FiniteDuration, y: FiniteDuration): Int = x compare y
-
-  override def eqv(x: FiniteDuration, y: FiniteDuration): Boolean = x == y
-  override def neqv(x: FiniteDuration, y: FiniteDuration): Boolean = x != y
-  override def gt(x: FiniteDuration, y: FiniteDuration): Boolean = x > y
-  override def gteqv(x: FiniteDuration, y: FiniteDuration): Boolean = x >= y
-  override def lt(x: FiniteDuration, y: FiniteDuration): Boolean = x < y
-  override def lteqv(x: FiniteDuration, y: FiniteDuration): Boolean = x <= y
-
-  override def min(x: FiniteDuration, y: FiniteDuration): FiniteDuration = x min y
-  override def max(x: FiniteDuration, y: FiniteDuration): FiniteDuration = x max y
-}
-
-class FiniteDurationGroup extends CommutativeGroup[FiniteDuration] {
-  def empty: FiniteDuration = Duration.Zero
-  def inverse(x: FiniteDuration): FiniteDuration = -x
-  def combine(x: FiniteDuration, y: FiniteDuration): FiniteDuration = x + y
-  override def remove(x: FiniteDuration, y: FiniteDuration): FiniteDuration = x - y
+trait FiniteDurationInstances extends cats.kernel.instances.FiniteDurationInstances {
+  implicit val catsStdShowForFiniteDuration: Show[FiniteDuration] =
+    Show.fromToString[FiniteDuration]
 }
