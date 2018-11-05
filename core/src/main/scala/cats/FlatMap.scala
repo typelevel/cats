0a9f9d9917c548d5662ce2b4cc28cae64f6aa80c
Kailuo Wang <kailuo.wang@gmail.com>
2018-11-05 13:00:33 -0500

    fmt

--- core/src/main/scala/cats/FlatMap.scala
+++ core/src/main/scala/cats/FlatMap.scala
@@ -21,7 +21,6 @@ import simulacrum.noop
 @typeclass trait FlatMap[F[_]] extends Apply[F] {
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
-
   /**
    * "flatten" a nested `F` of `F` structure into a single-layer `F` structure.
    *
