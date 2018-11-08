7ff5d8fffbc2bf5a53a2f6076dd159e8794a15a4
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-11-08 23:10:28 +1100

    Add Function1Syntax to AllSyntaxBinCompat3

--- core/src/main/scala/cats/syntax/all.scala
+++ core/src/main/scala/cats/syntax/all.scala
@@ -31,7 +31,6 @@ trait AllSyntax
     with EqSyntax
     with FlatMapSyntax
     with FoldableSyntax
-    with Function1Syntax
     with FunctorSyntax
     with GroupSyntax
     with HashSyntax
@@ -77,4 +76,4 @@ trait AllSyntaxBinCompat2
     with ListSyntaxBinCompat0
     with ValidatedSyntaxBincompat0
 
-trait AllSyntaxBinCompat3 extends UnorderedFoldableSyntax
+trait AllSyntaxBinCompat3 extends UnorderedFoldableSyntax with Function1Syntax
