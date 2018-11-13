b52471e854cf1f24b4bc66c3cde9f9412391dc92
Diego E. Alonso-Blas <diego.e.a@47deg.com>
2018-11-13 17:38:19 +0000

    Add an If-Then-Else operation to the Apply.
    The `FlatMap` type-class includes a function `ifM`, which allows
    evaluating a Boolean computation and, depending on the result,
    choosing between one effectful computation or other.
    
    We add to the `Apply` type-class a very similar function.
    
    Although the function is similar to `ifM`, there is an important
    difference between them, as to how they handle the effect:
    - The `ifM` will run the effects of the condition `F[Boolean]`,
      followed by the effects of _only one_ of the branches.
    - The `ifA` will always run the effect of the condition, that of the first
      branch, and that of the second branch, in that order.
    
    Thus, the if-then-else part in `ifA` only applies to the inner values,
    not to the effects.

--- core/src/main/scala/cats/syntax/apply.scala
+++ core/src/main/scala/cats/syntax/apply.scala
@@ -14,6 +14,34 @@ trait ApplySyntax extends TupleSemigroupalSyntax {
     new ApplyOps(fa)
 }
 
+final class IfAOps[F[_]](val fa: F[Boolean]) extends AnyVal {
+
+  /**
+   * A conditional lifted into the `F` context.
+   *
+   * Example:
+   * {{{
+   * scala> import cats.implicits._
+   *
+   * scala> val b1: Option[Boolean] = Some(true)
+   * scala> val asInt1: Option[Int] = b1.ifA(Some(1), Some(0))
+   * scala> asInt1.get
+   * res0: Int = 1
+   *
+   * scala> val b2: Option[Boolean] = Some(false)
+   * scala> val asInt2: Option[Int] = b2.ifA(Some(1), Some(0))
+   * scala> asInt2.get
+   * res1: Int = 0
+   *
+   * scala> val b3: Option[Boolean] = Some(true)
+   * scala> val asInt3: Option[Int] = b3.ifA(Some(1), None)
+   * asInt2: Option[Int] = None
+   *
+   * }}}
+   */
+  def ifA[A](ifTrue: F[A], ifFalse: => F[A])(implicit F: Apply[F]): F[A] = F.ifA(fa)(ifTrue, ifFalse)
+}
+
 final class ApplyOps[F[_], A](val fa: F[A]) extends AnyVal {
 
   /** Alias for [[Apply.productR]]. */
