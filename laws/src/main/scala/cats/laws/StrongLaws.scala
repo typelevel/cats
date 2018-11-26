ee6f12878c1048222dbb5d65309508c5c2db3b91
Piotr Paradziński <piotr.paradzinski@gmail.com>
2018-11-26 09:30:08 +0100

    add link to Haskell impl, refactor common parts

--- laws/src/main/scala/cats/laws/StrongLaws.scala
+++ laws/src/main/scala/cats/laws/StrongLaws.scala
@@ -9,6 +9,7 @@ import cats.syntax.strong._
  * Laws that must be obeyed by any `cats.functor.Strong`.
  *
  * See: [[https://arxiv.org/abs/1406.4823 E. Rivas, M. Jaskelioff Notions of Computation as Monoids, Chapter 7]]
+ * See: [[http://hackage.haskell.org/package/profunctors/docs/Data-Profunctor-Strong.html Haskell Data.Profunctor.Strong]]
  */
 trait StrongLaws[F[_, _]] extends ProfunctorLaws[F] {
   implicit override def F: Strong[F]
@@ -31,19 +32,16 @@ trait StrongLaws[F[_, _]] extends ProfunctorLaws[F] {
   def lmapEqualsSecondAndThenRmap[A, B, C](fab: F[A, B]): IsEq[F[(C, A), B]] =
     fab.lmap[(C, A)]({ case (_, b) => b }) <-> fab.second[C].rmap[B](_._2)
 
+  private def mapFirst[X, Y, Z](f: X => Z)(cb: (X, Y)): (Z, Y) = (f(cb._1), cb._2)
+  private def mapSecond[X, Y, Z](f: Y => Z)(cb: (X, Y)): (X, Z) = (cb._1, f(cb._2))
+
   /** lmap (second f) . first == rmap (second f) . first */
-  def dinaturalityFirst[A, B, C, D](fab: F[A, B], f: C => D): IsEq[F[(A, C), (B, D)]] = {
-    val idbf: ((B, C)) => (B, D) = { case (b, c) => (b, f(c)) }
-    val idaf: ((A, C)) => (A, D) = { case (a, c) => (a, f(c)) }
-    fab.first[C].rmap(idbf) <-> fab.first[D].lmap(idaf)
-  }
+  def dinaturalityFirst[A, B, C, D](fab: F[A, B], f: C => D): IsEq[F[(A, C), (B, D)]] =
+    fab.first[C].rmap(mapSecond(f)) <-> fab.first[D].lmap(mapSecond(f))
 
   /** lmap (first f) . second == rmap (first f) . second */
-  def dinaturalitySecond[A, B, C, D](fab: F[A, B], f: C => D): IsEq[F[(C, A), (D, B)]] = {
-    val idbf: ((C, B)) => (D, B) = { case (c, b) => (f(c), b) }
-    val idaf: ((C, A)) => (D, A) = { case (c, a) => (f(c), a) }
-    fab.second[C].rmap(idbf) <-> fab.second[D].lmap(idaf)
-  }
+  def dinaturalitySecond[A, B, C, D](fab: F[A, B], f: C => D): IsEq[F[(C, A), (D, B)]] =
+    fab.second[C].rmap(mapFirst(f)) <-> fab.second[D].lmap(mapFirst(f))
 
   private def assoc[A, B, C]: (((A, B), C)) => (A, (B, C)) = { case ((a, c), d)   => (a, (c, d)) }
   private def unassoc[A, B, C]: ((A, (B, C))) => ((A, B), C) = { case (a, (c, d)) => ((a, c), d) }
