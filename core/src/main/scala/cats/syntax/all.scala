2063356a330caa647fb62663fb570665331aa289
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-18 16:39:13 +0100

    Add foldMapK, reduceMapK

--- core/src/main/scala/cats/syntax/all.scala
+++ core/src/main/scala/cats/syntax/all.scala
@@ -79,4 +79,8 @@ trait AllSyntaxBinCompat2
 
 trait AllSyntaxBinCompat3 extends UnorderedFoldableSyntax with Function1Syntax
 
-trait AllSyntaxBinCompat4 extends TraverseFilterSyntaxBinCompat0 with ParallelApplySyntax
+trait AllSyntaxBinCompat4
+    extends TraverseFilterSyntaxBinCompat0
+    with ParallelApplySyntax
+    with FoldableSyntaxBinCompat0
+    with ReducibleSyntaxBinCompat0
