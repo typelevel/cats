dd02f8ee29a89029efb07d419a6dd92fe2c3032a
tkroman <rmn.tk.ml@gmail.com>
2019-03-04 06:50:32 +0200

    syntax

--- tests/src/test/scala/cats/tests/AlternativeSuite.scala
+++ tests/src/test/scala/cats/tests/AlternativeSuite.scala
@@ -22,7 +22,7 @@ class AlternativeSuite extends CatsSuite {
 
   test("separateFoldable") {
     forAll { (list: List[Either[Int, String]]) =>
-      val ints = list.collect { case Left(i) => i }
+      val ints = list.collect { case Left(i)     => i }
       val strings = list.collect { case Right(s) => s }
       val expected = (ints, strings)
 
