cde902caf829d33e6c7382fed7aab7385f9ae841
ctongfei <tongfei@protonmail.com>
2019-03-25 23:16:25 -0400

    Category[Is] tests

--- tests/src/test/scala/cats/tests/IsSuite.scala
+++ tests/src/test/scala/cats/tests/IsSuite.scala
@@ -3,10 +3,21 @@ package tests
 
 import cats.arrow._
 import cats.kernel.laws.discipline.SerializableTests
+import cats.laws.discipline.CategoryTests
+import org.scalacheck.{Arbitrary, Gen}
 
 class IsSuite extends CatsSuite {
   import evidence._
 
+  implicit def arbIs[A, B](implicit ev: A Is B): Arbitrary[A Is B] = Arbitrary(Gen.const(ev))
+  implicit def eqIs[A, B]: Eq[A Is B] = Eq.fromUniversalEquals
+
+  trait Top {
+    def foo: String = this.getClass.getName
+  }
+  case class Bottom() extends Top
+
+  checkAll("Is[Bottom, Bottom]", CategoryTests[Is].category[Bottom, Bottom, Bottom, Bottom])
   checkAll("Category[Is]", SerializableTests.serializable(Category[Is]))
 
   test("syntax") {
