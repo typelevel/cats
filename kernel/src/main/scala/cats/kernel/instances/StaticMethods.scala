7942148c711413995b970a9c9427a5da961819a8
Travis Brown <travisrobertbrown@gmail.com>
2019-04-14 10:23:28 -0500

    Fix Hash instances to match changes in 2.13

--- kernel/src/main/scala/cats/kernel/instances/StaticMethods.scala
+++ kernel/src/main/scala/cats/kernel/instances/StaticMethods.scala
@@ -4,7 +4,7 @@ package instances
 
 import scala.collection.mutable
 
-object StaticMethods {
+object StaticMethods extends cats.kernel.compat.HashCompat {
 
   def wrapMutableMap[K, V](m: mutable.Map[K, V]): Map[K, V] =
     new WrappedMutableMap(m)
@@ -96,34 +96,4 @@ object StaticMethods {
     h = mix(h, _2Hash)
     finalizeHash(h, 2)
   }
-
-  // adapted from [[scala.util.hashing.MurmurHash3]],
-  // but modified standard `Any#hashCode` to `ev.hash`.
-  def listHash[A](x: List[A])(implicit A: Hash[A]): Int = {
-    import scala.util.hashing.MurmurHash3._
-    var n = 0
-    var h = seqSeed
-    var elems = x
-    while (!elems.isEmpty) {
-      val head = elems.head
-      val tail = elems.tail
-      h = mix(h, A.hash(head))
-      n += 1
-      elems = tail
-    }
-    finalizeHash(h, n)
-  }
-
-  // adapted from scala.util.hashing.MurmurHash3
-  def orderedHash[A](xs: TraversableOnce[A])(implicit A: Hash[A]): Int = {
-    import scala.util.hashing.MurmurHash3._
-    var n = 0
-    var h = seqSeed
-    xs.foreach { x =>
-      h = mix(h, A.hash(x))
-      n += 1
-    }
-    finalizeHash(h, n)
-  }
-
 }
