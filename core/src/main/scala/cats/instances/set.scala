fbc7cf83169102c4648feaec9ed7e0da8fb6901c
Kailuo Wang <kailuo.wang@gmail.com>
2019-05-04 21:16:43 -0400

    override default implementation in map and set

--- core/src/main/scala/cats/instances/set.scala
+++ core/src/main/scala/cats/instances/set.scala
@@ -32,7 +32,11 @@ trait SetInstances extends cats.kernel.instances.SetInstances {
       override def forall[A](fa: Set[A])(p: A => Boolean): Boolean =
         fa.forall(p)
 
+      override def exists[A](fa: Set[A])(p: A => Boolean): Boolean =
+        fa.exists(p)
+
       override def isEmpty[A](fa: Set[A]): Boolean = fa.isEmpty
+
     }
 
   implicit def catsStdShowForSet[A: Show]: Show[Set[A]] = new Show[Set[A]] {
