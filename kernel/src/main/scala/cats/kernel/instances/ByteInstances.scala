8069e099e1ca84c1b542f8f9d22a00c5093126c4
Kai(luo) Wang <kailuo.wang@gmail.com>
2019-05-23 18:37:34 -0400

    Enables Hydra on cats build  (#2848)
    * Compile Scala with Triplequote Hydra
    
    * Refactoring to workaround initialization issue of package object with inheritance
    
    * added hydra to .gitignore
    
    * Test added hydra to test impact on travis
    
    * fix
    
    * fix
    
    * format
    
    * update hydra
    
    * remove redudant plugin file
    
    * added metrics server config/reformat
    
    * fix metrics conf
    
    * added finite duration instance package / updated hydra
    
    * remove finiteDuration package

---
+++ kernel/src/main/scala/cats/kernel/instances/ByteInstances.scala
@@ -0,0 +1,34 @@
+package cats.kernel
+package instances
+
+trait ByteInstances {
+  implicit val catsKernelStdOrderForByte: Order[Byte] with Hash[Byte] = new ByteOrder
+  implicit val catsKernelStdGroupForByte: CommutativeGroup[Byte] = new ByteGroup
+}
+
+class ByteGroup extends CommutativeGroup[Byte] {
+  def combine(x: Byte, y: Byte): Byte = (x + y).toByte
+  def empty: Byte = 0
+  def inverse(x: Byte): Byte = (-x).toByte
+  override def remove(x: Byte, y: Byte): Byte = (x - y).toByte
+}
+
+class ByteOrder extends Order[Byte] with Hash[Byte] {
+
+  def hash(x: Byte): Int = x.hashCode()
+
+  def compare(x: Byte, y: Byte): Int =
+    if (x < y) -1 else if (x > y) 1 else 0
+
+  override def eqv(x: Byte, y: Byte): Boolean = x == y
+  override def neqv(x: Byte, y: Byte): Boolean = x != y
+  override def gt(x: Byte, y: Byte): Boolean = x > y
+  override def gteqv(x: Byte, y: Byte): Boolean = x >= y
+  override def lt(x: Byte, y: Byte): Boolean = x < y
+  override def lteqv(x: Byte, y: Byte): Boolean = x <= y
+
+  override def min(x: Byte, y: Byte): Byte =
+    java.lang.Math.min(x.toInt, y.toInt).toByte
+  override def max(x: Byte, y: Byte): Byte =
+    java.lang.Math.max(x.toInt, y.toInt).toByte
+}
--- kernel/src/main/scala/cats/kernel/instances/symbol/package.scala
+++ kernel/src/main/scala/cats/kernel/instances/symbol/package.scala
@@ -2,19 +2,3 @@ package cats.kernel
 package instances
 
 package object symbol extends SymbolInstances
-
-trait SymbolInstances {
-  implicit val catsKernelStdOrderForSymbol: Order[Symbol] with Hash[Symbol] = new SymbolOrder
-}
-
-class SymbolOrder extends Order[Symbol] with Hash[Symbol] {
-
-  def hash(x: Symbol): Int = x.hashCode()
-
-  override def eqv(x: Symbol, y: Symbol): Boolean =
-    // Symbols are interned
-    x eq y
-
-  def compare(x: Symbol, y: Symbol): Int =
-    if (x eq y) 0 else x.name.compareTo(y.name)
-}
