20369c3b5e9560ebea200b98d2bdf3caad5d98db
Morgen Peschke <morgen.peschke@gmail.com>
2019-06-14 01:07:34 -0700

    Add missing factories similar to existing factories
    Fixes #2887

--- kernel/src/main/scala/cats/kernel/CommutativeMonoid.scala
+++ kernel/src/main/scala/cats/kernel/CommutativeMonoid.scala
@@ -15,4 +15,13 @@ object CommutativeMonoid extends MonoidFunctions[CommutativeMonoid] {
    * Access an implicit `CommutativeMonoid[A]`.
    */
   @inline final def apply[A](implicit ev: CommutativeMonoid[A]): CommutativeMonoid[A] = ev
+
+  /**
+   * Create a `CommutativeMonoid` instance from the given function and empty value.
+   */
+  @inline def instance[A](emptyValue: A, cmb: (A, A) => A): CommutativeMonoid[A] = new CommutativeMonoid[A] {
+    override val empty: A = emptyValue
+
+    override def combine(x: A, y: A): A = cmb(x, y)
+  }
 }
