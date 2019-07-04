c177384a84f34c4b76cc6d246beec2c5c75a8a77
Kai(luo) Wang <kailuo.wang@gmail.com>
2019-07-04 16:23:33 -0400

    Update NonEmptyStreamSuite.scala

--- tests/src/test/scala-2.12-/cats/tests/NonEmptyStreamSuite.scala
+++ tests/src/test/scala-2.12-/cats/tests/NonEmptyStreamSuite.scala
@@ -16,7 +16,7 @@ class NonEmptyStreamSuite extends CatsSuite {
 
   checkAll("NonEmptyStream[Int] with Option",
            NonEmptyTraverseTests[NonEmptyStream].nonEmptyTraverse[Option, Int, Int, Int, Int, Option, Option])
-  checkAll("NonEmptyTraverse[NonEmptyStream[A]]", SerializableTests.serializable(NonEmptyTraverse[NonEmptyStream[?]]))
+  checkAll("NonEmptyTraverse[NonEmptyStream[A]]", SerializableTests.serializable(NonEmptyTraverse[NonEmptyStream[*]]))
 
   checkAll("NonEmptyStream[Int]", ReducibleTests[NonEmptyStream].reducible[Option, Int, Int])
   checkAll("Reducible[NonEmptyStream]", SerializableTests.serializable(Reducible[NonEmptyStream]))
