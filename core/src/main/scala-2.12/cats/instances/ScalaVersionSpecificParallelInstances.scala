7fb3f67fd13e352356eff262c450c700637c8ecf
Filippo Mariotti <filippo.mariotti@gmail.com>
2019-10-07 17:59:48 +0100

    Changed deprecation version

--- core/src/main/scala-2.12/cats/instances/ScalaVersionSpecificParallelInstances.scala
+++ core/src/main/scala-2.12/cats/instances/ScalaVersionSpecificParallelInstances.scala
@@ -7,7 +7,7 @@ import cats.{NonEmptyParallel, Parallel}
 
 trait ParallelInstances extends ParallelInstances1 {
 
-  @deprecated("Use cats.instances.either.catsParallelForEitherAndValidated", "2.0.0")
+  @deprecated("Use cats.instances.either.catsParallelForEitherAndValidated", "2.1.0")
   def catsParallelForEitherValidated[E: Semigroup]: Parallel.Aux[Either[E, *], Validated[E, *]] =
     cats.instances.either.catsParallelForEitherAndValidated[E]
 
@@ -16,15 +16,15 @@ trait ParallelInstances extends ParallelInstances1 {
     implicit P: Parallel[M]
   ): Parallel.Aux[OptionT[M, *], Nested[P.F, Option, *]] = OptionT.catsDataParallelForOptionT[M]
 
-  @deprecated("Use cats.instances.list.catsStdNonEmptyParallelForListZipList", "2.0.0")
+  @deprecated("Use cats.instances.list.catsStdNonEmptyParallelForListZipList", "2.1.0")
   def catsStdNonEmptyParallelForZipList: NonEmptyParallel.Aux[List, ZipList] =
     cats.instances.list.catsStdNonEmptyParallelForListZipList
 
-  @deprecated("Use cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector", "2.0.0")
+  @deprecated("Use cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector", "2.1.0")
   def catsStdNonEmptyParallelForZipVector: NonEmptyParallel.Aux[Vector, ZipVector] =
     cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector
 
-  @deprecated("Use cats.instances.stream.catsStdParallelForStreamZipStream", "2.0.0")
+  @deprecated("Use cats.instances.stream.catsStdParallelForStreamZipStream", "2.1.0")
   def catsStdParallelForZipStream: Parallel.Aux[Stream, ZipStream] =
     cats.instances.stream.catsStdParallelForStreamZipStream
 
--- core/src/main/scala-2.13+/cats/instances/ScalaVersionSpecificParallelInstances.scala
+++ core/src/main/scala-2.13+/cats/instances/ScalaVersionSpecificParallelInstances.scala
@@ -8,7 +8,7 @@ import cats.{NonEmptyParallel, Parallel}
 
 trait ParallelInstances extends ParallelInstances1 {
 
-  @deprecated("Use cats.instances.either.catsParallelForEitherAndValidated", "2.0.0")
+  @deprecated("Use cats.instances.either.catsParallelForEitherAndValidated", "2.1.0")
   def catsParallelForEitherValidated[E: Semigroup]: Parallel.Aux[Either[E, *], Validated[E, *]] =
     cats.instances.either.catsParallelForEitherAndValidated[E]
 
@@ -17,11 +17,11 @@ trait ParallelInstances extends ParallelInstances1 {
     implicit P: Parallel[M]
   ): Parallel.Aux[OptionT[M, *], Nested[P.F, Option, *]] = OptionT.catsDataParallelForOptionT[M]
 
-  @deprecated("Use cats.instances.list.catsStdNonEmptyParallelForListZipList", "2.0.0")
+  @deprecated("Use cats.instances.list.catsStdNonEmptyParallelForListZipList", "2.1.0")
   def catsStdNonEmptyParallelForZipList: NonEmptyParallel.Aux[List, ZipList] =
     cats.instances.list.catsStdNonEmptyParallelForListZipList
 
-  @deprecated("Use cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector", "2.0.0")
+  @deprecated("Use cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector", "2.1.0")
   def catsStdNonEmptyParallelForZipVector: NonEmptyParallel.Aux[Vector, ZipVector] =
     cats.instances.vector.catsStdNonEmptyParallelForVectorZipVector
 
@@ -29,7 +29,7 @@ trait ParallelInstances extends ParallelInstances1 {
   def catsStdParallelForZipStream: Parallel.Aux[Stream, ZipStream] =
     cats.instances.stream.catsStdParallelForStreamZipStream
 
-  @deprecated("Use cats.instances.lazyList.catsStdParallelForLazyListZipLazyList", "2.0.0")
+  @deprecated("Use cats.instances.lazyList.catsStdParallelForLazyListZipLazyList", "2.1.0")
   def catsStdParallelForZipLazyList[A]: Parallel.Aux[LazyList, ZipLazyList] =
     cats.instances.lazyList.catsStdParallelForLazyListZipLazyList[A]
 
