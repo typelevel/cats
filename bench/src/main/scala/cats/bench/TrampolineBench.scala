45c483741ac777fcec960b924b22e9c4c3848eb5
Travis Brown <travisrobertbrown@gmail.com>
2019-11-11 09:47:44 +0100

    Run Scalafmt on bench code

--- bench/src/main/scala/cats/bench/TrampolineBench.scala
+++ bench/src/main/scala/cats/bench/TrampolineBench.scala
@@ -16,27 +16,31 @@ class TrampolineBench {
   def eval(): Int = evalFib(N).value
 
   def evalFib(n: Int): Eval[Int] =
-    if (n < 2) Eval.now(n) else for {
-      x <- Eval.defer(evalFib(n - 1))
-      y <- Eval.defer(evalFib(n - 2))
-    } yield x + y
-
-
+    if (n < 2) Eval.now(n)
+    else
+      for {
+        x <- Eval.defer(evalFib(n - 1))
+        y <- Eval.defer(evalFib(n - 2))
+      } yield x + y
   @Benchmark
   def trampoline(): Int = trampolineFib(N).run
 
   def trampolineFib(n: Int): Trampoline[Int] =
-    if (n < 2) Trampoline.done(n) else for {
-      x <- Trampoline.defer(trampolineFib(n - 1))
-      y <- Trampoline.defer(trampolineFib(n - 2))
-    } yield x + y
+    if (n < 2) Trampoline.done(n)
+    else
+      for {
+        x <- Trampoline.defer(trampolineFib(n - 1))
+        y <- Trampoline.defer(trampolineFib(n - 2))
+      } yield x + y
 
-   @Benchmark
-   def stdlib(): Int = stdlibFib(N).result
+  @Benchmark
+  def stdlib(): Int = stdlibFib(N).result
 
-   def stdlibFib(n: Int): TailCalls.TailRec[Int] =
-     if (n < 2) TailCalls.done(n) else for {
-       x <- TailCalls.tailcall(stdlibFib(n - 1))
-       y <- TailCalls.tailcall(stdlibFib(n - 2))
-     } yield x + y
+  def stdlibFib(n: Int): TailCalls.TailRec[Int] =
+    if (n < 2) TailCalls.done(n)
+    else
+      for {
+        x <- TailCalls.tailcall(stdlibFib(n - 1))
+        y <- TailCalls.tailcall(stdlibFib(n - 2))
+      } yield x + y
 }
