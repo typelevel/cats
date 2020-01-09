4b7bb2e99c8e059bf63089aa8d9972c23f9515b7
Travis Brown <travisrobertbrown@gmail.com>
2020-01-09 12:39:50 -0600

    Add macrosJS to release script for 2.1.0 backport

--- scripts/scala.js-1.0-publish.sh
+++ scripts/scala.js-1.0-publish.sh
@@ -2,6 +2,7 @@
 
 # For temporary use while cross-publishing for Scala.js 0.6 and 1.0.
 
+SCALAJS_VERSION=1.0.0-RC2 sbt +macrosJS/publishSigned
 SCALAJS_VERSION=1.0.0-RC2 sbt +kernelJS/publishSigned
 SCALAJS_VERSION=1.0.0-RC2 sbt +kernelLawsJS/publishSigned
 SCALAJS_VERSION=1.0.0-RC2 sbt +coreJS/publishSigned
