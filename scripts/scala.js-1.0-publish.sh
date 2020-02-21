2cd642237857de33535196de776b33ed8187c235
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-02-21 16:13:03 -0300

    Make scala.js 1.0 the default
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- scripts/scala.js-1.0-publish.sh
+++ scripts/scala.js-1.0-publish.sh
@@ -2,11 +2,11 @@
 
 # For temporary use while cross-publishing for Scala.js 0.6 and 1.0.
 
-SCALAJS_VERSION=1.0.0-RC2 sbt +kernelJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +kernelLawsJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +coreJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +lawsJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +freeJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +testkitJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +alleycatsCoreJS/publishSigned
-SCALAJS_VERSION=1.0.0-RC2 sbt +alleycatsLawsJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +kernelJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +kernelLawsJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +coreJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +lawsJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +freeJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +testkitJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +alleycatsCoreJS/publishSigned
+SCALAJS_VERSION=0.6.32 sbt +alleycatsLawsJS/publishSigned
