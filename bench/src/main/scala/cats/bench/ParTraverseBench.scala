75ee5e87db21e4426adc29010bba419642e4f23d
Filippo Mariotti <filippo.mariotti@gmail.com>
2020-03-08 03:50:40 +0000

    Unused import in bench

--- bench/src/main/scala/cats/bench/ParTraverseBench.scala
+++ bench/src/main/scala/cats/bench/ParTraverseBench.scala
@@ -3,7 +3,6 @@ package cats.bench
 import cats.{Bitraverse, Parallel, Traverse}
 import cats.instances.int._
 import cats.instances.either._
-import cats.instances.tuple._
 import cats.instances.vector._
 import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
 
