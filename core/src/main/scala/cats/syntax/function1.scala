0e81c3feafce8c43aa9435eb65bf936ebc2c3d10
Valentin Willscher <valentin@willscher.de>
2020-04-17 01:25:13 +0900

    Add syntax for kleisli compatible function1

--- core/src/main/scala/cats/syntax/function1.scala
+++ core/src/main/scala/cats/syntax/function1.scala
@@ -1,10 +1,18 @@
 package cats
 package syntax
 
+import cats.arrow.Compose
+import cats.data.Kleisli
+
 trait Function1Syntax {
 
   implicit def catsSyntaxFunction1[F[_]: Functor, A, B](fab: F[Function1[A, B]]): Function1Ops[F, A, B] =
     new Function1Ops[F, A, B](fab)
+
+  implicit def catsSyntaxFunction1ComposeKleisli[F[_], A, B](
+    fab: Function1[A, F[B]]
+  )(implicit ev: Compose[Kleisli[F, *, *]]): Function1ComposeKleisliOps[F, A, B] =
+    new Function1ComposeKleisliOps[F, A, B](fab)
 
   final class Function1Ops[F[_]: Functor, A, B](fab: F[Function1[A, B]]) {
 
@@ -29,5 +37,18 @@ trait Function1Syntax {
      * }}}
      */
     def mapApply(a: A): F[B] = Functor[F].map(fab)(_(a))
+  }
+
+  final class Function1ComposeKleisliOps[F[_], A, B](f: A => F[B])(implicit CK: Compose[Kleisli[F, *, *]]) {
+
+    /**
+     * Alias for `(Kleisli(f) >>> Kleisli(g)).run` or `(Kleisli(f) andThen Kleisli(g)).run`
+     */
+    def >=>[C](g: B => F[C]): A => F[C] = CK.andThen(Kleisli(f), Kleisli(g)).run
+
+    /**
+     * Alias for `(Kleisli(f) <<< Kleisli(g)).run` or `(Kleisli(f) compose Kleisli(g)).run`
+     */
+    def <=<[C](g: C => F[A]): C => F[B] = CK.compose(Kleisli(f), Kleisli(g)).run
   }
 }
