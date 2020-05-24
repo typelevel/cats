c067caced9853bfd3ddc282c228dd31a8aa0036c
Gagandeep kalra <gagandeepkalra1994@gmail.com>
2020-05-24 17:42:45 +0800

    Add laws to check Short-Circuiting behaviour   (#3375)
    * added laws to check short-circuting-behavior for traverse and traverseFilter
    
    * added short-circuting tests for chain. lazyList, list, stream, vector, map, set, queue and sortedMap instances
    
    * added short-circuting tests for nonemptychain. nonemptylazyList, nonemptylist, nonemptystream and nonemptyvector instances
    
    * fixed traverse, traverseFilter and filterA short-circuiting behavior for lazList, queue, stream and vector instances
    
    * fixed traverseFilter and filterA short-circuiting behavior for stream scala_2.13 instance
    
    * fixed fmt errors

--- tests/src/test/scala/cats/tests/NonEmptyChainSuite.scala
+++ tests/src/test/scala/cats/tests/NonEmptyChainSuite.scala
@@ -4,7 +4,14 @@ import cats.{Align, Bimonad, SemigroupK, Show, Traverse}
 import cats.data.{Chain, NonEmptyChain, NonEmptyChainOps}
 import cats.kernel.{Eq, Order, PartialOrder, Semigroup}
 import cats.kernel.laws.discipline.{EqTests, OrderTests, PartialOrderTests, SemigroupTests}
-import cats.laws.discipline.{AlignTests, BimonadTests, NonEmptyTraverseTests, SemigroupKTests, SerializableTests}
+import cats.laws.discipline.{
+  AlignTests,
+  BimonadTests,
+  NonEmptyTraverseTests,
+  SemigroupKTests,
+  SerializableTests,
+  ShortCircuitingTests
+}
 import cats.laws.discipline.arbitrary._
 import cats.syntax.either._
 import cats.syntax.foldable._
@@ -32,6 +39,8 @@ class NonEmptyChainSuite extends NonEmptyCollectionSuite[Chain, NonEmptyChain, N
 
   checkAll("NonEmptyChain[Int]", AlignTests[NonEmptyChain].align[Int, Int, Int, Int])
   checkAll("Align[NonEmptyChain]", SerializableTests.serializable(Align[NonEmptyChain]))
+
+  checkAll("NonEmptyChain[Int]", ShortCircuitingTests[NonEmptyChain].traverse[Int])
 
   {
     implicit val partialOrder: PartialOrder[ListWrapper[Int]] = ListWrapper.partialOrder[Int]
