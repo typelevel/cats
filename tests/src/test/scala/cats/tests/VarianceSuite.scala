c370ccab4ba08617ba83c40836b7838d7a05dc1c
Kalra <gagandeepkalra1994@gmail.com>
2020-06-23 21:40:03 +0800

    added tests for autoWidenProfunctor

--- tests/src/test/scala/cats/tests/VarianceSuite.scala
+++ tests/src/test/scala/cats/tests/VarianceSuite.scala
@@ -1,12 +1,14 @@
 package cats.tests
 
-import cats.{Bifunctor, Contravariant, Functor}
-import cats.implicits._
+import cats.arrow.Profunctor
 import cats.conversions.all._
+import cats.implicits._
+import cats.{Bifunctor, Contravariant, Functor}
 
 class VarianceSuite extends CatsSuite {
 
-  sealed trait Foo
+  sealed trait FooBar
+  sealed trait Foo extends FooBar
   case class Bar(x: Int) extends Foo
   case object Baz extends Foo
 
@@ -41,6 +43,27 @@ class VarianceSuite extends CatsSuite {
       fi.bimap(i => if (true) Left(Bar(i)) else Right(Baz), identity)
 
     def inferred[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Int] = shouldInfer[F](fi)
+  }
+
+  test("Auto-variance should widen a profunctor automatically") {
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[FooBar, FooBar], Either[Bar, Baz.type]] =
+      fi.dimap((_: Either[FooBar, FooBar]) => 1)(i => if (true) Left(Bar(i)) else Right(Baz))
+
+    def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Either[Foo, Foo]] = shouldInfer[F](fi)
+  }
+
+  test("Auto-variance should left widen a profunctor automatically") {
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Int, Either[Bar, Baz.type]] =
+      fi.dimap(identity[Int])(i => if (true) Left(Bar(i)) else Right(Baz))
+
+    def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Int, Either[Foo, Foo]] = shouldInfer[F](fi)
+  }
+
+  test("Auto-variance should right widen a profunctor automatically") {
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[FooBar, FooBar], Int] =
+      fi.dimap((_: Either[FooBar, FooBar]) => 1)(identity[Int])
+
+    def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Int] = shouldInfer[F](fi)
   }
 
 }
