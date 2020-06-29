04ba075d22be25472671e56e4451a95ade006e4a
Kalra <gagandeepkalra1994@gmail.com>
2020-06-29 20:57:20 +0800

    addressed review feedback

--- tests/src/test/scala/cats/tests/VarianceSuite.scala
+++ tests/src/test/scala/cats/tests/VarianceSuite.scala
@@ -13,7 +13,7 @@ class VarianceSuite extends CatsSuite {
   case object Baz extends Foo
 
   test("Auto-variance should infer subtypes correctly") {
-    def shouldInfer[F[_]: Functor](fi: F[Int]): F[Either[Bar, Baz.type]] =
+    def shouldInfer[F[_]: Functor](fi: F[Int]) =
       fi.map(i => if (true) Left(Bar(i)) else Right(Baz))
 
     def inferred[F[_]: Functor](fi: F[Int]): F[Either[Foo, Foo]] = shouldInfer[F](fi)
@@ -25,42 +25,42 @@ class VarianceSuite extends CatsSuite {
   }
 
   test("Auto-variance should widen a bifunctor automatically") {
-    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Either[Bar, Baz.type], Either[Bar, Baz.type]] =
+    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]) =
       fi.bimap(i => if (true) Left(Bar(i)) else Right(Baz), i => if (true) Left(Bar(i)) else Right(Baz))
 
     def inferred[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Either[Foo, Foo]] = shouldInfer[F](fi)
   }
 
   test("Auto-variance should left widen a bifunctor automatically") {
-    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Int, Either[Bar, Baz.type]] =
+    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]) =
       fi.bimap(identity, i => if (true) Left(Bar(i)) else Right(Baz))
 
     def inferred[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Int, Either[Foo, Foo]] = shouldInfer[F](fi)
   }
 
   test("Auto-variance should right widen a bifunctor automatically") {
-    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Either[Bar, Baz.type], Int] =
+    def shouldInfer[F[_, _]: Bifunctor](fi: F[Int, Int]) =
       fi.bimap(i => if (true) Left(Bar(i)) else Right(Baz), identity)
 
     def inferred[F[_, _]: Bifunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Int] = shouldInfer[F](fi)
   }
 
-  test("Auto-variance should widen a profunctor automatically") {
-    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[FooBar, FooBar], Either[Bar, Baz.type]] =
+  test("Auto-variance auto adjust a profunctor's variance") {
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]) =
       fi.dimap((_: Either[FooBar, FooBar]) => 1)(i => if (true) Left(Bar(i)) else Right(Baz))
 
     def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Either[Foo, Foo]] = shouldInfer[F](fi)
   }
 
   test("Auto-variance should widen the second type parameter of a profunctor automatically") {
-    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Int, Either[Bar, Baz.type]] =
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]) =
       fi.dimap(identity[Int])(i => if (true) Left(Bar(i)) else Right(Baz))
 
     def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Int, Either[Foo, Foo]] = shouldInfer[F](fi)
   }
 
   test("Auto-variance should narrow the first type parameter of a profunctor automatically") {
-    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[FooBar, FooBar], Int] =
+    def shouldInfer[F[_, _]: Profunctor](fi: F[Int, Int]) =
       fi.dimap((_: Either[FooBar, FooBar]) => 1)(identity[Int])
 
     def inferred[F[_, _]: Profunctor](fi: F[Int, Int]): F[Either[Foo, Foo], Int] = shouldInfer[F](fi)
