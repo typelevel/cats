4af7db2230b1e2f0850a45e55ff8d68c1dd08372
Stephen Lazaro <stephen.lazaro@gmail.com>
2020-07-21 10:51:10 -0700

    Pretty weird stuff I guess

--- core/src/main/scala/cats/instances/order.scala
+++ core/src/main/scala/cats/instances/order.scala
@@ -7,33 +7,6 @@ import cats.syntax.apply._
 import cats.kernel.instances.unit._
 
 trait OrderInstances extends kernel.instances.OrderInstances {
-  implicit val catsContravariantMonoidalForOrder: ContravariantMonoidal[Order] =
-    new ContravariantMonoidal[Order] {
-
-      /**
-       * Provides trivial order
-       */
-      def unit: Order[Unit] = Order[Unit]
-
-      /**
-       * Derive an `Order` for `B` given an `Order[A]` and a function `B => A`.
-       *
-       * Note: resulting instances are law-abiding only when the functions used are injective (represent a one-to-one mapping)
-       */
-      def contramap[A, B](fa: Order[A])(f: B => A): Order[B] =
-        Order.by(f)(fa)
-
-      def product[A, B](fa: Order[A], fb: Order[B]): Order[(A, B)] =
-        new Order[(A, B)] {
-          def compare(x: (A, B), y: (A, B)): Int = {
-            val z = fa.compare(x._1, y._1)
-            if (z == 0) fb.compare(x._2, y._2) else z
-          }
-        }
-    }
-}
-
-trait OrderInstancesBinCompat0 {
   implicit val catsDecidableForOrder: Decidable[Order] =
     new Decidable[Order] {
 
@@ -71,5 +44,32 @@ trait OrderInstancesBinCompat0 {
         }
 
       override def zero[A]: Order[INothing] = Order.by(_ => ())
+    }
+}
+
+private [instances] trait OrderInstancesBinCompat0 {
+  implicit val catsContravariantMonoidalForOrder: ContravariantMonoidal[Order] =
+    new ContravariantMonoidal[Order] {
+
+      /**
+       * Provides trivial order
+       */
+      def unit: Order[Unit] = Order[Unit]
+
+      /**
+       * Derive an `Order` for `B` given an `Order[A]` and a function `B => A`.
+       *
+       * Note: resulting instances are law-abiding only when the functions used are injective (represent a one-to-one mapping)
+       */
+      def contramap[A, B](fa: Order[A])(f: B => A): Order[B] =
+        Order.by(f)(fa)
+
+      def product[A, B](fa: Order[A], fb: Order[B]): Order[(A, B)] =
+        new Order[(A, B)] {
+          def compare(x: (A, B), y: (A, B)): Int = {
+            val z = fa.compare(x._1, y._1)
+            if (z == 0) fb.compare(x._2, y._2) else z
+          }
+        }
     }
 }
