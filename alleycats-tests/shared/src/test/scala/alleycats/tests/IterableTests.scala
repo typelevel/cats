52fd5e56be7b9ba65c687ce575cba9e48e9766f9
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-07-29 00:15:46 -0400

    Scalafmt alleycats

--- alleycats-tests/shared/src/test/scala/alleycats/tests/IterableTests.scala
+++ alleycats-tests/shared/src/test/scala/alleycats/tests/IterableTests.scala
@@ -10,17 +10,21 @@ class IterableTests extends AlleycatsSuite {
   test("foldLeft sum == sum") {
     val it = Iterable(1, 2, 3)
     assertEquals(Foldable[Iterable].foldLeft(it, 0) {
-      case (b, a) => a + b
-    }, it.sum)
+                   case (b, a) => a + b
+                 },
+                 it.sum
+    )
   }
 
   test("foldRight early termination") {
     assertEquals(Foldable[Iterable]
-      .foldRight(Iterable(1, 2, 3), Eval.now("KO")) {
-        case (2, _) => Eval.now("OK")
-        case (a, b) => b
-      }
-      .value, Eval.now("OK").value)
+                   .foldRight(Iterable(1, 2, 3), Eval.now("KO")) {
+                     case (2, _) => Eval.now("OK")
+                     case (a, b) => b
+                   }
+                   .value,
+                 Eval.now("OK").value
+    )
   }
 
   checkAll("Foldable[Iterable]", FoldableTests[Iterable].foldable[Int, Int])
