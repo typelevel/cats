1719f64acf705086e4bd17cecbb2ad2ffc6493bd
Chris Birchall <chris.birchall@47deg.com>
2020-09-02 22:38:27 +0100

    Scalafix rule to remove instance imports when upgrading to 2.2.0 (#3566)
    * Bump scalafix
    
    * Move the v1.0.0 tests to a subdirectory
    
    * WIP scalafix rule to remove instance imports
    
    * Catch the low-hanging fruit
    
    * Support for removing "import cats.instances.all._"
    
    The import is only removed if there is no possibility it is being used
    to import Future instances.
    
    * Support for removing "import cats.implicits._"
    
    * Update the scalafix readme
    
    * Update test command in .travis.yml
    
    * Put sbt-scalafix back how it was
    
    Upgrading it broke one of the v1.0.0 rules and I can't be bothered
    investigating
    
    * No need to list all the positives, just check the negatives
    
    * Add -Ypartial-unification
    
    The v1.0.0 expected output files won't compile without this.
    
    * Remove special handling of Future instances
    
    * Rewrite "import cats.implicits._" to "import cats.syntax.all._"
    
    * Bump cats-core dependency in scalafix build.sbt
    
    This is so we get Future instances in implicit scope
    
    * Bump cats dependency to 2.2.0-RC4
    
    * Add a test for rewriting of global imports
    
    i.e. imports at the top of the file
    
    * Work around a known Scalafix issue
    
    * Improve the check for use of extension methods
    
    * Handle implicit conversions
    
    Do not remove an import of cats.implicits._ if it is being used to
    import an implicit conversion
    
    * Reorganise readme

---
+++ scalafix/v2_2_0/output/src/main/scala/fix/v2_2_0/RemoveInstanceImports.scala
@@ -0,0 +1,44 @@
+package fix
+package to2_2_0
+
+import cats.Semigroup
+import scala.concurrent.Future
+
+object RemoveInstanceImportsTests {
+  {
+    Semigroup[Option[Int]].combine(Some(1), Some(2))
+  }
+
+  {
+    Semigroup[Option[Int]].combine(Some(1), Some(2))
+  }
+
+  {
+    Semigroup[Option[Int]].combine(Some(1), Some(2))
+  }
+
+  {
+    import cats.syntax.semigroup._
+    Option(1) |+| Option(2)
+  }
+
+  {
+    import cats.syntax.all._
+    1.some |+| 2.some
+  }
+
+  {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Semigroup[Future[Int]]
+  }
+
+  {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Semigroup[Future[Int]]
+  }
+
+  {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Semigroup[Future[Int]]
+  }
+}
