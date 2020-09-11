3a6b1f232df658d628ecb23157a6c8658685ada2
Ang Hao Yang <yan9za1@gmail.com>
2020-09-12 05:23:51 +0800

    Switch to `Chain` as `Option#unzip` already defined in Scala 2.13

--- core/src/main/scala/cats/syntax/functor.scala
+++ core/src/main/scala/cats/syntax/functor.scala
@@ -12,11 +12,10 @@ final class FunctorTuple2Ops[F[_], A, B](private val fab: F[(A, B)]) extends Any
    * Lifts `Tuple2#_1` to Functor
    *
    * {{{
-   * scala> import cats.instances.option._
-   * scala> import cats.syntax.option._
+   * scala> import cats.data.Chain
    * scala> import cats.syntax.functor._
    *
-   * scala> (1, 2).some._1F == Some(1)
+   * scala> Chain((1, 2), (3, 4), (5, 6))._1F == Chain(1, 3, 5)
    * res0: Boolean = true
    * }}}
    */
@@ -26,11 +25,10 @@ final class FunctorTuple2Ops[F[_], A, B](private val fab: F[(A, B)]) extends Any
    * Lifts `Tuple2#_2` to Functor
    *
    * {{{
-   * scala> import cats.instances.option._
-   * scala> import cats.syntax.option._
+   * scala> import cats.data.Chain
    * scala> import cats.syntax.functor._
    *
-   * scala> (1, 2).some._2F == Some(2)
+   * scala> Chain((1, 2), (3, 4), (5, 6))._2F == Chain(2, 4, 6)
    * res0: Boolean = true
    * }}}
    */
@@ -40,11 +38,10 @@ final class FunctorTuple2Ops[F[_], A, B](private val fab: F[(A, B)]) extends Any
    * Lifts `Tuple2#swap` to Functor
    *
    * {{{
-   * scala> import cats.instances.option._
-   * scala> import cats.syntax.option._
+   * scala> import cats.data.Chain
    * scala> import cats.syntax.functor._
    *
-   * scala> (1, 2).some.swapF == Some((2, 1))
+   * scala> Chain((1, 2), (3, 4), (5, 6)).swapF == Chain((2, 1), (4, 3), (6, 5))
    * res0: Boolean = true
    * }}}
    */
@@ -56,11 +53,10 @@ final class FunctorTuple2Ops[F[_], A, B](private val fab: F[(A, B)]) extends Any
    * NOTE: Check for effect duplication, possibly memoize before
    *
    * {{{
-   * scala> import cats.instances.option._
-   * scala> import cats.syntax.option._
+   * scala> import cats.data.Chain
    * scala> import cats.syntax.functor._
    *
-   * scala> 5.some.map(i => (i, i)).unzip == ((Some(5), Some(5)))
+   * scala> Chain((1, 2), (3, 4), (5, 6)).unzip == ((Chain(1, 3, 5), Chain(2, 4, 6)))
    * res0: Boolean = true
    * }}}
    */
