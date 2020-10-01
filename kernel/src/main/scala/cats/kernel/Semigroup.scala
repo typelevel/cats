4dc75b6908197e63a62084c321542ed707bb65a7
Joseph Moniz <monizmjoseph@gmail.com>
2020-10-01 17:39:10 -0400

    scalafmt

--- kernel/src/main/scala/cats/kernel/Semigroup.scala
+++ kernel/src/main/scala/cats/kernel/Semigroup.scala
@@ -1,7 +1,7 @@
 package cats.kernel
 
 import scala.annotation.tailrec
-import scala.collection.immutable.{BitSet, Queue, SortedMap, SortedSet, Seq}
+import scala.collection.immutable.{BitSet, Queue, Seq, SortedMap, SortedSet}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.{specialized => sp}
