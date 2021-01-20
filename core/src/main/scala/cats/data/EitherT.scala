f6ef23269d10dc54b266022ba3ddf220ecf9637e
Michal Pawlik <admin@michalp.net>
2021-01-20 09:52:53 +0100

    add Try based example for EitherT.rethrowT

--- core/src/main/scala/cats/data/EitherT.scala
+++ core/src/main/scala/cats/data/EitherT.scala
@@ -194,6 +194,17 @@ final case class EitherT[F[_], A, B](value: F[Either[A, B]]) {
    * scala> val e2: EitherT[Option, Unit, Int] = EitherT[Option, Unit, Int](Some(Left(())))
    * scala> e2.rethrowT
    * res1: Option[Int] = None
+   *
+   * scala> import scala.util.Try
+   * scala> import java.lang.Exception
+   *
+   * scala> val e3: EitherT[Try, Throwable, String] = EitherT[Try, Throwable, String](Try(Right("happy cats")))
+   * scala> e3.rethrowT
+   * res2: util.Try[String] = Success(happy cats)
+   *
+   * scala> val e4: EitherT[Try, Throwable, String] = EitherT[Try, Throwable, String](Try(Left(new Exception("sad cats"))))
+   * scala> e4.rethrowT
+   * res3: util.Try[String] = Failure(java.lang.Exception: sad cats)
    * }}}
    */
   def rethrowT(implicit F: MonadError[F, _ >: A]): F[B] =
