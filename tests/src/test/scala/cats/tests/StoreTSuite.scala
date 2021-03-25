aef632df5a08f0a4d15c07ceb24f69c7e6e8a2e3
Tim Spence <timothywspence@gmail.com>
2021-03-25 16:46:14 +0000

    Tests consistent with RepresentableSuite

--- tests/src/test/scala/cats/tests/StoreTSuite.scala
+++ tests/src/test/scala/cats/tests/StoreTSuite.scala
@@ -5,6 +5,8 @@ import cats.data.{StoreT, Validated}
 import cats.laws.discipline._
 import cats.laws.discipline.arbitrary._
 import cats.laws.discipline.eq._
+import cats.syntax.eq._
+import org.scalacheck.Prop._
 
 class StoreTSuite extends CatsSuite {
 
@@ -21,5 +23,11 @@ class StoreTSuite extends CatsSuite {
   checkAll("Comonad[StoreT[Validated[String, *], MiniInt, *]]",
            SerializableTests.serializable(Applicative[StoreT[Validated[String, *], MiniInt, *]])
   )
+
+  test("extract and peek are consistent") {
+    forAll { (store: StoreT[Id, String, String]) =>
+      assert(store.extract === (store.peek(store.index)))
+    }
+  }
 
 }
