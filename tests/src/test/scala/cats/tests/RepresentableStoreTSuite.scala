e6fed6231e14d4c3abe1e9f3d8a3661b95812f49
Tim Spence <timothywspence@gmail.com>
2021-03-29 13:03:52 +0100

    Manual implicit resolution for the sake of scala 2.12

--- tests/src/test/scala/cats/tests/RepresentableStoreTSuite.scala
+++ tests/src/test/scala/cats/tests/RepresentableStoreTSuite.scala
@@ -7,12 +7,47 @@ import cats.laws.discipline.arbitrary._
 import cats.laws.discipline.eq._
 import cats.syntax.eq._
 import org.scalacheck.Prop._
+import cats.data.RepresentableStoreT
+import org.scalacheck.{Arbitrary, Cogen}
 
-class StoreTSuite extends CatsSuite {
+class RepresentableStoreTSuite extends CatsSuite {
 
   implicit val monoid: Monoid[MiniInt] = MiniInt.miniIntAddition
 
-  checkAll("StoreT[Id, MiniInt, *]", ComonadTests[StoreT[Id, MiniInt, *]].comonad[MiniInt, MiniInt, MiniInt])
+  implicit val scala2_12_makes_me_sad: Comonad[StoreT[Id, MiniInt, *]] =
+    RepresentableStoreT.comonadForStoreT[Id, Function1[MiniInt, *], MiniInt]
+  //Like, really, really, really sad
+  val a: Arbitrary[Int] = implicitly[Arbitrary[Int]]
+  val b: Eq[Int] = Eq[Int]
+  val c: Arbitrary[StoreT[Id, MiniInt, Int]] = implicitly[Arbitrary[StoreT[Id, MiniInt, Int]]]
+  val d: Cogen[Int] = implicitly[Cogen[Int]]
+  val e: Cogen[StoreT[Id, MiniInt, Int]] = implicitly[Cogen[StoreT[Id, MiniInt, Int]]]
+  val f: Eq[StoreT[Id, MiniInt, Int]] = Eq[StoreT[Id, MiniInt, Int]]
+  val g: Eq[StoreT[Id, MiniInt, StoreT[Id, MiniInt, Int]]] = Eq[StoreT[Id, MiniInt, StoreT[Id, MiniInt, Int]]]
+  val h: Eq[StoreT[Id, MiniInt, StoreT[Id, MiniInt, StoreT[Id, MiniInt, Int]]]] =
+    Eq[StoreT[Id, MiniInt, StoreT[Id, MiniInt, StoreT[Id, MiniInt, Int]]]]
+
+  checkAll("StoreT[Id, MiniInt, *]",
+           ComonadTests[StoreT[Id, MiniInt, *]].comonad[Int, Int, Int](
+             a,
+             b,
+             a,
+             b,
+             a,
+             b,
+             c,
+             d,
+             d,
+             d,
+             e,
+             e,
+             f,
+             g,
+             h,
+             f,
+             f
+           )
+  )
 
   checkAll("Comonad[StoreT[Id, MiniInt, *]]", SerializableTests.serializable(Comonad[StoreT[Id, MiniInt, *]]))
 
