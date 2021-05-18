e6493b0cd6f8f4f2ec1b018d4748e2d9e0c88000
Ben Plommer <ben.plommer@itv.com>
2021-05-18 11:48:16 +0100

    remove unused aliases + import

--- tests/src/test/scala-3.x/cats/tests/FunctionKLiftSuite.scala
+++ tests/src/test/scala-3.x/cats/tests/FunctionKLiftSuite.scala
@@ -1,14 +1,11 @@
 package cats.tests
 
-import cats.Applicative
-import cats.data.NonEmptyList
 import cats.arrow.FunctionK
 import cats.implicits._
 import org.scalacheck.Prop._
 import cats.laws.discipline.arbitrary._
 
 class FunctionKLiftSuite extends CatsSuite {
-  type OptionOfNel[+A] = Option[NonEmptyList[A]]
 
   test("lift a polymorphic function directly") {
     val fHeadOption = FunctionK.lift[List, Option]([X] => (_: List[X]).headOption)
