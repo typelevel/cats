ef76701bf3d3dab5bb01df4c87615838cc935340
satorg <satorg@gmail.com>
2021-06-24 21:54:12 -0700

    add missing docs for `toNev`

--- core/src/main/scala/cats/syntax/vector.scala
+++ core/src/main/scala/cats/syntax/vector.scala
@@ -7,5 +7,23 @@ trait VectorSyntax {
 }
 
 final class VectorOps[A](private val va: Vector[A]) extends AnyVal {
+
+  /**
+   * Returns an Option of NonEmptyVector from a Vector
+   *
+   * Example:
+   * {{{
+   * scala> import cats.data.NonEmptyVector
+   * scala> import cats.implicits._
+   *
+   * scala> val result1: Vector[Int] = Vector(1, 2)
+   * scala> result1.toNev
+   * res0: Option[NonEmptyVector[Int]] = Some(NonEmptyVector(1, 2))
+   *
+   * scala> val result2: Vector[Int] = Vector.empty[Int]
+   * scala> result2.toNev
+   * res1: Option[NonEmptyVector[Int]] = None
+   * }}}
+   */
   def toNev: Option[NonEmptyVector[A]] = NonEmptyVector.fromVector(va)
 }
