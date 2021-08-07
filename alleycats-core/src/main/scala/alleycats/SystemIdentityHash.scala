f54101c7741f804e58491d156eeb6e403811090a
SimY4 <alex.simkin@outlook.com>
2021-08-07 15:24:40 +1000

    Scaladoc

--- alleycats-core/src/main/scala/alleycats/SystemIdentityHash.scala
+++ alleycats-core/src/main/scala/alleycats/SystemIdentityHash.scala
@@ -2,6 +2,10 @@ package alleycats
 
 import cats.Hash
 
+/**
+ * A `Hash[A]` that delegates to identity hashcode (`System.identityHashCode`).
+ * It implements `Eq[A]` via referential equality (`eq`) that it is not referentially transparent!
+ */
 trait SystemIdentityHash[A <: AnyRef] extends ReferentialEq[A] with Hash[A] {
   override def hash(a: A): Int = java.lang.System.identityHashCode(a)
 }
