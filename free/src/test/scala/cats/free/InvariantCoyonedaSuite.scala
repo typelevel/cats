ecc71a8e78477231f5b830b2c9db3059c636ae24
Arman Bilge <armanbilge@gmail.com>
2021-09-01 20:48:30 +0000

    Make tests more rigorous

--- free/src/test/scala/cats/free/InvariantCoyonedaSuite.scala
+++ free/src/test/scala/cats/free/InvariantCoyonedaSuite.scala
@@ -3,67 +3,79 @@ package cats.free
 import cats.~>
 import cats.arrow.FunctionK
 import cats.Invariant
-import cats.Semigroup
 import cats.instances.all._
 import cats.kernel.Eq
 import cats.laws.discipline.{InvariantTests, SerializableTests}
 import cats.tests.CatsSuite
 import org.scalacheck.{Arbitrary}
 import cats.syntax.eq._
-import cats.syntax.semigroup._
 import org.scalacheck.Prop._
 
 class InvariantCoyonedaSuite extends CatsSuite {
 
-  // If we can generate functions we can generate an interesting InvariantCoyoneda.
-  implicit def invariantCoyonedaArbitrary[F[_], A: Semigroup, T](implicit
-    F: Arbitrary[(A, A) => A]
-  ): Arbitrary[InvariantCoyoneda[Semigroup, A]] =
-    Arbitrary(
-      F.arbitrary.map(f =>
-        InvariantCoyoneda.lift[Semigroup, A](Semigroup.instance { (x, y) =>
-          f(x, y) |+| f(y, x)
-        })
-      )
-    )
+  type Magma[A] = (A, A) => A
+  implicit def stringMagma[A]: Magma[String] = _ + _
+  implicit def listMagma[A]: Magma[List[A]] = _ ++ _
+  implicit object invariantForMagma extends Invariant[Magma] {
+    override def imap[A, B](fa: Magma[A])(f: A => B)(g: B => A): Magma[B] =
+      (x, y) => f(fa(g(x), g(y)))
+  }
 
-  // We can't really test that semigroups are equal but we can try it with a bunch of test data.
-  implicit def invariantCoyonedaEq[A: Arbitrary: Eq]: Eq[InvariantCoyoneda[Semigroup, A]] =
-    new Eq[InvariantCoyoneda[Semigroup, A]] {
-      def eqv(cca: InvariantCoyoneda[Semigroup, A], ccb: InvariantCoyoneda[Semigroup, A]): Boolean =
+  // If we can generate functions we can generate an interesting InvariantCoyoneda.
+  implicit def invariantCoyonedaArbitrary[F[_], A: Magma, T](implicit
+    F: Arbitrary[(A, A) => A]
+  ): Arbitrary[InvariantCoyoneda[Magma, A]] =
+    Arbitrary(F.arbitrary.map(InvariantCoyoneda.lift[Magma, A]))
+
+  // We can't really test that magmas are equal but we can try it with a bunch of test data.
+  implicit def invariantCoyonedaEq[A: Arbitrary: Eq]: Eq[InvariantCoyoneda[Magma, A]] =
+    new Eq[InvariantCoyoneda[Magma, A]] {
+      def eqv(cca: InvariantCoyoneda[Magma, A], ccb: InvariantCoyoneda[Magma, A]): Boolean =
         Arbitrary.arbitrary[List[(A, A)]].sample.get.forall { case (x, y) =>
-          cca.run.combine(x, y) == ccb.run.combine(x, y)
+          cca.run.apply(x, y) == ccb.run.apply(x, y)
         }
     }
 
   checkAll("InvariantCoyoneda[Semigroup, Int]",
-           InvariantTests[InvariantCoyoneda[Semigroup, *]].invariant[Int, Int, Int]
+           InvariantTests[InvariantCoyoneda[Magma, *]].invariant[List[Int], List[Int], List[Int]]
   )
   checkAll("Invariant[InvariantCoyoneda[Option, *]]",
-           SerializableTests.serializable(Invariant[InvariantCoyoneda[Option, *]])
+           SerializableTests.serializable(Invariant[InvariantCoyoneda[Magma, *]])
   )
 
   test("mapK and run is same as applying natural trans") {
     forAll { (x: Option[Int], y: Option[Int]) =>
-      val nt = new (Semigroup ~> Semigroup) {
-        def apply[A](sg: Semigroup[A]): Semigroup[A] = sg.reverse
+      val nt = new (Magma ~> Magma) {
+        def apply[A](m: Magma[A]): Magma[A] = (x, y) => m(y, x)
       }
-      val c = InvariantCoyoneda.lift[Semigroup, Option[Int]](Semigroup[Option[Int]])
-      c.mapK[Semigroup](nt).run.combine(x, y) === nt(Semigroup[Option[Int]]).combine(x, y)
+      val m: Magma[Option[Int]] = _.orElse(_)
+      val c = InvariantCoyoneda.lift[Magma, Option[Int]](m)
+      c.mapK[Magma](nt).run.apply(x, y) === nt(m).apply(x, y)
+    }
+  }
+
+  test("contramap order") {
+    forAll { (x: String, y: String) =>
+      InvariantCoyoneda
+        .lift[Magma, String](_ + _)
+        .imap(Some(_))(_.value)
+        .imap(Right(_))(_.value)
+        .run
+        .apply(Right(Some(x)), Right(Some(y))) == Right(Some(x + y))
     }
   }
 
   test("stack-safe imapmap") {
-    def loop(n: Int, acc: InvariantCoyoneda[Semigroup, Int]): InvariantCoyoneda[Semigroup, Int] =
+    def loop(n: Int, acc: InvariantCoyoneda[Magma, Int]): InvariantCoyoneda[Magma, Int] =
       if (n <= 0) acc
       else loop(n - 1, acc.imap((_: Int) + 1)((_: Int) - 1))
-    loop(20000, InvariantCoyoneda.lift[Semigroup, Int](Semigroup[Int])).run.combine(10, 11)
+    loop(20000, InvariantCoyoneda.lift[Magma, Int](_ + _)).run.apply(10, 11)
   }
 
   test("run, foldMap consistent") {
     forAll {
       (
-        c: InvariantCoyoneda[Semigroup, String],
+        c: InvariantCoyoneda[Magma, String],
         f1: String => Byte,
         g1: Byte => String,
         f2: Byte => Float,
@@ -72,8 +84,8 @@ class InvariantCoyonedaSuite extends CatsSuite {
         s2: Float
       ) =>
         val cʹ = c.imap(f1)(g1).imap(f2)(g2) // just to ensure there's some structure
-        val h = cʹ.foldMap[Semigroup](FunctionK.id[Semigroup])
-        cʹ.run.combine(s1, s2) === h.combine(s1, s2)
+        val h = cʹ.foldMap[Magma](FunctionK.id[Magma])
+        cʹ.run.apply(s1, s2) === h.apply(s1, s2)
     }
   }
 
