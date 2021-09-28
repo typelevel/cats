1b565bb774ce895665e0747418a9ac10f7a4564e
Tim Spence <timothywspence@gmail.com>
2021-09-28 12:47:42 +0100

    Initial attempt at Conversions from FunctionK to polymorphic functions

--- core/src/main/scala-3.x/src/main/scala/cats/arrow/FunctionKMacros.scala
+++ core/src/main/scala-3.x/src/main/scala/cats/arrow/FunctionKMacros.scala
@@ -1,4 +1,16 @@
 package cats
 package arrow
 
-private[arrow] class FunctionKMacroMethods
+private[arrow] class FunctionKMacroMethods {
+
+  given [F[_], G[_]]: Conversion[F ~> G, [A] => F[A] => G[A]] =
+    (f: F ~> G) =>
+      [A] => (fa: F[A]) => f(fa)
+
+  given [F[_], G[_]]: Conversion[[A] => F[A] => G[A], F ~> G] =
+    (f: [A] => F[A] => G[A]) =>
+      new (F ~> G) {
+        def apply[A](fa: F[A]): G[A] = f(fa)
+      }
+
+}
