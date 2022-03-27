314a72194aeb6c5e9fdf2ecb1450a4039e4882a3
Ben Plommer <ben.plommer@gmail.com>
2022-03-27 15:21:41 +0100

    Clean up iterable instances

--- alleycats-core/src/main/scala/alleycats/std/iterable.scala
+++ alleycats-core/src/main/scala/alleycats/std/iterable.scala
@@ -9,56 +9,51 @@ import scala.collection.immutable.{IndexedSeq => ImIndexedSeq}
 object iterable extends IterableInstances
 
 trait IterableInstances {
-  implicit def alleycatsStdIterableTraverse: Traverse[Iterable] = IterableInstances.alleycatsStdIterableTraverse
+  implicit def alleycatsStdIterableTraverse: Traverse[Iterable] = new Traverse[Iterable] {
+    override def foldLeft[A, B](fa: Iterable[A], b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
+
+    override def foldRight[A, B](fa: Iterable[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+      Foldable.iterateRight(fa, lb)(f)
+
+    override def foldMap[A, B](fa: Iterable[A])(f: A => B)(implicit B: Monoid[B]): B =
+      B.combineAll(fa.iterator.map(f))
+
+    override def reduceLeftOption[A](fa: Iterable[A])(f: (A, A) => A): Option[A] = fa.reduceLeftOption(f)
+
+    override def collectFirst[A, B](fa: Iterable[A])(pf: PartialFunction[A, B]): Option[B] = fa.collectFirst(pf)
+
+    override def fold[A](fa: Iterable[A])(implicit A: Monoid[A]): A = fa.fold(A.empty)(A.combine)
+
+    override def find[A](fa: Iterable[A])(f: A => Boolean): Option[A] = fa.find(f)
+
+    override def toIterable[A](fa: Iterable[A]): Iterable[A] = fa
+
+    override def exists[A](fa: Iterable[A])(p: A => Boolean): Boolean = fa.exists(p)
+
+    override def forall[A](fa: Iterable[A])(p: A => Boolean): Boolean = fa.forall(p)
+
+    override def toList[A](fa: Iterable[A]): List[A] = fa.toList
+
+    override def isEmpty[A](fa: Iterable[A]): Boolean = fa.isEmpty
+
+    override def nonEmpty[A](fa: Iterable[A]): Boolean = fa.nonEmpty
+
+    // Adapted from List and Vector instances.
+    override def traverse[G[_], A, B](fa: Iterable[A])(f: A => G[B])(implicit G: Applicative[G]): G[Iterable[B]] =
+      if (fa.isEmpty) G.pure(Iterable.empty)
+      else {
+        val imIndexedSeq = fa match {
+          case iseq: ImIndexedSeq[A] => iseq
+          case _ =>
+            val as = collection.mutable.ArrayBuffer[A]()
+            as ++= fa
+            wrapMutableIndexedSeq(as)
+        }
+
+        G.map(Chain.traverseViaChain(imIndexedSeq)(f))(_.toVector)
+      }
+  }
 
   @deprecated("use alleycatsStdIterableTraverse", "2.7.1")
   val alleycatsStdIterableFoldable: Foldable[Iterable] = alleycatsStdIterableTraverse
-}
-
-private[std] object IterableInstances {
-  val alleycatsStdIterableTraverse: Traverse[Iterable] =
-    new Traverse[Iterable] {
-      override def foldLeft[A, B](fa: Iterable[A], b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
-
-      override def foldRight[A, B](fa: Iterable[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
-        Foldable.iterateRight(fa, lb)(f)
-
-      override def foldMap[A, B](fa: Iterable[A])(f: A => B)(implicit B: Monoid[B]): B =
-        B.combineAll(fa.iterator.map(f))
-
-      override def reduceLeftOption[A](fa: Iterable[A])(f: (A, A) => A): Option[A] = fa.reduceLeftOption(f)
-
-      override def collectFirst[A, B](fa: Iterable[A])(pf: PartialFunction[A, B]): Option[B] = fa.collectFirst(pf)
-
-      override def fold[A](fa: Iterable[A])(implicit A: Monoid[A]): A = fa.fold(A.empty)(A.combine)
-
-      override def find[A](fa: Iterable[A])(f: A => Boolean): Option[A] = fa.find(f)
-
-      override def toIterable[A](fa: Iterable[A]): Iterable[A] = fa
-
-      override def exists[A](fa: Iterable[A])(p: A => Boolean): Boolean = fa.exists(p)
-
-      override def forall[A](fa: Iterable[A])(p: A => Boolean): Boolean = fa.forall(p)
-
-      override def toList[A](fa: Iterable[A]): List[A] = fa.toList
-
-      override def isEmpty[A](fa: Iterable[A]): Boolean = fa.isEmpty
-
-      override def nonEmpty[A](fa: Iterable[A]): Boolean = fa.nonEmpty
-
-      // Adapted from List and Vector instances.
-      override def traverse[G[_], A, B](fa: Iterable[A])(f: A => G[B])(implicit G: Applicative[G]): G[Iterable[B]] =
-        if (fa.isEmpty) G.pure(Iterable.empty)
-        else {
-          val imIndexedSeq = fa match {
-            case iseq: ImIndexedSeq[A] => iseq
-            case _ =>
-              val as = collection.mutable.ArrayBuffer[A]()
-              as ++= fa
-              wrapMutableIndexedSeq(as)
-          }
-
-          G.map(Chain.traverseViaChain(imIndexedSeq)(f))(_.toVector)
-        }
-    }
 }
