8bc0b8ce327328078c266f41cc0ff85d8b13b88c
Tim Spence <timothywspence@gmail.com>
2022-06-06 11:21:34 +0100

    Scalafmt

--- core/src/main/scala-3/src/main/scala/cats/arrow/FunctionKMacros.scala
+++ core/src/main/scala-3/src/main/scala/cats/arrow/FunctionKMacros.scala
@@ -37,8 +37,7 @@ private[arrow] class FunctionKMacroMethods {
     }
 
   given [F[_], G[_]]: Conversion[F ~> G, [A] => F[A] => G[A]] =
-    (f: F ~> G) =>
-      [A] => (fa: F[A]) => f(fa)
+    (f: F ~> G) => [A] => (fa: F[A]) => f(fa)
 
   given [F[_], G[_]]: Conversion[[A] => F[A] => G[A], F ~> G] =
     (f: [A] => F[A] => G[A]) =>
