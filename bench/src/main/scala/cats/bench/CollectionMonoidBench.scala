76b382e1e367da0513c2d68c435d5eb7718d0c41
Antonio Gelameris <toniogela89@gmail.com>
2022-06-26 22:25:58 +0200

    Refactored Chain and CollectiveMonoid benchmarks

--- bench/src/main/scala/cats/bench/CollectionMonoidBench.scala
+++ bench/src/main/scala/cats/bench/CollectionMonoidBench.scala
@@ -21,10 +21,8 @@
 
 package cats.bench
 
-import cats.Monoid
 import cats.data.Chain
 import cats.implicits._
-import chain.{Chain => OldChain}
 import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
 
 @State(Scope.Thread)
@@ -32,14 +30,7 @@ class CollectionMonoidBench {
 
   private val largeList = (0 to 1000000).toList
 
-  implicit def monoidOldChain[A]: Monoid[OldChain[A]] = new Monoid[OldChain[A]] {
-    def empty: OldChain[A] = OldChain.empty[A]
-
-    def combine(x: OldChain[A], y: OldChain[A]): OldChain[A] = x ++ y
-  }
-
   @Benchmark def accumulateChain: Chain[Int] = largeList.foldMap(Chain.one)
   @Benchmark def accumulateVector: Vector[Int] = largeList.foldMap(Vector(_))
   @Benchmark def accumulateList: List[Int] = largeList.foldMap(List(_))
-  @Benchmark def accumulateOldChain: OldChain[Int] = largeList.foldMap(OldChain.single)
 }
