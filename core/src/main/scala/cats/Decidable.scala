c787ae8a088ce6bde6f970d9656464ac299a4320
Stephen Lazaro <stephen.lazaro@gmail.com>
2022-06-29 09:58:39 -1000

    Remove Haskell style name aliases

--- core/src/main/scala/cats/Decidable.scala
+++ core/src/main/scala/cats/Decidable.scala
@@ -40,7 +40,6 @@ trait Decidable[F[_]] extends ContravariantMonoidal[F] {
   def decide[A, B, C](fa: F[A], fb: F[B])(f: C => Either[A, B]): F[C] =
     contramap(sum(fa, fb))(f)
 
-  final def chosen[B, C](fb: F[B], fc: F[C]): F[Either[B, C]] = sum(fb, fc)
   def lose[A](f: A => Nothing): F[A] = contramap[Nothing, A](zero)(f)
 }
 
