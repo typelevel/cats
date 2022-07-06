4814ed521091b5989c3b71762231f2335a43f3b6
Stephen Lazaro <stephen.lazaro@gmail.com>
2022-07-05 18:13:12 -0700

    Commit link to original source change
    Co-authored-by: Arman Bilge <armanbilge@gmail.com>

--- core/src/main/scala/cats/Decidable.scala
+++ core/src/main/scala/cats/Decidable.scala
@@ -31,7 +31,7 @@ package cats
  * Must obey laws in `cats.laws.DecidableLaws`.
  *
  * Based on ekmett's contravariant library:
- * https://hackage.haskell.org/package/contravariant-1.4/docs/Data-Functor-Contravariant-Divisible.html#g:2
+ * [[https://hackage.haskell.org/package/contravariant-1.4/docs/Data-Functor-Contravariant-Divisible.html#g:2]]
  */
 trait Decidable[F[_]] extends ContravariantMonoidal[F] {
   def sum[A, B](fa: F[A], fb: F[B]): F[Either[A, B]]
