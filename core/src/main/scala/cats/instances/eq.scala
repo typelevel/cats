bf5e3526ac56f7dcede874adcfc19e93796ae4a5
Stephen Lazaro <stephen.lazaro@gmail.com>
2022-07-05 18:43:08 -0700

    Simpler bincompat strategy for Eq and Equiv

--- core/src/main/scala/cats/instances/eq.scala
+++ core/src/main/scala/cats/instances/eq.scala
@@ -22,7 +22,7 @@
 package cats
 package instances
 
-trait EqInstances extends kernel.instances.EqInstances with EqInstances0 {
+trait EqInstances extends kernel.instances.EqInstances {
   implicit def catsDecidableForEq: Decidable[Eq] =
     new Decidable[Eq] {
 
@@ -56,30 +56,5 @@ trait EqInstances extends kernel.instances.EqInstances with EqInstances0 {
     }
 
   val catsContravariantMonoidalForEq: ContravariantMonoidal[Eq] =
-    catsContravariantMonoidalForEqForBinCompat
-}
-
-private[instances] trait EqInstances0 {
-  implicit def catsContravariantMonoidalForEqForBinCompat: ContravariantMonoidal[Eq] =
-    new ContravariantMonoidal[Eq] {
-
-      /**
-       * Defaults to the trivial equivalence relation
-       * contracting the type to a point
-       */
-      def unit: Eq[Unit] = Eq.allEqual
-
-      /**
-       * Derive an `Eq` for `B` given an `Eq[A]` and a function `B => A`.
-       *
-       * Note: resulting instances are law-abiding only when the functions used are injective (represent a one-to-one mapping)
-       */
-      def contramap[A, B](fa: Eq[A])(f: B => A): Eq[B] =
-        Eq.by(f)(fa)
-
-      def product[A, B](fa: Eq[A], fb: Eq[B]): Eq[(A, B)] =
-        Eq.instance { (left, right) =>
-          fa.eqv(left._1, right._1) && fb.eqv(left._2, right._2)
-        }
-    }
+    catsDecidableForEq
 }
--- core/src/main/scala/cats/instances/equiv.scala
+++ core/src/main/scala/cats/instances/equiv.scala
@@ -22,7 +22,7 @@
 package cats
 package instances
 
-trait EquivInstances extends EquivInstances0 {
+trait EquivInstances {
   implicit def catsDecidableForEquiv: Decidable[Equiv] =
     new Decidable[Equiv] {
 
@@ -65,37 +65,5 @@ trait EquivInstances extends EquivInstances0 {
     }
 
   val catsContravariantMonoidalForEquiv: ContravariantMonoidal[Equiv] =
-    catsContravariantMonoidalForEquivForBinCompat
-}
-
-private[instances] trait EquivInstances0 {
-  implicit def catsContravariantMonoidalForEquivForBinCompat: ContravariantMonoidal[Equiv] =
-    new ContravariantMonoidal[Equiv] {
-
-      /**
-       * Defaults to trivially contracting the type
-       * to a point
-       */
-      val unit: Equiv[Unit] =
-        new Equiv[Unit] {
-          def equiv(x: Unit, y: Unit): Boolean = true
-        }
-
-      /**
-       * Derive an `Equiv` for `B` given an `Equiv[A]` and a function `B => A`.
-       *
-       * Note: resulting instances are law-abiding only when the functions used are injective (represent a one-to-one mapping)
-       */
-      def contramap[A, B](fa: Equiv[A])(f: B => A): Equiv[B] =
-        new Equiv[B] {
-          def equiv(l: B, r: B): Boolean =
-            fa.equiv(f(l), f(r))
-        }
-
-      def product[A, B](fa: Equiv[A], fb: Equiv[B]): Equiv[(A, B)] =
-        new Equiv[(A, B)] {
-          def equiv(l: (A, B), r: (A, B)): Boolean =
-            fa.equiv(l._1, r._1) && fb.equiv(l._2, r._2)
-        }
-    }
+    catsDecidableForEquiv
 }
