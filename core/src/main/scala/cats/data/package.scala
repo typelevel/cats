45ae24acfe18506d0d8ab6d474e4844b83f0ebcd
David Strawn <isomarcte@gmail.com>
2022-11-14 15:25:49 -0700

    Introduce The Inverse Type
    The type `Inverse[A]` is a newtype which exists to invert the `Order`, `PartialOrder`, `UpperBounded` and `LowerBounded` instances for some type `A`.
    
    It's basically just `Id[A]` with specialized instances for ordering.
    
    Prior art: https://hackage.haskell.org/package/base-4.6.0.1/docs/Data-Ord.html#Down

--- core/src/main/scala/cats/data/package.scala
+++ core/src/main/scala/cats/data/package.scala
@@ -41,6 +41,29 @@ package object data extends ScalaVersionSpecificPackage {
   type NonEmptyChain[+A] = NonEmptyChainImpl.Type[A]
   val NonEmptyChain = NonEmptyChainImpl
 
+  /** Inverse is a data type which exists merely to invert the `Order`,
+    * `PartialOrder`, `UpperBounded`, and `LowerBounded` instances for some
+    * type `A`. In some languages this type is called `Down`.
+    *
+    * {{{
+    * scala> import cats.data._
+    * import cats.data._
+    *
+    * scala> import scala.collection.immutable.SortedSet
+    * import scala.collection.immutable.SortedSet
+    *
+    * scala> SortedSet(1, 2, 3)
+    * val res0: scala.collection.immutable.SortedSet[Int] = TreeSet(1, 2, 3)
+    *
+    * scala> res0.map(Inverse.apply)
+    * val res1: scala.collection.immutable.SortedSet[cats.data.Inverse[Int]] = TreeSet(3, 2, 1)
+    * }}}
+    *
+    * @see [[https://hackage.haskell.org/package/base-4.6.0.1/docs/Data-Ord.html#Down]]
+    */
+  type Inverse[A] = InverseImpl.Type[A]
+  val Inverse = InverseImpl
+
   type ReaderT[F[_], -A, B] = Kleisli[F, A, B]
   val ReaderT = Kleisli
 
