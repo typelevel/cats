4690306dae636cb6f79e8e1faee91eed21437534
Antonio Gelameris <toniogela89@gmail.com>
2023-06-20 22:55:14 +0200

    Adding benchmarks

---
+++ bench/src/main/scala/cats/bench/UnorderedTraverseMapBench.scala
@@ -0,0 +1,59 @@
+package cats.bench
+
+import org.openjdk.jmh.annotations._
+import cats.{CommutativeApplicative, Eval, UnorderedTraverse}
+import java.util.concurrent.TimeUnit
+import cats.data.Chain
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+class UnorderedTraverseMapBench {
+  val instance = UnorderedTraverse[Map[Int, *]]
+
+  val xs1: Map[Int, Int] = (1 to 1_000).map(x => (x, x)).toMap
+  val xs2: Map[Int, Int] = (1 to 1_000_000).map(x => (x, x)).toMap
+
+  def unorderedTraverseViaTree[G[_], A, B](
+    fa: Map[Int, A]
+  )(f: A => G[B])(implicit G: CommutativeApplicative[G]): G[Map[Int, B]] = {
+    def runHalf(size: Int, fa: Map[Int, A]): Eval[G[Map[Int, B]]] =
+      if (size > 1) {
+        val leftSize = size / 2
+        val rightSize = size - leftSize
+        val (leftL, rightL) = fa.splitAt(leftSize)
+        runHalf(leftSize, leftL).flatMap { left =>
+          val right = runHalf(rightSize, rightL)
+          G.map2Eval(left, right) { (lm, rm) => lm ++ rm }
+        }
+      } else {
+        val (k, a) = fa.head
+        Eval.always(G.map(f(a))(b => Map(k -> b)))
+      }
+
+    val len = fa.size
+    if (len == 0) G.pure(Map.empty)
+    else runHalf(len, fa).value
+  }
+
+  def unorderedTraverseViaChain[G[_], A, B](
+    fa: Map[Int, A]
+  )(f: A => G[B])(implicit G: CommutativeApplicative[G]): G[Map[Int, B]] = {
+    if (fa.isEmpty) G.pure(Map.empty[Int, B])
+    else
+      G.map(Chain.traverseViaChain(fa.toIndexedSeq) { case (k, a) =>
+        G.map(f(a))((k, _))
+      }) { chain => chain.foldLeft(Map.empty[Int, B]) { case (m, (k, b)) => m.updated(k, b) } }
+  }
+
+  @Benchmark def unorderedTraverseTupleViaTree1: (Int, Map[Int, Int]) =
+    unorderedTraverseViaTree(xs1)(x => (x, x))
+  @Benchmark def unorderedTraverseTupleViaChain1: (Int, Map[Int, Int]) =
+    unorderedTraverseViaChain(xs1)(x => (x, x))
+
+  @Benchmark def unorderedTraverseTupleViaTree2: (Int, Map[Int, Int]) =
+    unorderedTraverseViaTree(xs2)(x => (x, x))
+  @Benchmark def unorderedTraverseTupleViaChain2: (Int, Map[Int, Int]) =
+    unorderedTraverseViaChain(xs2)(x => (x, x))
+
+}
