277593b5ead39f2b9a860fb1494422351aaf2bdd
Max Smirnov <masynchin@gmail.com>
2024-02-19 22:13:03 +0300

    Optimize mapAccumulateFilter for Option

--- core/src/main/scala/cats/instances/option.scala
+++ core/src/main/scala/cats/instances/option.scala
@@ -284,6 +284,13 @@ private[instances] trait OptionInstancesBinCompat0 {
         case None    => G.pure(Option.empty[A])
         case Some(a) => G.map(f(a))(b => if (b) Some(a) else None)
       }
+  
+    override def mapAccumulateFilter[S, A, B](init: S, fa: Option[A])(f: (S, A) => (S, Option[B])): (S, Option[B]) = {
+      fa match {
+        case Some(a) => f(init, a)
+        case None => (init, None)
+      }
+    }
 
   }
 }
