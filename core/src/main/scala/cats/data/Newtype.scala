4ef2ada895bd2d831a89b2e831cb9ec333f23524
Mark Eibes <mark.eibes@gmail.com>
2024-06-14 11:24:13 +0200

    Update Newtype.scala

--- core/src/main/scala/cats/data/Newtype.scala
+++ core/src/main/scala/cats/data/Newtype.scala
@@ -25,7 +25,7 @@ package data
 /**
  * Helper trait for `newtype`s. These allow you to create a zero-allocation wrapper around a specific type.
  * Similar to `AnyVal` value classes, but never have any runtime overhead.
- * It's coped from the newtypes lib by @alexknvl
+ * It's copied from the newtypes lib by @alexknvl
  * For more detail see https://github.com/alexknvl/newtypes
  */
 private[data] trait Newtype { self =>
